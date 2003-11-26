3d7caa32ee6e16bba7f43f05e7bdb9a1eafa4bec
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2003-11-26 11:01:22 +0000

    Mathemagix documentation

---
+++ TeXmacs/styles/documentation/mmxdoc.ts
@@ -0,0 +1,30 @@
+<TeXmacs|1.0.2.9>
+
+<style|tmdoc>
+
+<\body>
+  <assign|mmxdoc-style|1.0>
+
+  <assign|mmx|<with|font-shape|small-caps|Mathemagix>>
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+    <associate|page-even|30mm>
+    <associate|page-reduce-right|25mm>
+    <associate|page-reduce-bot|15mm>
+    <associate|page-reduce-left|25mm>
+    <associate|sfactor|4>
+    <associate|page-top|30mm>
+    <associate|page-type|a4>
+    <associate|page-right|30mm>
+    <associate|par-width|150mm>
+    <associate|page-odd|30mm>
+    <associate|page-bot|30mm>
+    <associate|language|english>
+    <associate|page-reduce-top|15mm>
+  </collection>
+</initial>
