f383c54c744c92d1929a4f96d8de8787e58ca871
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-01-18 16:39:40 +0000

    First version of Lisp plug-in.

---
+++ src/plugins/lisp/clisp/clisp-init.lisp
@@ -0,0 +1,13 @@
+(setf tm-data-begin "")
+(setf tm-data-end "")
+
+(EXT:WITHOUT-PACKAGE-LOCK ("SYSTEM")
+	(defun SYSTEM::PROMPT-STRING1 () ""))
+
+(EXT:WITHOUT-PACKAGE-LOCK ("SYSTEM")
+	(defun SYSTEM::PROMPT-STRING2 () "channel:promptCLisp> "))
+
+(EXT:WITHOUT-PACKAGE-LOCK ("SYSTEM")
+	(defun SYSTEM::PROMPT-STRING3 () ""))
+
+(load "$TEXMACS_LISP_PATH/lisp/tmlib.lisp")
