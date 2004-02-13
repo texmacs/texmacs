d6c821d5739bd523cacd098434d2966315b8e17a
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-02-13 17:53:06 +0000

    [patch #2577] new-line does shoving

--- src/src/Typeset/Stack/stacker.cpp
+++ src/src/Typeset/Stack/stacker.cpp
@@ -225,7 +225,6 @@ stacker_rep::new_paragraph () {
   unit_sb->nobr_before= unit_sb->nobr_after;
   unit_sb->nobr_after = false;
   unit_start          = N(l);
-  unit_flag           = true;
 }
 
 void
