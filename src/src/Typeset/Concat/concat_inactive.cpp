4a8002787e98cd24e0c991b032fb203c95cac49f
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-03-02 23:15:17 +0000

    Reorganize inactive markup (part II).

--- src/src/Typeset/Concat/concat_inactive.cpp
+++ src/src/Typeset/Concat/concat_inactive.cpp
@@ -11,6 +11,7 @@
 ******************************************************************************/
 
 #include "concater.hpp"
+#include "analyze.hpp"
 
 /******************************************************************************
 * Typesetting deactivated trees
@@ -235,7 +236,7 @@ concater_rep::typeset_unknown (string which, tree t, path ip, bool flag) {
 }
 
 /******************************************************************************
-* Dispatch
+* Dispatching
 ******************************************************************************/
 
 void
@@ -256,174 +257,15 @@ concater_rep::typeset_inactive (tree t, path ip) {
   case RAW_DATA:
     typeset_inactive_string ("<raw-data>", ip);
     break;
-  case DOCUMENT:
-    typeset_inactive_expand ("document", t, ip);
-    break;
-  case PARA:
-    typeset_inactive_expand ("paragraph", t, ip);
-    break;
-  case SURROUND:
-    typeset_inactive_expand ("surround", t, ip);
-    break;
   case CONCAT:
     typeset_concat (t, ip);
     break;
-  case GROUP:
-    typeset_group (t, ip);
-    break;
-  case HSPACE:
-    typeset_inactive_expand ("hspace", t, ip);
-    break;
-  case VAR_VSPACE:
-    typeset_inactive_expand ("vspace*", t, ip);
-    break;
-  case VSPACE:
-    typeset_inactive_expand ("vspace", t, ip);
-    break;
-  case SPACE:
-    typeset_inactive_expand ("space", t, ip);
-    break;
-  case HTAB:
-    typeset_inactive_expand ("tab", t, ip);
-    break;
-  case MOVE:
-    typeset_inactive_expand ("move", t, ip);
-    break;
-  case RESIZE:
-    typeset_inactive_expand ("resize", t, ip);
-    break;
-  case REPEAT:
-    typeset_inactive_expand ("repeat", t, ip);
-    break;
-  case _FLOAT:
-    typeset_inactive_expand ("float", t, ip);
-    break;
-  case DATOMS:
-    typeset_inactive_expand ("decorate atoms", t, ip);
-    break;
-  case DLINES:
-    typeset_inactive_expand ("decorate lines", t, ip);
-    break;
-  case DPAGES:
-    typeset_inactive_expand ("decorate pages", t, ip);
-    break;
-  case DBOX:
-    typeset_decorated_box (t, ip);
-    break;
 
-  case WITH_LIMITS:
-    typeset_inactive_string ("<with limits>", ip);
-    break;
-  case LINE_BREAK:
-    typeset_inactive_string ("<line break>", ip);
-    break;
-  case NEW_LINE:
-  case LINE_SEP:
-  case NEXT_LINE:
-    {
-      string name= env->drd->get_name (L(t));
-      typeset_inactive_string ("<" * name * ">", ip);
-      break;
-    }
-  case NO_BREAK:
-    typeset_inactive_string ("<no-break>", ip);
-    break;
-  case YES_INDENT:
-    typeset_inactive_string ("<do indent>", ip);
-    break;
-  case NO_INDENT:
-    typeset_inactive_string ("<don't indent>", ip);
-    break;
-  case VAR_YES_INDENT:
-    typeset_inactive_string ("<do indent after>", ip);
-    break;
-  case VAR_NO_INDENT:
-    typeset_inactive_string ("<don't indent after>", ip);
-    break;
-  case VAR_PAGE_BREAK:
-  case PAGE_BREAK:
-  case VAR_NO_PAGE_BREAK:
-  case NO_PAGE_BREAK:
-  case VAR_NEW_PAGE:
-  case NEW_PAGE:
-  case VAR_NEW_DPAGE:
-  case NEW_DPAGE:
-    {
-      string name= env->drd->get_name (L(t));
-      typeset_inactive_string ("<" * name * ">", ip);
-      break;
-    }
-
-  case LEFT:
-    typeset_left (t, ip);
-    break;
-  case MID:
-    typeset_middle (t, ip);
-    break;
-  case RIGHT:
-    typeset_right (t, ip);
-    break;
-  case BIG:
-    typeset_bigop (t, ip);
-    break;
-  case LPRIME:
-    typeset_lprime (t, ip);
-    break;
-  case RPRIME:
-    typeset_rprime (t, ip);
-    break;
-  case BELOW:
-    typeset_below (t, ip);
-    break;
-  case ABOVE:
-    typeset_above (t, ip);
-    break;
-  case LSUB:
-  case LSUP:
-    typeset_script (t, ip, false);
-    break;
-  case RSUB:
-  case RSUP:
-    typeset_script (t, ip, true);
-    break;
-  case FRAC:
-    typeset_frac (t, ip);
-    break;
-  case SQRT:
-    typeset_sqrt (t, ip);
-    break;
-  case WIDE:
-    typeset_wide (t, ip, true);
-    break;
-  case VAR_WIDE:
-    typeset_wide (t, ip, false);
-    break;
-  case NEG:
-    typeset_neg (t, ip);
-    break;
-  case TREE:
-    typeset_tree (t, ip);
-    break;
-
-  case TFORMAT:
-    typeset_inactive_expand ("table-format", t, ip);
-    break;
-  case TWITH:
-    typeset_inactive_expand ("table-with", t, ip);
-    break;
-  case CWITH:
-    typeset_inactive_expand ("cell-with", t, ip);
-    break;
-  case TMARKER:
-    typeset_inactive_expand ("table-marker", t, ip);
-    break;
+    /*
   case TABLE:
     typeset_table (t, ip);
     break;
-  case ROW:
-  case CELL:
-  case SUBTABLE:
-    break;
+    */
 
   case ASSIGN:
     typeset_inactive_expand ("assign", t, ip, 1);
@@ -431,147 +273,14 @@ concater_rep::typeset_inactive (tree t, path ip) {
   case WITH:
     typeset_inactive_expand ("with", t, ip, N(t)-1);
     break;
-  case PROVIDES:
-    typeset_inactive_expand ("provides", t, ip);
-    break;
-  case VALUE:
-    typeset_inactive_expand ("value", t, ip);
-    break;
   case MACRO:
     typeset_inactive_expand ("macro", t, ip, N(t)-1);
     break;
   case DRD_PROPS:
     typeset_inactive_expand ("drd-properties", t, ip, 1);
     break;
-  case ARG:
-    typeset_inactive_expand ("argument", t, ip);
-    break;
-  case COMPOUND:
-    typeset_inactive_compound (t, ip);
-    break;
   case XMACRO:
     typeset_inactive_expand ("xmacro", t, ip, 1);
-    break;
-  case GET_LABEL:
-    typeset_inactive_expand ("tree-label", t, ip);
-    break;
-  case GET_ARITY:
-    typeset_inactive_expand ("arity", t, ip);
-    break;
-  case MAP_ARGS:
-    typeset_inactive_expand ("map-args", t, ip);
-    break;
-  case EVAL_ARGS:
-    typeset_inactive_expand ("eval-args", t, ip);
-    break;
-  case EVAL:
-    typeset_inactive_expand ("eval", t, ip);
-    break;
-  case QUOTE:
-    typeset_inactive_expand ("quote", t, ip);
-    break;
-  case DELAY:
-    typeset_inactive_expand ("delay", t, ip);
-    break;
-  case HOLD:
-    typeset_inactive_expand ("hold", t, ip);
-    break;
-  case RELEASE:
-    typeset_inactive_expand ("release", t, ip);
-    break;
-  case EXTERN:
-    typeset_inactive_expand ("extern", t, ip);
-    break;
-  case INCLUDE:
-    typeset_inactive_expand ("include", t, ip);
-    break;
-
-  case OR:
-    typeset_inactive_expand ("or", t, ip);
-    break;
-  case XOR:
-    typeset_inactive_expand ("xor", t, ip);
-    break;
-  case AND:
-    typeset_inactive_expand ("and", t, ip);
-    break;
-  case NOT:
-    typeset_inactive_expand ("not", t, ip);
-    break;
-  case PLUS:
-    typeset_inactive_expand ("+", t, ip);
-    break;
-  case MINUS:
-    typeset_inactive_expand ("-", t, ip);
-    break;
-  case TIMES:
-    typeset_inactive_expand ("*", t, ip);
-    break;
-  case OVER:
-    typeset_inactive_expand ("/", t, ip);
-    break;
-  case DIV:
-    typeset_inactive_expand ("div", t, ip);
-    break;
-  case MOD:
-    typeset_inactive_expand ("mod", t, ip);
-    break;
-  case MERGE:
-    typeset_inactive_expand ("merge", t, ip);
-    break;
-  case LENGTH:
-    typeset_inactive_expand ("length", t, ip);
-    break;
-  case RANGE:
-    typeset_inactive_expand ("range", t, ip);
-    break;
-  case NUMBER:
-    typeset_inactive_expand ("number", t, ip);
-    break;
-  case _DATE:
-    typeset_inactive_expand ("date", t, ip);
-    break;
-  case TRANSLATE:
-    typeset_inactive_expand ("translate", t, ip);
-    break;
-  case FIND_FILE:
-    typeset_inactive_expand ("find file", t, ip);
-    break;
-  case IS_TUPLE:
-    typeset_inactive_expand ("is tuple", t, ip);
-    break;
-  case LOOK_UP:
-    typeset_inactive_expand ("look up", t, ip);
-    break;
-  case EQUAL:
-    typeset_inactive_expand ("equal", t, ip);
-    break;
-  case UNEQUAL:
-    typeset_inactive_expand ("unequal", t, ip);
-    break;
-  case LESS:
-    typeset_inactive_expand ("less", t, ip);
-    break;
-  case LESSEQ:
-    typeset_inactive_expand ("less or equal", t, ip);
-    break;
-  case GREATER:
-    typeset_inactive_expand ("greater", t, ip);
-    break;
-  case GREATEREQ:
-    typeset_inactive_expand ("greater or equal", t, ip);
-    break;
-  case IF:
-    typeset_inactive_expand ("if", t, ip);
-    break;
-  case VAR_IF:
-    typeset_inactive_expand ("if*", t, ip);
-    break;
-  case CASE:
-    typeset_inactive_expand ("case", t, ip);
-    break;
-  case WHILE:
-    typeset_inactive_expand ("while", t, ip);
     break;
 
   case INACTIVE:
@@ -595,87 +304,15 @@ concater_rep::typeset_inactive (tree t, path ip) {
   case HYBRID:
     typeset_inactive_hybrid (t, ip);
     break;
-  case TUPLE:
-    typeset_inactive_expand ("tuple", t, ip);
-    break;
-  case ATTR:
-    typeset_inactive_expand ("attr", t, ip);
-    break;
-  case COLLECTION:
-    typeset_inactive_expand ("collection", t, ip);
-    break;
-  case ASSOCIATE:
-    typeset_inactive_expand ("associate", t, ip);
-    break;
-  case BACKUP:
-    typeset_inactive_expand ("backup", t, ip);
-    break;
-  case LABEL:
-    typeset_inactive_expand ("label", t, ip);
-    break;
-  case REFERENCE:
-    typeset_inactive_expand ("reference", t, ip);
-    break;
-  case PAGEREF:
-    typeset_inactive_expand ("page reference", t, ip);
-    break;
-  case WRITE:
-    typeset_inactive_expand ("write", t, ip);
-    break;
   case SPECIFIC:
     typeset_inactive_specific (t, ip);
     break;
-  case HLINK:
-    typeset_inactive_action ("hyperlink", t, ip);
-    break;
-  case ACTION:
-    typeset_inactive_action ("action", t, ip);
-    break;
-  case TAG:
-    typeset_inactive_expand ("tag", t, ip);
-    break;
-  case MEANING:
-    typeset_inactive_expand ("meaning", t, ip);
-    break;
-  case FLAG:
-    typeset_inactive_expand ("flag", t, ip);
-    break;
 
-  case GRAPHICS:
-    typeset_inactive_expand ("graphics", t, ip);
-    break;
-  case SUPERPOSE:
-    typeset_inactive_expand ("superpose", t, ip);
-    break;
-  case TEXT_AT:
-    typeset_inactive_expand ("text at", t, ip);
-    break;
-  case _POINT:
-    typeset_inactive_expand ("point", t, ip);
-    break;
-  case LINE:
-    typeset_inactive_expand ("line", t, ip);
-    break;
-  case CLINE:
-    typeset_inactive_expand ("cline", t, ip);
-    break;
-  case SPLINE:
-    typeset_inactive_expand ("spline", t, ip);
-    break;
-  case VAR_SPLINE:
-    typeset_inactive_expand ("var_spline", t, ip);
-    break;
-  case CSPLINE:
-    typeset_inactive_expand ("cspline", t, ip);
-    break;
-  case FILL:
-    typeset_inactive_expand ("fill", t, ip);
-    break;
-  case POSTSCRIPT:
-    typeset_inactive_expand ("postscript", t, ip);
-    break;
   default:
-    typeset_inactive_expand (as_string (L(t)), t, ip);
-    break;
+    {
+      string name= replace (env->drd->get_name (L(t)), " ", "-");
+      typeset_inactive_expand (name, t, ip);
+      break;
+    }
   }
 }
