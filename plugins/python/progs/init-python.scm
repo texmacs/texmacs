81be515c0abb66011ae8b3552f2243fa1c042fc4
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-03-29 15:16:29 +0000

    Fix python recognition.

--- plugins/python/progs/init-python.scm
+++ plugins/python/progs/init-python.scm
@@ -1,5 +1,5 @@
 (plugin-configure python
-  (:require (url-exists-in-path? "tm_python"))
+  (:require (url-exists-in-path? "python"))
   (:launch "tm_python --texmacs")
   (:tab-completion #t)
   (:session "Python"))
