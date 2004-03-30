deb9a348e6877923909add48f903c51bfba78072
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-03-30 09:21:58 +0000

    Report dangerous changes in new version.

--- src/System/Boot/init_upgrade.cpp
+++ src/System/Boot/init_upgrade.cpp
@@ -90,8 +90,10 @@ init_upgrade_doc (string install_version) {
     append (from_dir * "changes-6.en.ptm", to);
   if (version_inf_eq (install_version, "1.0.2.6"))
     append (from_dir * "changes-7.en.ptm", to);
-  if (version_inf_eq (install_version, "1.0.2.8")) {
+  if (version_inf_eq (install_version, "1.0.2.8"))
     append (from_dir * "changes-8.en.ptm", to);
+  if (version_inf_eq (install_version, "1.0.3.4")) {
+    append (from_dir * "changes-9.en.ptm", to);
     append (from_dir * "changes-post.en.ptm", to);
   }
   else remove (to);
