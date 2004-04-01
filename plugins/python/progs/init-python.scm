50ec4d326027e2aa82a86101f2942fe578d58615
Ero <ero>
2004-04-01 20:28:00 +0000

    Added additional check for the main plugin script.

--- plugins/python/progs/init-python.scm
+++ plugins/python/progs/init-python.scm
@@ -1,5 +1,6 @@
 (plugin-configure python
   (:require (url-exists-in-path? "python"))
+  (:require (url-exists-in-path? "tm_python"))
   (:launch "tm_python --texmacs")
   (:tab-completion #t)
   (:session "Python"))
