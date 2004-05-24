d1d8dfc1b9b520704e2426bbe26c77e1111ecad5
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-05-24 14:03:30 +0000

    Added benchmarking flag --debug-bench

--- src/src/Texmacs/Server/tm_server.cpp
+++ src/src/Texmacs/Server/tm_server.cpp
@@ -97,8 +97,12 @@ tm_server_rep::tm_server_rep (display dis2):
     tm_init_file= "$TEXMACS_PATH/progs/init-texmacs.scm";
   if (is_none (my_init_file))
     my_init_file= "$TEXMACS_HOME_PATH/progs/my-init-texmacs.scm";
+  time_t scheme_start= texmacs_time ();
   if (exists (tm_init_file)) exec_file (tm_init_file);
   if (exists (my_init_file)) exec_file (my_init_file);
+  time_t scheme_boot = texmacs_time () - scheme_start;
+  if (DEBUG_BENCH)
+    cout << "Bench  ] Initialized scheme in " << scheme_boot << " ms\n";
   if (my_init_cmds != "") {
     my_init_cmds= "(begin" * my_init_cmds * ")";
     exec_delayed (my_init_cmds);
