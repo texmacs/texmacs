452987597fbcac74f0b370dee5df5819e34bbf44
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-06-29 18:08:25 +0000

    Faster editing of long documents

---
+++ src/Typeset/Bridge/bridge_docrange.cpp
@@ -0,0 +1,276 @@
+
+/******************************************************************************
+* MODULE     : bridge_docrange.cpp
+* DESCRIPTION: Bridge between logical and physically typesetted document
+* COPYRIGHT  : (C) 1999  Joris van der Hoeven
+*******************************************************************************
+* This file contains a VERY HACKY way to accelerate the editing of
+* document nodes with many (> 64) children. It should be noticed that
+* the notification call-back methods do not behave as usual: the st tree
+* remains the st tree of the entire document tree and brs stands for
+* a reference to the corresponding bridges. The bridge_docrange structure
+* only behaves as usual for the my_exec_until, my_typeset_will_be_complete
+* and my_typeset methods.
+*******************************************************************************
+* This software falls under the GNU general public license and comes WITHOUT
+* ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for more details.
+* If you don't have this file, write to the Free Software Foundation, Inc.,
+* 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+******************************************************************************/
+
+#include "bridge.hpp"
+
+#define ACC_THRESHOLD 64
+
+/******************************************************************************
+* The docrange bridge structure
+******************************************************************************/
+
+class bridge_docrange_rep: public bridge_rep {
+protected:
+  array<bridge> acc;
+  array<bridge>& brs;
+  array<int> mid;
+  int begin, end;
+  bool divide;
+
+public:
+  bridge_docrange_rep (typesetter ttt, tree st, path ip, array<bridge>& brs,
+		       int begin, int end, bool divide);
+  void rebalance ();
+
+  void notify_assign (path p, tree u);
+  void notify_insert (path p, tree u);
+  void notify_remove (path p, int nr);
+  bool notify_macro  (int type, string var, int l, path p, tree u);
+  void notify_change ();
+
+  void my_exec_until (path p);
+  bool my_typeset_will_be_complete ();
+  void my_typeset (int desired_status);
+};
+
+bridge
+bridge_docrange (typesetter ttt, tree st, path ip,
+		 array<bridge>& brs, int begin, int end, bool divide= false)
+{
+  // cout << "Make range: " << begin << " -- " << end << "\n";
+  return new bridge_docrange_rep (ttt, st, ip, brs, begin, end, divide);
+}
+
+bridge_docrange_rep::bridge_docrange_rep (
+  typesetter ttt, tree st, path ip, array<bridge>& brs2,
+  int begin2, int end2, bool divide2):
+    bridge_rep (ttt, st, ip), brs (brs2),
+    begin (begin2), end (end2), divide (divide2)
+{
+  if (divide) {
+    int i, n= ((end - begin - 1) / ACC_THRESHOLD) + 1;
+    acc= array<bridge> (n);
+    mid= array<int> (n+1);
+    for (i=0; i<n; i++) {
+      mid[i]  = begin + i * ACC_THRESHOLD;
+      mid[i+1]= min (mid[i] + ACC_THRESHOLD, end);
+      acc[i]  = bridge_docrange (ttt, st, ip, brs, mid[i], mid[i+1]);
+    }
+  }
+}
+
+void
+bridge_docrange_rep::rebalance () {
+  int i, n= N(acc);
+  array<bridge> acc2;
+  array<int>    mid2;
+
+  for (i=0; i<n; i++) {
+    // Compactify?
+    if ((i < n-1) && ((mid[i+2] == mid[i+1]) ||
+		      (mid[i+2] - mid[i] <= ACC_THRESHOLD)))
+      {
+	int start= i;
+	while ((i < n-1) && ((mid[i+2] == mid[start+1]) ||
+			     (mid[i+2] - mid[start] <= ACC_THRESHOLD))) i++;
+	// cout << "  Compactify " << i-start << " at " << start << "\n";
+	if (mid[i+1] == mid[start+1]) acc2 << acc[i];
+	else acc2 << bridge_docrange (ttt, st, ip, brs, mid[start], mid[i+1]);
+	mid2 << mid[start];
+      }
+    
+    // Expand?
+    else if (mid[i+1] - mid[i] > (7 * ACC_THRESHOLD / 4)) {
+      int j, k= (mid[i+1] - mid[i] - 1) / ACC_THRESHOLD + 1;
+      // cout << "  Expand " << k << " at " << i << "\n";
+      for (j=0; j<k; j++) {
+	int b= mid[i] + j * ACC_THRESHOLD;
+	int e= min (b + ACC_THRESHOLD, mid[i+1]);
+	acc2 << bridge_docrange (ttt, st, ip, brs, b, e);
+	mid2 << b;
+      }
+    }
+    
+    // OK
+    else {
+      acc2 << acc[i];
+      mid2 << mid[i];
+    }
+  }
+
+  mid2 << end;
+  acc= acc2;
+  mid= mid2;
+}
+
+/******************************************************************************
+* Event notification
+******************************************************************************/
+
+void
+bridge_docrange_rep::notify_assign (path p, tree u) {
+  if (nil (p))
+    fatal_error ("Erroneous nil path", "bridge_docrange_rep::notify_assign");
+  if (divide) {
+    int i, n= N(acc);
+    for (i=0; i<n; i++)
+      if ((p->item >= mid[i]) && (p->item < mid[i+1]))
+	acc[i]->notify_assign (p, u);
+  }
+  status= CORRUPTED;
+}
+
+void
+bridge_docrange_rep::notify_insert (path p, tree u) {
+  if (nil (p))
+    fatal_error ("Erroneous nil path", "bridge_docrange_rep::notify_insert");
+  if (p->item > end) {
+    cerr << "\nNotify insert " << u << " at " << p << "\n";
+    fatal_error ("Out of range", "bridge_docrange_rep::notify_insert");
+  }
+  if (p->item >= begin) status= CORRUPTED;
+  else begin += N(u);
+  end += N(u);
+
+  if (divide) {
+    int i, n= N(acc);
+    for (i=0; i<n; i++)
+      if (p->item <= mid[i+1])
+	break;
+    for (; i<n; i++) {
+      acc[i]->notify_insert (p, u);
+      mid[i+1] += N(u);
+    }
+    // cout << "mid= " << mid << "\n";
+    rebalance ();
+    // cout << "mid= " << mid << "\n";
+  }
+}
+
+void
+bridge_docrange_rep::notify_remove (path p, int nr) {
+  if (nil (p))
+    fatal_error ("Erroneous nil path", "bridge_docrange_rep::notify_insert");
+  if (p->item >= end)
+    fatal_error ("Out of range", "bridge_docrange_rep::notify_insert");
+  if (p->item + nr > begin) {
+    status= CORRUPTED;
+    begin= min (begin , p->item);
+    end  = max (end-nr, p->item);
+  }
+  else {
+    begin -= nr;
+    end   -= nr;
+  }
+
+  if (divide) {
+    int i, n= N(acc);
+    for (i=0; i<n; i++)
+      if (p->item < mid[i+1])
+	break;
+    for (; i<n; i++) {
+      acc[i]->notify_remove (p, nr);
+      mid[i+1]= max (mid[i+1]-nr, p->item);
+    }
+    // cout << "mid= " << mid << "\n";
+    rebalance ();
+    // cout << "mid= " << mid << "\n";
+  }
+}
+
+bool
+bridge_docrange_rep::notify_macro (int type, string v, int l, path p, tree u) {
+  (void) type; (void) v; (void) l; (void) p; (void) u;
+  fatal_error ("Method should never be called",
+	       "bridge_docrange_rep::notify_macro");
+}
+
+void
+bridge_docrange_rep::notify_change () {
+  status= CORRUPTED;
+  if (divide) acc[0]->notify_change ();
+}
+
+/******************************************************************************
+* Typesetting
+******************************************************************************/
+
+void
+bridge_docrange_rep::my_exec_until (path p) {
+  if (p->item < begin);
+  else if ((p->item >= end) && ((status & VALID_MASK) == PROCESSED))
+    env->patch_env (changes);
+  else if (divide) {
+    int i, n= N(acc);
+    for (i=0; i<n; i++)
+      acc[i]->my_exec_until (p);
+  }
+  else {
+    int i;
+    for (i=begin; i<p->item; i++)
+      brs[i]->exec_until (path (1));
+    brs[i]->exec_until (p->next);
+  }
+}
+
+bool
+bridge_docrange_rep::my_typeset_will_be_complete () {
+  int i, n= N(acc);
+  if (divide)
+    for (i=0; i<n; i++)
+      if (!acc[i]->my_typeset_will_be_complete ()) return false;
+  else
+    for (i=begin; i<end; i++)
+      if (!brs[i]->my_typeset_will_be_complete ()) return false;
+  return true;
+}
+
+// static bool top_level= true;
+
+void
+bridge_docrange_rep::my_typeset (int desired_status) {
+  int i, n= N(acc);
+  array<line_item> a= ttt->a;
+  array<line_item> b= ttt->b;
+  if (divide) {
+    for (i=0; i<n; i++) {
+      int wanted= (i==n-1? desired_status & WANTED_MASK: WANTED_PARAGRAPH);
+      ttt->a= (i==0  ? a: array<line_item> ());
+      ttt->b= (i==n-1? b: array<line_item> ());
+      acc[i]->typeset (PROCESSED+ wanted);
+    }
+  }
+  else {
+    //bool show= top_level;
+    //bool old_top_level= top_level;
+    //top_level= false;
+    //if (show) cout << "Typeset range: " << begin << " -- " << end << "\n  ";
+    for (i=begin; i<end; i++) {
+      int wanted= (i==end-1? desired_status & WANTED_MASK: WANTED_PARAGRAPH);
+      ttt->a= (i==0    ? a: array<line_item> ());
+      ttt->b= (i==end-1? b: array<line_item> ());
+      //if (show) cout << (brs[i]->status == PROCESSED + wanted);
+      //if (show) cout.flush ();
+      brs[i]->typeset (PROCESSED+ wanted);
+    }
+    //if (show) cout << "\n";
+    //top_level= old_top_level;
+  }
+}
--- src/Typeset/Bridge/bridge_document.cpp
+++ src/Typeset/Bridge/bridge_document.cpp
@@ -12,13 +12,18 @@
 
 #include "bridge.hpp"
 
+bridge bridge_docrange (typesetter ttt, tree st, path ip, array<bridge>& brs,
+			int begin, int end, bool divide);
+
 class bridge_document_rep: public bridge_rep {
 protected:
   array<bridge> brs;
+  bridge acc; // binary splitting acceleration for long documents
 
 public:
   bridge_document_rep (typesetter ttt, tree st, path ip);
   void initialize ();
+  void initialize_acc ();
 
   void notify_assign (path p, tree u);
   void notify_insert (path p, tree u);
@@ -43,6 +48,13 @@ bridge_document_rep::initialize () {
   brs= array<bridge> (n);
   for (i=0; i<n; i++)
     brs[i]= make_bridge (ttt, st[i], descend (ip, i));
+  initialize_acc ();
+}
+
+void
+bridge_document_rep::initialize_acc () {
+  if (ttt->paper) acc= bridge ();
+  else acc= bridge_docrange (ttt, st, ip, brs, 0, N(st), true);
 }
 
 bridge
@@ -60,13 +72,16 @@ bridge_document_rep::notify_assign (path p, tree u) {
   if (nil (p) && (!is_func (u, DOCUMENT)) && (!is_func (u, PARA)))
     fatal_error ("Nil path", "bridge_document_rep::notify_assign");
   if (nil (p)) { st= u; initialize (); }
-  else if (atom (p)) {
-    replace_bridge (brs[p->item], u, descend (ip, p->item));
-    st= substitute (st, p->item, brs[p->item]->st);
-  }
   else {
-    brs[p->item]->notify_assign (p->next, u);
-    st= substitute (st, p->item, brs[p->item]->st);
+    if (atom (p)) {
+      replace_bridge (brs[p->item], u, descend (ip, p->item));
+      st= substitute (st, p->item, brs[p->item]->st);
+    }
+    else {
+      brs[p->item]->notify_assign (p->next, u);
+      st= substitute (st, p->item, brs[p->item]->st);
+    }
+    if (!nil (acc)) acc->notify_assign (p, u);
   }
   status= CORRUPTED;
 }
@@ -86,10 +101,13 @@ bridge_document_rep::notify_insert (path p, tree u) {
     }
     brs= brs2;
     st = (st (0, p->item) * u) * st (p->item, N(st));
+    if (!nil (acc)) acc->notify_insert (p, u);
+    // initialize_acc ();
   }
   else {
     brs[p->item]->notify_insert (p->next, u);
     st= substitute (st, p->item, brs[p->item]->st);
+    if (!nil (acc)) acc->notify_assign (p->item, st[p->item]);
   }
   status= CORRUPTED;
 }
@@ -117,10 +135,13 @@ bridge_document_rep::notify_remove (path p, int nr) {
     if (change_flag) // touch brs[pos..n] for correct ``changes handling''
       for (i=pos; i<n; i++)
 	brs[i]->notify_change ();
+    if (!nil (acc)) acc->notify_remove (p, nr);
+    // initialize_acc ();
   }
   else {
     brs[p->item]->notify_remove (p->next, nr);
     st= substitute (st, p->item, brs[p->item]->st);
+    if (!nil (acc)) acc->notify_assign (p->item, st[p->item]);
   }
   status= CORRUPTED;
 }
@@ -131,15 +152,18 @@ bridge_document_rep::notify_macro (int tp, string var, int l, path p, tree u) {
   int i, n= N(brs);
   for (i=0; i<n; i++)
     flag= brs[i]->notify_macro (tp, var, l, p, u) || flag;
-  if (flag) status= CORRUPTED;
+  if (flag) {
+    status= CORRUPTED;
+    if (!nil (acc)) acc->notify_change ();
+  }
   return flag;
 }
 
 void
 bridge_document_rep::notify_change () {
   status= CORRUPTED;
-  if (N(brs)>0)
-    brs[0]->notify_change ();
+  if (!nil (acc)) acc->notify_change ();
+  if (N(brs)>0) brs[0]->notify_change ();
 }
 
 /******************************************************************************
@@ -148,29 +172,38 @@ bridge_document_rep::notify_change () {
 
 void
 bridge_document_rep::my_exec_until (path p) {
-  int i;
-  for (i=0; i<p->item; i++)
-    brs[i]->exec_until (path (1));
-  brs[i]->exec_until (p->next);
+  if (nil (acc)) {
+    int i;
+    for (i=0; i<p->item; i++)
+      brs[i]->exec_until (path (1));
+    brs[i]->exec_until (p->next);
+  }
+  else acc->my_exec_until (p);
 }
 
 bool
 bridge_document_rep::my_typeset_will_be_complete () {
-  int i, n= N(brs);
-  for (i=0; i<n; i++)
-    if (!brs[i]->my_typeset_will_be_complete ()) return false;
-  return true;
+  if (nil (acc)) {
+    int i, n= N(brs);
+    for (i=0; i<n; i++)
+      if (!brs[i]->my_typeset_will_be_complete ()) return false;
+    return true;
+  }
+  else return acc->my_typeset_will_be_complete ();
 }
 
 void
 bridge_document_rep::my_typeset (int desired_status) {
-  int i, n= N(st);
-  array<line_item> a= ttt->a;
-  array<line_item> b= ttt->b;
-  for (i=0; i<n; i++) {
-    int wanted= (i==n-1? desired_status & WANTED_MASK: WANTED_PARAGRAPH);
-    ttt->a= (i==0  ? a: array<line_item> ());
-    ttt->b= (i==n-1? b: array<line_item> ());
-    brs[i]->typeset (PROCESSED+ wanted);
+  if (nil (acc)) {
+    int i, n= N(st);
+    array<line_item> a= ttt->a;
+    array<line_item> b= ttt->b;
+    for (i=0; i<n; i++) {
+      int wanted= (i==n-1? desired_status & WANTED_MASK: WANTED_PARAGRAPH);
+      ttt->a= (i==0  ? a: array<line_item> ());
+      ttt->b= (i==n-1? b: array<line_item> ());
+      brs[i]->typeset (PROCESSED+ wanted);
+    }
   }
+  else acc->my_typeset (desired_status);
 }
