3fc20d2b70a54b6d0ad600ca8dfc19d7adadbfed
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-07-14 17:50:17 +0000

    Describe default values of environment variables.

--- src/TeXmacs/packages/documentation/tmdoc-markup.ts
+++ src/TeXmacs/packages/documentation/tmdoc-markup.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.0.3.7>
+<TeXmacs|1.0.3.10>
 
 <style|source>
 
@@ -75,6 +75,8 @@
   grey|(<arg|x>)><vspace|0.25fn>>>
 
   <assign|src-value|<macro|x|<with|font-shape|right|color|black|<arg|x>>>>
+
+  <assign|var-val|<macro|var|val|<src-var|<arg|var>><active*|<with|mode|math|\<assign\>>><with|font-family|tt|<arg|val>>>>
 
   <\active*>
     <\src-comment>
@@ -180,6 +182,5 @@
     <associate|language|english>
     <associate|par-par-sep|0fn>
     <associate|preamble|true>
-    <associate|sfactor|4>
   </collection>
 </initial>
