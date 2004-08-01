e871be7f0f39ebbb653111394d6c9d7ace312fc3
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-08-01 11:37:08 +0000

    Easier styling of equation/eqnarray rendering.

--- TeXmacs/packages/environment/env-math.ts
+++ TeXmacs/packages/environment/env-math.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.0.3.4>
+<TeXmacs|1.0.4>
 
 <style|source>
 
@@ -37,15 +37,57 @@
 
   <\active*>
     <\src-comment>
+      Styling parameters.
+
+      \ \ eqn-short-above + par-sep: smallest skip above equation (no
+      collisions).
+
+      \ \ eqn-short-above + eqn-ver-sep: largest skip above equation (when
+      collisions).
+
+      \ \ eqn-long-above + par-row-sep: skip above equation array.
+
+      \ \ eqn-short-below and eqn-long-below: similar to above.
+    </src-comment>
+  </active*>
+
+  <assign|eqn-short-above|<macro|0.35fn>>
+
+  <assign|eqn-short-below|<macro|0.35fn>>
+
+  <assign|eqn-long-above|<macro|0.5fn>>
+
+  <assign|eqn-long-below|<macro|0.5fn>>
+
+  <assign|eqn-ver-sep|<macro|0.6fn>>
+
+  <assign|eqn-row-sep|<macro|0.45fn>>
+
+  <\active*>
+    <\src-comment>
       Single equations.
     </src-comment>
   </active*>
 
-  <assign|equation-lab|<macro|body|lab|<with|mode|math|math-display|true|par-sep|0.45fn|<style-with|src-compact|none|<surround|<no-page-break*><vspace*|0.5fn><no-indent><assign|the-label|<arg|lab>><htab|5mm>|<htab|5mm>(<with|mode|text|<arg|lab>>)<vspace|0.5fn><no-indent*>|<arg|body>>>>>>
+  <assign|equation*|<macro|body|<\with|mode|math|math-display|true|par-ver-sep|<eqn-ver-sep>>
+    <style-with|src-compact|none|<surround|<no-page-break*><vspace*|<eqn-short-above>><no-indent><htab|0fn>|<htab|0fn><vspace|<eqn-short-below>><no-indent*>|<arg|body>>>
+  </with>>>
 
-  <assign|equation|<macro|body|<with|mode|math|math-display|true|par-sep|0.45fn|<style-with|src-compact|none|<surround|<no-page-break*><vspace*|0.5fn><no-indent><next-equation><htab|5mm>|<htab|5mm><with|mode|text|(<the-equation>)><vspace|0.5fn><no-indent*>|<arg|body>>>>>>
+  <assign|equation-lab|<\macro|body|lab>
+    <\surround|<assign|the-label|<arg|lab>>|<space|5mm><with|mode|text|(<arg|lab>)>>
+      <\equation*>
+        <arg|body>
+      </equation*>
+    </surround>
+  </macro>>
 
-  <assign|equation*|<macro|body|<with|mode|math|math-display|true|par-sep|0.45fn|<style-with|src-compact|none|<surround|<no-page-break*><vspace*|0.5fn><no-indent><htab|0fn>|<htab|0fn><vspace|0.5fn><no-indent*>|<arg|body>>>>>>
+  <assign|equation|<\macro|body>
+    <\surround|<next-equation>|>
+      <\equation-lab>
+        <arg|body>
+      </equation-lab|<the-equation>>
+    </surround>
+  </macro>>
 
   <\active*>
     <\src-comment>
@@ -53,23 +95,49 @@
     </src-comment>
   </active*>
 
-  <assign|eqnarray*|<macro|body|<with|par-mode|center|mode|math|math-display|true|par-sep|0.45fn|<surround|<no-page-break*><vspace*|0.5fn>|<vspace|0.5fn><no-indent*>|<tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|3>|<twith|table-max-cols|3>|<cwith|1|-1|1|1|cell-lsep|0spc>|<cwith|1|-1|-1|-1|cell-rsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0sep>|<cwith|1|-1|1|-1|cell-tsep|0sep>|<cwith|1|-1|1|1|cell-hpart|1>|<cwith|1|-1|-1|-1|cell-hpart|1>|<cwith|1|-1|1|1|cell-halign|r>|<cwith|1|-1|1|1|cell-hyphen|b>|<cwith|1|-1|2|-2|cell-halign|c>|<cwith|1|-1|-1|-1|cell-halign|l>|<cwith|1|-1|-1|-1|cell-hyphen|t>|<arg|body>>>>>>
+  <assign|equations-base|<\macro|body>
+    <\with|mode|math|math-display|true|par-mode|center|par-sep|<eqn-row-sep>>
+      <style-with|src-compact|none|<surround|<no-page-break*><vspace*|<eqn-long-above>>|<vspace|<eqn-long-below>><no-indent*>|<arg|body>>>
+    </with>
+  </macro>>
+
+  <assign|eqnarray*|<\macro|body>
+    <\equations-base>
+      <tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|3>|<twith|table-max-cols|3>|<cwith|1|-1|1|1|cell-lsep|0spc>|<cwith|1|-1|-1|-1|cell-rsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0sep>|<cwith|1|-1|1|-1|cell-tsep|0sep>|<cwith|1|-1|1|1|cell-hpart|1>|<cwith|1|-1|-1|-1|cell-hpart|1>|<cwith|1|-1|1|1|cell-halign|r>|<cwith|1|-1|1|1|cell-hyphen|b>|<cwith|1|-1|2|-2|cell-halign|c>|<cwith|1|-1|-1|-1|cell-halign|l>|<cwith|1|-1|-1|-1|cell-hyphen|t>|<arg|body>>
+    </equations-base>
+  </macro>>
 
   <assign|eqnarray|<value|eqnarray*>>
 
-  <assign|leqnarray*|<macro|body|<with|mode|math|math-display|true|par-sep|0.45fn|<surround|<no-page-break*><vspace*|0.5fn>|<vspace|0.5fn><no-indent*>|<tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|3>|<twith|table-max-cols|3>|<cwith|1|-1|1|1|cell-lsep|0spc>|<cwith|1|-1|-1|-1|cell-rsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0sep>|<cwith|1|-1|1|-1|cell-tsep|0sep>|<cwith|1|-1|-1|-1|cell-hpart|1>|<cwith|1|-1|1|1|cell-halign|r>|<cwith|1|-1|2|-2|cell-halign|c>|<cwith|1|-1|-1|-1|cell-halign|l>|<cwith|1|-1|1|1|cell-width|1.5fn>|<cwith|1|-1|2|2|cell-width|1fn>|<cwith|1|-1|-1|-1|cell-hyphen|t>|<arg|body>>>>>>
+  <assign|leqnarray*|<\macro|body>
+    <\equations-base>
+      <tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|3>|<twith|table-max-cols|3>|<cwith|1|-1|1|1|cell-lsep|0spc>|<cwith|1|-1|-1|-1|cell-rsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0sep>|<cwith|1|-1|1|-1|cell-tsep|0sep>|<cwith|1|-1|-1|-1|cell-hpart|1>|<cwith|1|-1|1|1|cell-halign|r>|<cwith|1|-1|2|-2|cell-halign|c>|<cwith|1|-1|-1|-1|cell-halign|l>|<cwith|1|-1|1|1|cell-width|1.5fn>|<cwith|1|-1|2|2|cell-width|1fn>|<cwith|1|-1|-1|-1|cell-hyphen|t>|<arg|body>>
+    </equations-base>
+  </macro>>
 
   <assign|leqnarray|<value|leqnarray*>>
 
-  <assign|align*|<macro|body|<with|par-mode|center|mode|math|math-display|true|<surround|<no-page-break*><vspace*|0.5fn>|<vspace|0.5fn><no-indent*>|<tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|2>|<twith|table-max-cols|2>|<cwith|1|-1|1|1|cell-lsep|0spc>|<cwith|1|-1|1|1|cell-rsep|0.5spc>|<cwith|1|-1|-1|-1|cell-rsep|0spc>|<cwith|1|-1|-1|-1|cell-lsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0sep>|<cwith|1|-1|1|-1|cell-tsep|0sep>|<cwith|1|-1|1|1|cell-hpart|1>|<cwith|1|-1|-1|-1|cell-hpart|1>|<cwith|1|-1|1|1|cell-halign|r>|<cwith|1|-1|1|1|cell-hyphen|b>|<cwith|1|-1|-1|-1|cell-halign|l>|<cwith|1|-1|-1|-1|cell-hyphen|t>|<arg|body>>>>>>
+  <assign|align*|<\macro|body>
+    <\equations-base>
+      <tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|2>|<twith|table-max-cols|2>|<cwith|1|-1|1|1|cell-lsep|0spc>|<cwith|1|-1|1|1|cell-rsep|0.5spc>|<cwith|1|-1|-1|-1|cell-rsep|0spc>|<cwith|1|-1|-1|-1|cell-lsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0sep>|<cwith|1|-1|1|-1|cell-tsep|0sep>|<cwith|1|-1|1|1|cell-hpart|1>|<cwith|1|-1|-1|-1|cell-hpart|1>|<cwith|1|-1|1|1|cell-halign|r>|<cwith|1|-1|1|1|cell-hyphen|b>|<cwith|1|-1|-1|-1|cell-halign|l>|<cwith|1|-1|-1|-1|cell-hyphen|t>|<arg|body>>
+    </equations-base>
+  </macro>>
 
   <assign|align|<value|align*>>
 
-  <assign|gather*|<macro|body|<with|par-mode|center|mode|math|math-display|true|<surround|<no-page-break*><vspace*|0.5fn>|<vspace|0.5fn><no-indent*>|<tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|1>|<twith|table-max-cols|1>|<cwith|1|-1|1|-1|cell-lsep|0spc>|<cwith|1|-1|1|-1|cell-rsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0spc>|<cwith|1|-1|1|-1|cell-tsep|1spc>|<cwith|1|-1|1|-1|cell-hpart|1>|<cwith|1|-1|1|-1|cell-hyphen|b>|<cwith|1|-1|1|-1|cell-halign|c>|<arg|body>>>>>>
+  <assign|gather*|<\macro|body>
+    <\equations-base>
+      <tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|1>|<twith|table-max-cols|1>|<cwith|1|-1|1|-1|cell-lsep|0spc>|<cwith|1|-1|1|-1|cell-rsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0spc>|<cwith|1|-1|1|-1|cell-tsep|0spc>|<cwith|1|-1|1|-1|cell-hpart|1>|<cwith|1|-1|1|-1|cell-hyphen|b>|<cwith|1|-1|1|-1|cell-halign|c>|<arg|body>>
+    </equations-base>
+  </macro>>
 
   <assign|gather|<value|gather*>>
 
-  <assign|eqsplit*|<macro|body|<with|par-mode|center|mode|math|math-display|true|<surround|<no-page-break*><vspace*|0.5fn>|<vspace|0.5fn><no-indent*>|<tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|1>|<twith|table-max-cols|1>|<cwith|1|-1|1|-1|cell-lsep|0spc>|<cwith|1|-1|1|-1|cell-rsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0spc>|<cwith|1|-1|1|-1|cell-tsep|1spc>|<cwith|1|-1|1|-1|cell-hpart|1>|<cwith|1|-1|1|-1|cell-hyphen|b>|<cwith|-1|-1|1|1|cell-halign|r>|<cwith|2|-2|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-halign|l>|<arg|body>>>>>>
+  <assign|eqsplit*|<\macro|body>
+    <\equations-base>
+      <tformat|<twith|table-hyphen|y>|<twith|table-width|1par>|<twith|table-min-cols|1>|<twith|table-max-cols|1>|<cwith|1|-1|1|-1|cell-lsep|0spc>|<cwith|1|-1|1|-1|cell-rsep|0spc>|<cwith|1|-1|1|-1|cell-bsep|0spc>|<cwith|1|-1|1|-1|cell-tsep|0spc>|<cwith|1|-1|1|-1|cell-hpart|1>|<cwith|1|-1|1|-1|cell-hyphen|b>|<cwith|-1|-1|1|1|cell-halign|r>|<cwith|2|-2|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-halign|l>|<arg|body>>
+    </equations-base>
+  </macro>>
 
   <assign|eqsplit|<value|eqsplit*>>
 
@@ -78,18 +146,6 @@
 
 <\initial>
   <\collection>
-    <associate|page-bot|30mm>
-    <associate|page-even|30mm>
-    <associate|page-odd|30mm>
-    <associate|page-reduce-bot|15mm>
-    <associate|page-reduce-left|25mm>
-    <associate|page-reduce-right|25mm>
-    <associate|page-reduce-top|15mm>
-    <associate|page-right|30mm>
-    <associate|page-top|30mm>
-    <associate|page-type|a4>
-    <associate|par-width|150mm>
     <associate|preamble|true>
-    <associate|sfactor|4>
   </collection>
 </initial>
