e25320aea6b5a6b9250689286ca606e691b8a642
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2004-08-05 21:15:07 +0000

    Separate title and header packages.

--- TeXmacs/styles/seminar.ts
+++ TeXmacs/styles/seminar.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.0.3.7>
+<TeXmacs|1.0.4>
 
 <style|source>
 
@@ -25,7 +25,7 @@
     </src-title>
   </active*>
 
-  <use-package|std|env|header-seminar|section-seminar>
+  <use-package|std|env|title-seminar|header-seminar|section-seminar>
 
   \;
 
@@ -60,18 +60,6 @@
 
 <\initial>
   <\collection>
-    <associate|page-bot|30mm>
-    <associate|page-even|30mm>
-    <associate|page-odd|30mm>
-    <associate|page-reduce-bot|15mm>
-    <associate|page-reduce-left|25mm>
-    <associate|page-reduce-right|25mm>
-    <associate|page-reduce-top|15mm>
-    <associate|page-right|30mm>
-    <associate|page-top|30mm>
-    <associate|page-type|a4>
-    <associate|par-width|150mm>
     <associate|preamble|true>
-    <associate|sfactor|4>
   </collection>
 </initial>
