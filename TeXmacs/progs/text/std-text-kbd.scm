718f8a634cda0e3d5ac211437620f82e3a5c3e87
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2005-05-07 10:45:12 +0000

    Add forgotten file

---
+++ TeXmacs/progs/text/std-text-kbd.scm
@@ -0,0 +1,47 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : std-text-kbd.scm
+;; DESCRIPTION : enter semantic textual markup using the keyboard
+;; COPYRIGHT   : (C) 1999  Joris van der Hoeven
+;;
+;; This software falls under the GNU general public license and comes WITHOUT
+;; ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for details.
+;; If you don't have this file, write to the Free Software Foundation, Inc.,
+;; 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(texmacs-module (text std-text-kbd)
+  (:use (text std-text-edit)))
+
+(kbd-map in-std-text?
+  ("text" "" "Insert structural markup")
+
+  ("$" (if (inside? 'hybrid) (insert "$") (make-with "mode" "math")))
+  ("text $" (make 'equation*) (temp-proof-fix))
+  ("text &" (make 'eqnarray*) (temp-proof-fix))
+  ("text *" (toggle-section-number))
+
+  ("text a" (make 'abbr))
+  ("text d" (make-tmlist 'description))
+  ("text e" (make-tmlist 'enumerate))
+  ("text i" (make-tmlist 'itemize))
+  ("text m" (make 'em))
+  ("text n" (make 'name))
+  ("text p" (make 'samp))
+  ("text s" (make 'strong))
+  ("text v" (make 'verbatim))
+  ("text ;" (make-item))
+  ("text 0" (make-section 'chapter))
+  ("text 1" (make-section 'section))
+  ("text 2" (make-section 'subsection))
+  ("text 3" (make-section 'subsubsection))
+  ("text 4" (make-section 'paragraph))
+  ("text 5" (make-section 'subparagraph))
+
+  ("F5" (make 'em))
+  ("F6" (make 'strong))
+  ("F7" (make 'verbatim))
+  ("F8" (make 'samp))
+  ("S-F6" (make 'name)))
