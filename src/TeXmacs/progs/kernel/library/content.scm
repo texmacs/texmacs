5ff3c2f981c1f177317951f2625589c8517693eb
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2005-06-20 14:45:16 +0000

    Fix a bug in tree-set-diff and further improvements

--- src/TeXmacs/progs/kernel/library/content.scm
+++ src/TeXmacs/progs/kernel/library/content.scm
@@ -40,10 +40,22 @@
 	      (tm-equal? (cdr x) (cdr y))))
 	(else (== x y))))
 
+(define-public (tm-length x)
+  (cond ((string? x) (string-length x))
+	((list? x) (- (length x) 1))
+	((tree-atomic? x) (string-length (tree->string x)))
+	(else (tree-arity x))))
+
 (define-public (tm-arity x)
   (cond ((list? x) (- (length x) 1))
 	((string? x) 0)
 	(else (tree-arity x))))
+
+(define-public (tm->string x)
+  (if (string? x) x (tree->string x)))
+
+(define-public (tm->list x)
+  (if (list? x) x (tree->list x)))
 
 (define-public (tm-car x)
   (car (if (pair? x) x (tree->list x))))
@@ -51,8 +63,11 @@
 (define-public (tm-cdr x)
   (cdr (if (pair? x) x (tree->list x))))
 
-(define-public (tm->list x)
-  (if (list? x) x (tree->list x)))
+(define-public (tm-range x from to)
+  (cond ((string? x) (substring x from to))
+	((list? x) (cons (car x) (sublist (cdr x) from to)))
+	((tree-atomic? x) (substring (tm->string x) from to))
+	(else (cons (tm-car x) (sublist (tm-cdr x) from to)))))
 
 (define-public (tm-func? x . args)
   (or (and (list? x) (apply func? (cons x args)))
