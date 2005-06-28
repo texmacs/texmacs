3cf4aa088ddb0cf85aa738d2b4fdfea05c6f8553
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2005-06-28 15:28:05 +0000

    Adapted configure.in for Imlib2 plug-in

--- src/src/Plugins/Imlib2/imlib2.hpp
+++ src/src/Plugins/Imlib2/imlib2.hpp
@@ -2,7 +2,7 @@
 /******************************************************************************
 * MODULE     : imlib2.hpp
 * DESCRIPTION: interface with Imlib2
-* COPYRIGHT  : (C) 1999  Joris van der Hoeven
+* COPYRIGHT  : (C) 2005  Joris van der Hoeven
 *******************************************************************************
 * This software falls under the GNU general public license and comes WITHOUT
 * ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for more details.
@@ -14,12 +14,18 @@
 #define IMLIB2_H
 #include "url.hpp"
 #include <X11/Xlib.h>
-#include <X11/Xatom.h>
 
-bool supports_imlib2 ();
+bool imlib2_present ();
 bool imlib2_supports (url u);
 void imlib2_image_size (url u, int& w, int& h);
 void imlib2_display (Display* dpy, Pixmap pm, url image, int w, int h,
 		     double cx1, double cy1, double cx2, double cy2);
+
+#ifdef USE_IMLIB2
+#include <Imlib2.h>
+#include <stdio.h>
+#include <string.h>
+
+#endif // USE_IMLIB2
 
 #endif // IMLIB2_H
