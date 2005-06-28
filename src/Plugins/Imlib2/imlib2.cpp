9087ccd6c155e2c4f01cb93fe3bcaf0a6dba5911
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2005-06-28 16:12:52 +0000

    Support for dynamically linked Imlib2 library

--- src/Plugins/Imlib2/imlib2.cpp
+++ src/Plugins/Imlib2/imlib2.cpp
@@ -15,17 +15,85 @@
 
 #ifdef USE_IMLIB2
 
+/******************************************************************************
+* Routines used from Imlib2
+******************************************************************************/
+
+Imlib_Image (*IMLIB2_load_image) (const char *file);
+int (*IMLIB2_image_get_width) (void);
+int (*IMLIB2_image_get_height) (void);
+void (*IMLIB2_context_set_image) (Imlib_Image image);
+void (*IMLIB2_render_image_part_on_drawable_at_size)
+  (int source_x, int source_y, int source_width, int source_height,
+   int x, int y, int width, int height);
+void (*IMLIB2_free_image) (void);
+void (*IMLIB2_context_set_display) (Display * display);
+void (*IMLIB2_context_set_visual) (Visual * visual);
+void (*IMLIB2_context_set_colormap) (Colormap colormap);
+void (*IMLIB2_context_set_drawable) (Drawable drawable);
+
+/******************************************************************************
+* Initialization
+******************************************************************************/
+
+static bool imlib2_initialized= false;
+static bool imlib2_error      = false;
+
+#ifdef LINKED_IMLIB2
+#define imlib2_bind(orig,tm) \
+  tm= orig;
+#else
+#define imlib2_bind(orig,tm) \
+  (void) symbol_install ("libImlib2.so", #orig, (pointer&) tm); \
+  if (tm == NULL) return;
+#endif
+
+void
+imlib2_initialize () {
+  imlib2_initialized= true;
+  imlib2_error      = true;
+
+  int status= debug_off ();
+  imlib2_bind (imlib_load_image, IMLIB2_load_image);
+  imlib2_bind (imlib_image_get_width, IMLIB2_image_get_width);
+  imlib2_bind (imlib_image_get_height, IMLIB2_image_get_height);
+  imlib2_bind (imlib_context_set_image, IMLIB2_context_set_image);
+  imlib2_bind (imlib_render_image_part_on_drawable_at_size,
+	       IMLIB2_render_image_part_on_drawable_at_size);
+  imlib2_bind (imlib_free_image, IMLIB2_free_image);
+  imlib2_bind (imlib_context_set_display, IMLIB2_context_set_display);
+  imlib2_bind (imlib_context_set_visual, IMLIB2_context_set_visual);
+  imlib2_bind (imlib_context_set_colormap, IMLIB2_context_set_colormap);
+  imlib2_bind (imlib_context_set_drawable, IMLIB2_context_set_drawable);
+  debug_on (status);
+
+#ifdef LINKED_IMLIB2
+  if (DEBUG_AUTO) cout << "TeXmacs] With linked Imlib2 support\n";
+#else
+  if (DEBUG_AUTO) cout << "TeXmacs] Installed Imlib2 support\n";
+#endif
+
+  imlib2_error= false;
+}
+
+/******************************************************************************
+* Functionality provided by the plug-in
+******************************************************************************/
+
 hashmap<tree,tree> imlib2_size_cache ("");
 
 bool
 imlib2_present () {
-  return true;
+  if (!imlib2_initialized)
+    imlib2_initialize ();
+  return !imlib2_error;
 }
 
 bool
 imlib2_supports (url u) {
   string s= suffix (u);
-  return s != "ps" && s != "eps" && s != "pdf" && s != "xpm";
+  if (s == "ps" || s == "eps" || s == "pdf" || s == "xpm") return false;
+  return imlib2_present ();
 }
 
 static Imlib_Image
@@ -35,7 +103,7 @@ imlib2_load_image (url u) {
     name= resolve ("$TEXMACS_PIXMAP_PATH/TeXmacs-gnu.xpm");
   char *_name= as_charp (as_string (name));
   Imlib_Image image;
-  image = imlib_load_image (_name);
+  image = IMLIB2_load_image (_name);
   delete[] _name;
   return image;
 }
@@ -49,10 +117,10 @@ imlib2_image_size (url u, int& w, int& h) {
   else {
     Imlib_Image image= imlib2_load_image (u);
     if (image) {
-      imlib_context_set_image (image);
-      w= imlib_image_get_width ();
-      h= imlib_image_get_height ();
-      imlib_free_image ();
+      IMLIB2_context_set_image (image);
+      w= IMLIB2_image_get_width ();
+      h= IMLIB2_image_get_height ();
+      IMLIB2_free_image ();
       imlib2_size_cache (u->t)= tuple (as_string (w), as_string (h));
     }
   }
@@ -66,27 +134,31 @@ imlib2_display (Display* dpy, Pixmap pm, url u, SI w, SI h,
   if (image) {
     Visual *vis= DefaultVisual (dpy, DefaultScreen (dpy));
     Colormap cm= DefaultColormap (dpy, DefaultScreen (dpy));
-    imlib_context_set_display (dpy);
-    imlib_context_set_visual (vis);
-    imlib_context_set_colormap (cm);
-    imlib_context_set_drawable (pm);
+    IMLIB2_context_set_display (dpy);
+    IMLIB2_context_set_visual (vis);
+    IMLIB2_context_set_colormap (cm);
+    IMLIB2_context_set_drawable (pm);
 
-    imlib_context_set_image (image);
-    int iw= imlib_image_get_width ();
-    int ih= imlib_image_get_height ();
+    IMLIB2_context_set_image (image);
+    int iw= IMLIB2_image_get_width ();
+    int ih= IMLIB2_image_get_height ();
     int x1= (int) (cx1 * iw + 0.5);
     int y1= (int) (cy1 * ih + 0.5);
     int x2= (int) (cx2 * iw + 0.5);
     int y2= (int) (cy2 * ih + 0.5);
     int ww= x2 - x1;
     int hh= y2 - y1;
-    imlib_render_image_part_on_drawable_at_size
+    IMLIB2_render_image_part_on_drawable_at_size
       (x1, hh-y2, ww, hh, 0, 0, w, h);
-    imlib_free_image ();
+    IMLIB2_free_image ();
   }
 }
 
 #else // USE_IMLIB2
+
+/******************************************************************************
+* If Imlib2 is not present...
+******************************************************************************/
 
 bool imlib2_present () { return false; }
 bool imlib2_supports (url u) { (void) u; return false; }
