9388170592fe1bd8f3ed5202460837f59239e045
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2005-10-19 17:43:42 +0000

    Fix exportation of document preambles to LaTeX

--- src/src/Data/Convert/Tex/fromtex.cpp
+++ src/src/Data/Convert/Tex/fromtex.cpp
@@ -557,6 +557,8 @@ latex_command_to_tree (tree t) {
   if (is_tuple (t, "\\text", 1) ||
       is_tuple (t, "\\mbox", 1) || is_tuple (t, "\\hbox", 1))
     return var_m2e (t, MODE, "text");
+  if (is_tuple (t, "\\ensuremath", 1))
+    return var_m2e (t, MODE, "math");
   if (is_tuple (t, "\\Mvariable", 1))
     return compound ("Mvariable", var_m2e (t, MODE, "text"));
   if (is_tuple (t, "\\Mfunction", 1))
