01632751b2ee627a3c3548bc4e3c71af1b70735c
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2005-10-24 13:21:05 +0000

    Add copyright notices

--- src/plugins/lisp/scl/scl-init.lisp
+++ src/plugins/lisp/scl/scl-init.lisp
@@ -1,3 +1,15 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MODULE      : scl-init.lisp
+;; DESCRIPTION : Initialize scl plugin
+;; COPYRIGHT   : (C) 2005 Douglas Crosher
+;;
+;; This software falls under the GNU general public license and comes WITHOUT
+;; ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for details.
+;; If you don't have this file, write to the Free Software Foundation, Inc.,
+;; 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setf tm-data-begin "")
 (setf tm-data-end "")
 
