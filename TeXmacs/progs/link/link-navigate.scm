9968db73b946ca28be53cafb1310041464f410e8
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2006-06-06 16:26:39 +0000

    Filter links which can be followed during navigation

--- TeXmacs/progs/link/link-navigate.scm
+++ TeXmacs/progs/link/link-navigate.scm
@@ -16,6 +16,52 @@
   (:use (link link-edit) (link link-extern)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Navigation mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define navigation-bidirectional-links? #t)
+(define navigation-external-links? #t)
+(define navigation-blocked-types (make-ahash-table))
+
+(define (navigation-bidirectional?) navigation-bidirectional-links?)
+(tm-define (navigation-toggle-bidirectional)
+  (:synopsis "Toggle whether we may follow links in both directions.")
+  (:check-mark "v" navigation-bidirectional?)
+  (toggle! navigation-bidirectional-links?))
+
+(define (navigation-external?) navigation-external-links?)
+(tm-define (navigation-toggle-external)
+  (:synopsis "Toggle whether we may follow links defined in other loci.")
+  (:check-mark "v" navigation-external?)
+  (toggle! navigation-external-links?))
+
+(define (navigation-allow-type? type)
+  (not (ahash-ref navigation-blocked-types type)))
+(tm-define (navigation-toggle-type type)
+  (:synopsis "Toggle whether we may follow links of a given @type.")
+  (:check-mark "v" navigation-allow-type?)
+  (ahash-set! navigation-blocked-types type
+	      (not (ahash-ref navigation-blocked-types type))))
+
+(define (navigation-allow-no-types?)
+  (with l (ahash-table->list navigation-blocked-types)
+    (null? (list-difference (current-link-types)
+			    (map car (list-filter l cdr))))))
+(tm-define (navigation-allow-no-types)
+  (:synopsis "Disallow any link types from being followed.")
+  (:check-mark "v" navigation-allow-no-types?)
+  (for-each (cut ahash-set! navigation-blocked-types <> #t)
+	    (current-link-types)))
+
+(define (navigation-allow-all-types?)
+  (with l (ahash-table->list navigation-blocked-types)
+    (null? (list-filter l cdr))))
+(tm-define (navigation-allow-all-types)
+  (:synopsis "Allow all link types to be followed.")
+  (:check-mark "v" navigation-allow-all-types?)
+  (set! navigation-blocked-types (make-ahash-table)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Finding links
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -24,24 +70,49 @@
 	 (sts (map tree->stree lns)))
     (map (cut cons id <>) (map cdr sts))))
 
-(tm-define (exact-link-list t)
+(define (exact-link-list-global t)
   (let* ((ids (tree->ids t))
 	 (lns (map id->link-list ids)))
     (apply append lns)))
 
-(define (upward-link-list-sub t)
-  (with l (exact-link-list t)
+(define (exact-link-list-local t)
+  (if (not (tm-func? t 'locus)) '()
+      (let* ((id (locus-id t))
+	     (ch (cDdr (tree->stree t)))
+	     (lns (list-filter ch (cut func? <> 'link))))
+	(map (cut cons id <>) (map cdr lns)))))
+
+(define (filter-on-bidirectional x)
+  (with (id type first . other) x
+    (== id (Id->id first))))
+
+(define (filter-on-type x)
+  (with (id type . args) x
+    (navigation-allow-type? type)))
+
+(tm-define (exact-link-list t filter?)
+  (with l (if (and filter? (not (navigation-external?)))
+	      (exact-link-list-local t)
+	      (exact-link-list-global t))
+    (if (and filter? (not (navigation-bidirectional?)))
+	(set! l (list-filter l filter-on-bidirectional)))
+    (if filter?
+	(set! l (list-filter l filter-on-type)))
+    l))
+
+(define (upward-link-list-sub t filter?)
+  (with l (exact-link-list t filter?)
     (if (== (buffer-path) (tree->path t)) l
-	(append l (upward-link-list-sub (tree-up t))))))
+	(append l (upward-link-list-sub (tree-up t) filter?)))))
 
-(tm-define (upward-link-list)
+(tm-define (upward-link-list filter?)
   (with t (cursor-tree)
-    (upward-link-list-sub t)))
+    (upward-link-list-sub t filter?)))
 
-(tm-define (complete-link-list t)
-  (with l (exact-link-list t)
+(tm-define (complete-link-list t filter?)
+  (with l (exact-link-list t filter?)
     (if (tree-atomic? t) l
-	(with ls (map complete-link-list (tree-children t))
+	(with ls (map (cut complete-link-list <> filter?) (tree-children t))
 	  (apply append (cons l ls))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -66,7 +137,7 @@
 	(list-remove-duplicates (append h r)))))
 
 (tm-define (upward-navigate-list)
-  (link-list->navigate-list (upward-link-list)))
+  (link-list->navigate-list (upward-link-list #t)))
 
 (tm-define (navigate-list-types l)
   (list-remove-duplicates (map car l)))
@@ -104,7 +175,7 @@
 
 (tm-define (link-follow)
   (:synopsis "Follow one of the links at the current locus.")
-  (let* ((l (link-list->navigate-list (upward-link-list)))
+  (let* ((l (link-list->navigate-list (upward-link-list #t)))
 	 (types (navigate-list-types l)))
     (cond ((null? types) (noop))
 	  ((null? (cdr types)) (go-to-Id (cadddr (car l))))
