b05f768b85fa85e5554d2425b9e1ac6a4d455e47
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2007-01-15 17:59:10 +0000

    Improvements to the GUI styles

--- src/TeXmacs/packages/gui/gui-utils.ts
+++ src/TeXmacs/packages/gui/gui-utils.ts
@@ -33,17 +33,35 @@
 
   <assign|par-line-sep|0em>
 
+  <assign|page-screen-left|0.5em>
+
+  <assign|page-screen-right|0.5em>
+
+  <assign|page-screen-top|0.5em>
+
+  <assign|page-screen-bottom|0.5em>
+
   <\active*>
     <\src-comment>
       Gui environment variables
     </src-comment>
   </active*>
 
-  <assign|gui-small-sep|3px>
+  <assign|gui-small-sep|0.25em>
 
-  <assign|gui-med-sep|6px>
+  <assign|gui-med-sep|0.5em>
 
-  <assign|gui-big-sep|12px>
+  <assign|gui-big-sep|1em>
+
+  <assign|gui-up-color|light grey>
+
+  <assign|gui-down-color|#e0e0e0>
+
+  <assign|gui-input-color|#f0f0f0>
+
+  <assign|gui-sun-color|white>
+
+  <assign|gui-shadow-color|grey>
 
   <\active*>
     <\src-comment>
@@ -63,7 +81,7 @@
     </src-comment>
   </active*>
 
-  <assign|gui-small-framed|<macro|main-col|border1-col|border2-col|pad|body|<gui-dense-table|<tformat|<twith|table-hmode|auto>|<cwith|1|1|1|-1|cell-background|<arg|border1-col>>|<cwith|3|3|1|-1|cell-background|<arg|border2-col>>|<cwith|1|-1|3|3|cell-background|<arg|border2-col>>|<cwith|1|-1|1|1|cell-background|<arg|border1-col>>|<cwith|1|-1|1|1|cell-width|1px>|<cwith|1|-1|1|1|cell-hmode|exact>|<cwith|1|-1|3|3|cell-width|1px>|<cwith|1|-1|3|3|cell-hmode|exact>|<cwith|1|1|1|-1|cell-height|1px>|<cwith|1|1|1|-1|cell-vmode|exact>|<cwith|3|3|1|-1|cell-height|1px>|<cwith|3|3|1|-1|cell-vmode|exact>|<cwith|2|2|2|2|cell-lsep|<arg|pad>>|<cwith|2|2|2|2|cell-rsep|<arg|pad>>|<cwith|2|2|2|2|cell-bsep|<arg|pad>>|<cwith|2|2|2|2|cell-tsep|<arg|pad>>|<table|<row|<cell|>|<cell|>|<cell|>>|<row|<cell|>|<cell|<arg|body>>|<cell|>>|<row|<cell|>|<cell|>|<cell|>>>>>>>
+  <assign|gui-small-framed|<macro|main-col|border1-col|border2-col|pad|body|<gui-dense-table|<tformat|<twith|table-hmode|auto>|<cwith|1|1|1|-1|cell-background|<arg|border1-col>>|<cwith|3|3|1|-1|cell-background|<arg|border2-col>>|<cwith|1|-1|3|3|cell-background|<arg|border2-col>>|<cwith|1|-1|1|1|cell-background|<arg|border1-col>>|<cwith|1|-1|1|1|cell-width|1px>|<cwith|1|-1|1|1|cell-hmode|exact>|<cwith|1|-1|3|3|cell-width|1px>|<cwith|1|-1|3|3|cell-hmode|exact>|<cwith|1|1|1|-1|cell-height|1px>|<cwith|1|1|1|-1|cell-vmode|exact>|<cwith|3|3|1|-1|cell-height|1px>|<cwith|3|3|1|-1|cell-vmode|exact>|<cwith|2|2|2|2|cell-lsep|<arg|pad>>|<cwith|2|2|2|2|cell-rsep|<arg|pad>>|<cwith|2|2|2|2|cell-bsep|<arg|pad>>|<cwith|2|2|2|2|cell-tsep|<arg|pad>>|<cwith|2|2|2|2|cell-vcorrect|a>|<cwith|2|2|2|2|cell-hyphen|n>|<cwith|2|2|2|2|cell-hpart|1>|<table|<row|<cell|>|<cell|>|<cell|>>|<row|<cell|>|<cell|<arg|body>>|<cell|>>|<row|<cell|>|<cell|>|<cell|>>>>>>>
 
   <assign|gui-line-framed|<macro|main-col|border1-col|border2-col|pad|body|<gui-dense-table|<tformat|<cwith|1|1|1|-1|cell-background|<arg|border1-col>>|<cwith|3|3|1|-1|cell-background|<arg|border2-col>>|<cwith|1|-1|1|1|cell-background|<arg|border1-col>>|<cwith|1|-1|1|1|cell-width|1px>|<cwith|1|-1|1|1|cell-hmode|exact>|<cwith|1|1|1|-1|cell-height|1px>|<cwith|1|1|1|-1|cell-vmode|exact>|<cwith|3|3|1|-1|cell-height|1px>|<cwith|3|3|1|-1|cell-vmode|exact>|<cwith|2|2|2|2|cell-background|<arg|main-col>>|<cwith|2|2|2|2|cell-lsep|<arg|pad>>|<cwith|2|2|2|2|cell-rsep|<arg|pad>>|<cwith|2|2|2|2|cell-bsep|<arg|pad>>|<cwith|2|2|2|2|cell-tsep|<arg|pad>>|<cwith|2|2|2|2|cell-vcorrect|a>|<cwith|2|2|2|2|cell-hyphen|n>|<cwith|2|2|2|2|cell-hpart|1>|<table|<row|<cell|>|<cell|>|<cell|>>|<row|<cell|>|<cell|<arg|body>>|<cell|>>|<row|<cell|>|<cell|>|<cell|>>>>>>>
 
@@ -71,19 +89,73 @@
     <arg|body>
   </cell>|<cell|>>|<row|<cell|>|<cell|>|<cell|>>>>>>>
 
+  \;
+
+  <assign|gui-big-raise|<style-with|src-compact|none|<macro|body|<gui-big-framed|<value|gui-up-color>|<value|gui-sun-color>|<value|gui-shadow-color>|<value|gui-med-sep>|<arg|body>>>>>
+
+  <assign|gui-big-lower|<style-with|src-compact|none|<macro|body|<gui-big-framed|<value|gui-down-color>|<value|gui-shadow-color>|<value|gui-sun-color>|<value|gui-med-sep>|<arg|body>>>>>
+
+  <assign|gui-big-input|<style-with|src-compact|none|<macro|body|<gui-big-framed|<value|gui-input-color>|<value|gui-shadow-color>|<value|gui-sun-color>|<value|gui-med-sep>|<arg|body>>>>>
+
+  <assign|gui-small-raise|<style-with|src-compact|none|<macro|body|<gui-small-framed|<value|gui-up-color>|<value|gui-sun-color>|<value|gui-shadow-color>|<value|gui-small-sep>|<arg|body>>>>>
+
+  <assign|gui-small-lower|<style-with|src-compact|none|<macro|body|<gui-small-framed|<value|gui-down-color>|<value|gui-shadow-color>|<value|gui-sun-color>|<value|gui-small-sep>|<arg|body>>>>>
+
+  <assign|gui-small-input|<style-with|src-compact|none|<macro|body|<gui-small-framed|<value|gui-input-color>|<value|gui-shadow-color>|<value|gui-sun-color>|<value|gui-small-sep>|<arg|body>>>>>
+
+  <assign|gui-line-raise|<style-with|src-compact|none|<macro|body|<gui-line-framed|<value|gui-up-color>|<value|gui-sun-color>|<value|gui-shadow-color>|<value|gui-small-sep>|<arg|body>>>>>
+
+  <assign|gui-line-lower|<style-with|src-compact|none|<macro|body|<gui-line-framed|<value|gui-down-color>|<value|gui-shadow-color>|<value|gui-sun-color>|<value|gui-small-sep>|<arg|body>>>>>
+
+  <assign|gui-line-input|<style-with|src-compact|none|<macro|body|<gui-line-framed|<value|gui-input-color>|<value|gui-shadow-color>|<value|gui-sun-color>|<value|gui-small-sep>|<arg|body>>>>>
+
   <\active*>
     <\src-comment>
-      Framed boxes
+      Switches
     </src-comment>
   </active*>
 
-  <assign|gui-big-lower|<macro|body|<gui-big-framed|#e0e0e0|grey|#f0f0f0|<value|gui-med-sep>|<arg|body>>>>
+  <assign|gui-centered-bar|<macro|l|c|r|<gui-dense-table|<tformat|<cwith|1|-1|1|-1|cell-hyphen|n>|<cwith|1|1|1|1|cell-hpart|1>|<cwith|1|1|3|3|cell-hpart|1>|<cwith|1|1|3|3|cell-halign|r>|<cwith|1|1|2|2|cell-halign|c>|<cwith|1|1|1|1|cell-halign|l>|<cwith|1|1|1|1|cell-hyphen|t>|<cwith|1|1|3|3|cell-hyphen|t>|<cwith|1|1|2|2|cell-width|>|<cwith|1|1|2|2|cell-hmode|auto>|<table|<row|<\cell>
+    <arg|l>
+  </cell>|<cell|<arg|c>>|<\cell>
+    <arg|r>
+  </cell>>>>>>>
 
-  <assign|gui-small-raise|<macro|body|<gui-small-framed|light
-  grey|#f0f0f0|grey|<value|gui-small-sep>|<arg|body>>>>
+  <assign|gui-centered-switch|<macro|body|<style-with|src-compact|none|<gui-centered-bar|<gui-hrule-sep|0px|1px|0px|5px|<value|gui-down-color>>|<arg|body>|<gui-hrule-sep|0px|0px|1px|5px|<value|gui-down-color>>>>>>
 
-  <assign|gui-line-input|<macro|body|<gui-line-framed|#f0f0f0|light
-  grey|white|<value|gui-small-sep>|<arg|body>>>>
+  <\active*>
+    <\src-comment>
+      Vertical separators
+    </src-comment>
+  </active*>
+
+  <assign|gui-hrule-sep|<macro|around|left|right|height|col|<gui-dense-table|<tformat|<cwith|1|-1|1|-1|cell-vmode|exact>|<cwith|1|1|1|-1|cell-height|<arg|around>>|<cwith|2|2|1|-1|cell-height|1px>|<cwith|3|3|1|-1|cell-height|<arg|height>>|<cwith|4|4|1|-1|cell-height|1px>|<cwith|5|5|1|-1|cell-height|<arg|around>>|<cwith|2|2|1|-1|cell-background|<value|gui-shadow-color>>|<cwith|4|4|1|-1|cell-background|<value|gui-sun-color>>|<cwith|3|3|1|1|cell-background|<value|gui-shadow-color>>|<cwith|3|3|2|2|cell-background|<arg|col>>|<cwith|3|3|3|3|cell-background|<value|gui-sun-color>>|<cwith|1|-1|1|1|cell-width|<arg|left>>|<cwith|1|-1|1|1|cell-hmode|exact>|<cwith|1|-1|3|3|cell-width|<arg|right>>|<cwith|1|-1|3|3|cell-hmode|exact>|<cwith|1|-1|2|2|cell-hpart|1>|<table|<row|<cell|>|<cell|>|<cell|>>|<row|<cell|>|<cell|>|<cell|>>|<row|<cell|>|<cell|>|<cell|>>|<row|<cell|>|<cell|>|<cell|>>|<row|<cell|>|<cell|>|<cell|>>>>>>>
+
+  <assign|gui-vspace-sep|<macro|height|<gui-dense-table|<tformat|<cwith|1|-1|1|1|cell-vmode|exact>|<cwith|1|1|1|1|cell-height|<arg|height>>|<table|<row|<cell|>>>>>>>
+
+  <assign|gui-hrule|<gui-hrule-sep|<value|gui-med-sep>|0px|0px|0px|<value|gui-down-color>>>
+
+  <assign|gui-vspace|<gui-vspace-sep|<value|gui-med-sep>>>
+
+  <\active*>
+    <\src-comment>
+      Buttons
+    </src-comment>
+  </active*>
+
+  <assign|magnify|<macro|factor|body|<with|magnification|<times|<value|magnification>|<arg|factor>>|<arg|body>>>>
+
+  <assign|gui-centered-cell|<macro|body|<tabular|<tformat|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-valign|c>|<cwith|1|1|1|1|cell-width|1em>|<cwith|1|1|1|1|cell-hmode|exact>|<cwith|1|1|1|1|cell-height|1em>|<cwith|1|1|1|1|cell-vmode|exact>|<cwith|1|1|1|1|cell-vcorrect|n>|<cwith|1|1|1|1|cell-lsep|0em>|<cwith|1|1|1|1|cell-rsep|0em>|<cwith|1|1|1|1|cell-bsep|0em>|<cwith|1|1|1|1|cell-tsep|0em>|<table|<row|<cell|<arg|body>>>>>>>>
+
+  <assign|gui-circ-button|<macro|col|body|<style-with|src-compact|none|<superpose|<gui-centered-cell|<magnify|2.5|<style-with|src-compact|none|<move|<with|bg-color|<value|color>|<with|mode|math|color|<arg|col>|\<bullet\>>>|0.02em|-0.055em>>>>|<gui-centered-cell|<magnify|1.1|<with|mode|math|\<bigcirc\>>>>|<gui-centered-cell|<arg|body>>>>>>
+
+  <assign|gui-square-button|<macro|col|body|<tabular|<tformat|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-valign|c>|<cwith|1|1|1|1|cell-width|0.9em>|<cwith|1|1|1|1|cell-hmode|exact>|<cwith|1|1|1|1|cell-height|0.9em>|<cwith|1|1|1|1|cell-vmode|exact>|<cwith|1|1|1|1|cell-vcorrect|n>|<cwith|1|1|1|1|cell-lsep|0em>|<cwith|1|1|1|1|cell-rsep|0em>|<cwith|1|1|1|1|cell-bsep|0em>|<cwith|1|1|1|1|cell-tsep|0em>|<cwith|1|1|1|1|cell-lborder|1ln>|<cwith|1|1|1|1|cell-rborder|1ln>|<cwith|1|1|1|1|cell-bborder|1ln>|<cwith|1|1|1|1|cell-tborder|1ln>|<cwith|1|1|1|1|cell-background|<arg|col>>|<table|<row|<cell|<arg|body>>>>>>>>
+
+  <assign|gui-toggle-bullet|<macro|on|<style-with|src-compact|none|<if|<equal|<arg|on>|true>|<magnify|1.5|<move|<with|mode|math|\<bullet\>>|0.02em|-0.055em>>|>>>>
+
+  <assign|gui-toggle-checked|<macro|on|<style-with|src-compact|none|<if|<equal|<arg|on>|true>|<magnify|1.25|<move|<with|mode|math|\<checked\>>|0.2em|0.2em>>|>>>>
+
+  <assign|gui-button-bar|<macro|body|<tformat|<twith|table-valign|C>|<cwith|1|-1|1|-1|cell-vcorrect|n>|<cwith|1|-1|1|-1|cell-lsep|0px>|<cwith|1|-1|1|-1|cell-rsep|0px>|<cwith|-1|1|1|-1|cell-tsep|0px>|<cwith|1|-1|1|-1|cell-bsep|0px>|<arg|body>>>>
 
   \;
 </body>
@@ -92,6 +164,7 @@
   <\collection>
     <associate|language|english>
     <associate|preamble|true>
+    <associate|sfactor|5>
   </collection>
 </initial>
 
