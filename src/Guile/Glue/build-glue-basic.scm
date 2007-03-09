4b3a2500a808d41b8b02fb91d1681b52dd8a9a27
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2007-03-09 14:26:40 +0000

    Remove dangerous operation

--- src/Guile/Glue/build-glue-basic.scm
+++ src/Guile/Glue/build-glue-basic.scm
@@ -53,7 +53,6 @@
   (tree-children A (array_tree tree))
   (tree-arity N (int tree))
   (tree-child tree_ref (tree tree int))
-  (tree-set-child! tree_set (void tree int tree))
   (tree-ip obtain_ip (path tree))
   (tree-eq? strong_equal (bool tree tree))
   (subtree subtree (tree tree path))
