cc56ae06b964a3ab30b7cbc216f7e8eb0fdd7a48
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2008-10-07 19:17:03 +0000

    Fix mouse moving

--- src/Plugins/Qt/QTMWidget.hpp
+++ src/Plugins/Qt/QTMWidget.hpp
@@ -1,13 +1,14 @@
+
 /******************************************************************************
- * MODULE     : QTMWidget.hpp
- * DESCRIPTION: QT Texmacs widget class
- * COPYRIGHT  : (C) 2008 Massimiliano Gubinelli
- *******************************************************************************
- * This software falls under the GNU general public license and comes WITHOUT
- * ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for more details.
- * If you don't have this file, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- ******************************************************************************/
+* MODULE     : QTMWidget.hpp
+* DESCRIPTION: QT Texmacs widget class
+* COPYRIGHT  : (C) 2008 Massimiliano Gubinelli
+*******************************************************************************
+* This software falls under the GNU general public license and comes WITHOUT
+* ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for more details.
+* If you don't have this file, write to the Free Software Foundation, Inc.,
+* 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+******************************************************************************/
 
 #ifndef QTMWIDGET_HPP
 #define QTMWIDGET_HPP
@@ -17,26 +18,26 @@
 #include <QWidget>
 
 class QTMWidget : public QWidget {
-	Q_OBJECT
+  Q_OBJECT
 
 public:
-	QTMWidget(simple_widget_rep *_wid) : QWidget() { 
+  QTMWidget(simple_widget_rep *_wid) : QWidget() { 
     setObjectName("A QTMWidget");
-	  setFocusPolicy(Qt::StrongFocus);
-	//	setBackgroundRole(QPalette::Window);
-//		setAutoFillBackground(true);
-		setAutoFillBackground(false);
-//		setAttribute (Qt::WA_OpaquePaintEvent);
-		setAttribute (Qt::WA_NoSystemBackground);
-
-	  setProperty("texmacs_widget", QVariant::fromValue((void*)_wid));  
-	};
-
+    setFocusPolicy(Qt::StrongFocus);
+    // setBackgroundRole(QPalette::Window);
+    // setAutoFillBackground(true);
+    setAutoFillBackground(false);
+    // setAttribute (Qt::WA_OpaquePaintEvent);
+    setAttribute (Qt::WA_NoSystemBackground);
+    setProperty("texmacs_widget", QVariant::fromValue((void*)_wid));
+    setMouseTracking (true);
+  };
 	
-	simple_widget_rep *tm_widget() { 
-		QVariant v= property("texmacs_widget");
-		return (simple_widget_rep *)(v.canConvert<void *>() ? v.value<void *>() : NULL);
-	}
+  simple_widget_rep*
+  tm_widget() { 
+    QVariant v= property("texmacs_widget");
+    return (simple_widget_rep *)(v.canConvert<void *>() ? v.value<void *>() : NULL);
+  }
 	
 private slots:
   void postponedUpdate(QRect r);	
@@ -46,9 +47,8 @@ protected:
   virtual void keyPressEvent ( QKeyEvent * event );
   virtual void mousePressEvent ( QMouseEvent * event );
   virtual void mouseReleaseEvent ( QMouseEvent * event );
-	virtual void mouseMoveEvent ( QMouseEvent * event );
-	virtual bool event(QEvent *event);
-
+  virtual void mouseMoveEvent ( QMouseEvent * event );
+  virtual bool event(QEvent *event);
 };
 
 #endif
