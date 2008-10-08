95169ebca3042a71cb3ea6e88fc1b18a631cc02c
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2008-10-08 16:48:48 +0000

    Track focus

--- src/src/Plugins/Qt/QTMWidget.hpp
+++ src/src/Plugins/Qt/QTMWidget.hpp
@@ -44,6 +44,8 @@ private slots:
 	
 protected:	
   virtual void paintEvent ( QPaintEvent * event );
+  virtual void focusInEvent ( QFocusEvent * event );
+  virtual void focusOutEvent ( QFocusEvent * event );
   virtual void keyPressEvent ( QKeyEvent * event );
   virtual void mousePressEvent ( QMouseEvent * event );
   virtual void mouseReleaseEvent ( QMouseEvent * event );
