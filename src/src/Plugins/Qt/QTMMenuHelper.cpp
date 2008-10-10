16576bd5d6f6bb9179d76eef5d9b30b589237009
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2008-10-10 07:55:10 +0000

    Cleaning of C++ files

--- src/src/Plugins/Qt/QTMMenuHelper.cpp
+++ src/src/Plugins/Qt/QTMMenuHelper.cpp
@@ -1,15 +1,13 @@
+
 /******************************************************************************
- * MODULE     : QTMMenuHelper.hpp
- * DESCRIPTION: QT Texmacs menu helper class
- * COPYRIGHT  : (C) 2008 Massimiliano Gubinelli
- *******************************************************************************
- * This software falls under the GNU general public license and comes WITHOUT
- * ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for more details.
- * If you don't have this file, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- ******************************************************************************/
+* MODULE     : QTMMenuHelper.hpp
+* DESCRIPTION: QT Texmacs menu helper class
+* COPYRIGHT  : (C) 2008 Massimiliano Gubinelli
+*******************************************************************************
+* This software falls under the GNU general public license and comes WITHOUT
+* ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for more details.
+* If you don't have this file, write to the Free Software Foundation, Inc.,
+* 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+******************************************************************************/
 
 #include "QTMMenuHelper.hpp"
-
-
-
