fcbb03ccab0be9e370f338fd29424291548e6dee
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2008-10-10 07:55:10 +0000

    Cleaning of C++ files

--- src/Plugins/Qt/QTMStyle.cpp
+++ src/Plugins/Qt/QTMStyle.cpp
@@ -1,14 +1,14 @@
-/******************************************************************************
- * MODULE     : QTMStyle.hpp
- * DESCRIPTION: QT Texmacs custom style (for some elements)
- * COPYRIGHT  : (C) 2008 Massimiliano Gubinelli
- *******************************************************************************
- * This software falls under the GNU general public license and comes WITHOUT
- * ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for more details.
- * If you don't have this file, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- ******************************************************************************/
 
+/******************************************************************************
+* MODULE     : QTMStyle.hpp
+* DESCRIPTION: QT Texmacs custom style (for some elements)
+* COPYRIGHT  : (C) 2008 Massimiliano Gubinelli
+*******************************************************************************
+* This software falls under the GNU general public license and comes WITHOUT
+* ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for more details.
+* If you don't have this file, write to the Free Software Foundation, Inc.,
+* 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+******************************************************************************/
 
 #include "QTMStyle.hpp"
 #include <QApplication>
@@ -16,183 +16,177 @@
 #include <iostream>
 #include "QTMStyle.moc"
 
-// QTMProxyStyle does not own *style
+/******************************************************************************
+* QTMProxyStyle (does not own *style)
+******************************************************************************/
 
-QTMProxyStyle::QTMProxyStyle(QStyle* _style)
-: QStyle(), style(_style)
-{
+QTMProxyStyle::QTMProxyStyle (QStyle* _style):
+  QStyle (), style (_style) {}
+
+QTMProxyStyle::~QTMProxyStyle() {
+  // delete style;
 }
 
-QTMProxyStyle::~QTMProxyStyle()
-{
-//	delete style;
+void
+QTMProxyStyle::drawComplexControl (ComplexControl control, const QStyleOptionComplex* option, QPainter* painter, const QWidget* widget) const {
+  style->drawComplexControl (control, option, painter, widget);
 }
 
-void QTMProxyStyle::drawComplexControl(ComplexControl control, const QStyleOptionComplex* option, QPainter* painter, const QWidget* widget) const
-{
-	style->drawComplexControl(control, option, painter, widget);
+void
+QTMProxyStyle::drawControl (ControlElement element, const QStyleOption* option, QPainter* painter, const QWidget* widget) const {
+  style->drawControl (element, option, painter, widget);
 }
 
-void QTMProxyStyle::drawControl(ControlElement element, const QStyleOption* option, QPainter* painter, const QWidget* widget)  const
-{
-	style->drawControl(element, option, painter, widget);
+void
+QTMProxyStyle::drawItemPixmap (QPainter* painter, const QRect& rect, int alignment, const QPixmap& pixmap) const {
+  style->drawItemPixmap (painter, rect, alignment, pixmap);
 }
 
-void QTMProxyStyle::drawItemPixmap(QPainter* painter, const QRect& rect, int alignment, const QPixmap& pixmap) const
-{
-	style->drawItemPixmap(painter, rect, alignment, pixmap);
+void
+QTMProxyStyle::drawItemText (QPainter* painter, const QRect& rect, int alignment, const QPalette& pal, bool enabled, const QString& text, QPalette::ColorRole textRole) const {
+  style->drawItemText (painter, rect, alignment, pal, enabled, text, textRole);
 }
 
-void QTMProxyStyle::drawItemText(QPainter* painter, const QRect& rect, int alignment, const QPalette& pal, bool enabled, const QString& text, QPalette::ColorRole textRole) const
-{
-	style->drawItemText(painter, rect, alignment, pal, enabled, text, textRole);
+void
+QTMProxyStyle::drawPrimitive (PrimitiveElement elem, const QStyleOption* option, QPainter* painter, const QWidget* widget) const {
+  style->drawPrimitive (elem, option, painter, widget);
 }
 
-void QTMProxyStyle::drawPrimitive(PrimitiveElement elem, const QStyleOption* option, QPainter* painter, const QWidget* widget) const
-{
-	style->drawPrimitive(elem, option, painter, widget);
+QPixmap
+QTMProxyStyle::generatedIconPixmap (QIcon::Mode iconMode, const QPixmap& pixmap, const QStyleOption* option) const {
+  return style->generatedIconPixmap (iconMode, pixmap, option);
 }
 
-QPixmap QTMProxyStyle::generatedIconPixmap(QIcon::Mode iconMode, const QPixmap& pixmap, const QStyleOption* option) const
-{
-	return style->generatedIconPixmap(iconMode, pixmap, option);
+QStyle::SubControl
+QTMProxyStyle::hitTestComplexControl (ComplexControl control, const QStyleOptionComplex* option, const QPoint& pos, const QWidget* widget) const {
+  return style->hitTestComplexControl (control, option, pos, widget);
 }
 
-QStyle::SubControl QTMProxyStyle::hitTestComplexControl(ComplexControl control, const QStyleOptionComplex* option, const QPoint& pos, const QWidget* widget) const
-{
-	return style->hitTestComplexControl(control, option, pos, widget);
+QRect
+QTMProxyStyle::itemPixmapRect (const QRect& rect, int alignment, const QPixmap& pixmap) const {
+  return style->itemPixmapRect (rect, alignment, pixmap);
 }
 
-QRect QTMProxyStyle::itemPixmapRect(const QRect& rect, int alignment, const QPixmap& pixmap) const
-{
-	return style->itemPixmapRect(rect, alignment, pixmap);
+QRect
+QTMProxyStyle::itemTextRect (const QFontMetrics& metrics, const QRect& rect, int alignment, bool enabled, const QString& text) const {
+  return style->itemTextRect (metrics, rect, alignment, enabled, text);
 }
 
-QRect QTMProxyStyle::itemTextRect(const QFontMetrics& metrics, const QRect& rect, int alignment, bool enabled, const QString& text) const
-{
-	return style->itemTextRect(metrics, rect, alignment, enabled, text);
+int
+QTMProxyStyle::pixelMetric (PixelMetric metric, const QStyleOption* option, const QWidget* widget) const {
+  return style->pixelMetric (metric, option, widget);
 }
 
-int QTMProxyStyle::pixelMetric(PixelMetric metric, const QStyleOption* option, const QWidget* widget) const
-{
-	return style->pixelMetric(metric, option, widget);
+void
+QTMProxyStyle::polish (QWidget* widget) {
+  style->polish (widget);
 }
 
-void QTMProxyStyle::polish(QWidget* widget)
-{
-	style->polish(widget);
+void
+QTMProxyStyle::polish (QApplication* app) {
+  style->polish (app);
 }
 
-void QTMProxyStyle::polish(QApplication* app)
-{
-	style->polish(app);
+void
+QTMProxyStyle::polish (QPalette& pal) {
+  style->polish (pal);
 }
 
-void QTMProxyStyle::polish(QPalette& pal)
-{
-	style->polish(pal);
+QSize
+QTMProxyStyle::sizeFromContents (ContentsType type, const QStyleOption* option, const QSize& contentsSize, const QWidget* widget) const {
+  return style->sizeFromContents (type, option, contentsSize, widget);
 }
 
-QSize QTMProxyStyle::sizeFromContents(ContentsType type, const QStyleOption* option, const QSize& contentsSize, const QWidget* widget) const
-{
-	return style->sizeFromContents(type, option, contentsSize, widget);
+QIcon
+QTMProxyStyle::standardIcon (StandardPixmap standardIcon, const QStyleOption* option, const QWidget* widget) const {
+  return style->standardIcon (standardIcon, option, widget);
 }
 
-QIcon QTMProxyStyle::standardIcon(StandardPixmap standardIcon, const QStyleOption* option, const QWidget* widget) const
-{
-	return style->standardIcon(standardIcon, option, widget);
+QPalette
+QTMProxyStyle::standardPalette () const {
+  return style->standardPalette ();
 }
 
-QPalette QTMProxyStyle::standardPalette() const
-{
-	return style->standardPalette();
+QPixmap
+QTMProxyStyle::standardPixmap (StandardPixmap standardPixmap, const QStyleOption* option, const QWidget* widget) const {
+  return style->standardPixmap (standardPixmap, option, widget);
 }
 
-QPixmap QTMProxyStyle::standardPixmap(StandardPixmap standardPixmap, const QStyleOption* option, const QWidget* widget) const
-{
-	return style->standardPixmap(standardPixmap, option, widget);
+int
+QTMProxyStyle::styleHint (StyleHint hint, const QStyleOption* option, const QWidget* widget, QStyleHintReturn* returnData) const {
+  return style->styleHint (hint, option, widget, returnData);
 }
 
-int QTMProxyStyle::styleHint(StyleHint hint, const QStyleOption* option, const QWidget* widget, QStyleHintReturn* returnData) const
-{
-	return style->styleHint(hint, option, widget, returnData);
+QRect
+QTMProxyStyle::subControlRect (ComplexControl control, const QStyleOptionComplex* option, SubControl subControl, const QWidget* widget) const {
+  return style->subControlRect (control, option, subControl, widget);
 }
 
-QRect QTMProxyStyle::subControlRect(ComplexControl control, const QStyleOptionComplex* option, SubControl subControl, const QWidget* widget) const
-{
-	return style->subControlRect(control, option, subControl, widget);
+QRect
+QTMProxyStyle::subElementRect (SubElement element, const QStyleOption* option, const QWidget* widget) const {
+  return style->subElementRect (element, option, widget);
 }
 
-QRect QTMProxyStyle::subElementRect(SubElement element, const QStyleOption* option, const QWidget* widget) const
-{
-	return style->subElementRect(element, option, widget);
+void
+QTMProxyStyle::unpolish (QWidget* widget) {
+  style->unpolish (widget);
 }
 
-void QTMProxyStyle::unpolish(QWidget* widget)
-{
-	style->unpolish(widget);
+void
+QTMProxyStyle::unpolish (QApplication* app) {
+  style->unpolish (app);
 }
 
-void QTMProxyStyle::unpolish(QApplication* app)
-{
-	style->unpolish(app);
-}
+/******************************************************************************
+* QTMStyle
+******************************************************************************/
 
-
-
-
-
-
-
-void QTMStyle::drawPrimitive(PrimitiveElement element, const QStyleOption *option,
-							 QPainter *painter, const QWidget *widget)  const
-{
-	//  if (element == QStyle::PE_FrameStatusBarItem) return;
-	if (element == QStyle::PE_FrameStatusBar) return;
-	style->drawPrimitive(element,option,painter,widget);
+void
+QTMStyle::drawPrimitive (PrimitiveElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const {
+  //  if (element == QStyle::PE_FrameStatusBarItem) return;
+  if (element == QStyle::PE_FrameStatusBar) return;
+  style->drawPrimitive(element,option,painter,widget);
 }  
 
-int QTMStyle::pixelMetric(PixelMetric metric, const QStyleOption *opt, const QWidget *widget) const
-{
-	switch (metric) {
-		case PM_ToolBarItemSpacing:
-			return 0;
-		default: 
-			return style->pixelMetric(metric,opt,widget);
-	}
+int
+QTMStyle::pixelMetric (PixelMetric metric, const QStyleOption *opt, const QWidget *widget) const {
+  switch (metric) {
+  case PM_ToolBarItemSpacing:
+    return 0;
+  default: 
+    return style->pixelMetric(metric,opt,widget);
+  }
 }
 
-
 #if 0
-void QTMStyle::drawControl(ControlElement element, const QStyleOption* option, QPainter* painter, const QWidget* widget)  const
-{
+void
+QTMStyle::drawControl (ControlElement element, const QStyleOption* option, QPainter* painter, const QWidget* widget) const {
   switch (element) {
-    case CE_MenuItem: 
-      if (const QStyleOptionMenuItem *mi = qstyleoption_cast<const QStyleOptionMenuItem *>(option)) {
-        QStyleOptionMenuItem mi2(*mi);
-        mi2.text = QString("pippo");
-        style->drawControl(element,&mi2,painter,widget);
-        break;
-      }
-    default:
-      style->drawControl(element,option,painter,widget);
+  case CE_MenuItem: 
+    if (const QStyleOptionMenuItem *mi =
+	qstyleoption_cast<const QStyleOptionMenuItem *> (option)) {
+      QStyleOptionMenuItem mi2(*mi);
+      mi2.text= QString ("pippo");
+      style->drawControl (element, &mi2, painter, widget);
+      break;
+    }
+  default:
+    style->drawControl (element, option, painter, widget);
   }
 }
 #endif
 
-QTMStyle *qtmstyle()
-{
-	static QTMStyle *qtmstyle = NULL;
-	if (!qtmstyle) {
-		//cout << "custom style !!!!\n";
-    QStyle *s = qApp->style();
-//    s->setParent(NULL);
-    // FIXME: if we change style at application level and the old style is deleted 
-    //        the application will crash
-		qtmstyle = new QTMStyle(s);
-//    QApplication::setStyle(qtmstyle);
-	}
-	return qtmstyle;
+QTMStyle*
+qtmstyle () {
+  static QTMStyle* qtmstyle= NULL;
+  if (!qtmstyle) {
+    // cout << "custom style !!!!\n";
+    QStyle* s= qApp->style();
+    // s->setParent(NULL);
+    // FIXME: if we change style at application level and
+    // the old style is deleted the application will crash
+    qtmstyle = new QTMStyle(s);
+    // QApplication::setStyle(qtmstyle);
+  }
+  return qtmstyle;
 }
-
-
--- src/Plugins/Qt/QTMWidget.cpp
+++ src/Plugins/Qt/QTMWidget.cpp
@@ -23,7 +23,7 @@ extern bool qt_update_flag;
 hashmap<int,string> qtkeymap (0);
 
 inline void
-scale (QPoint &point) {	
+scale (QPoint& point) {	
   point.rx() *= PIXEL; point.ry() *= -PIXEL;
 }
 
@@ -34,92 +34,93 @@ map (int code, string name) {
 
 void
 initkeymap () {
-  map (Qt::Key_Space,"space");
-  map (Qt::Key_Return,"return");
-  map (Qt::Key_Tab,"tab");
-  map (Qt::Key_Backspace,"backspace");
-  map (Qt::Key_Enter,"enter");
-  map (Qt::Key_Escape,"escape");
-  //m ap(0x0003,"K-enter");
-  map (Qt::Key_Backspace,"backspace");
+  map (Qt::Key_Space     , "space");
+  map (Qt::Key_Return    , "return");
+  map (Qt::Key_Tab       , "tab");
+  map (Qt::Key_Backspace , "backspace");
+  map (Qt::Key_Enter     , "enter");
+  map (Qt::Key_Escape    , "escape");
+  map (Qt::Key_Backspace , "backspace");
+  map (Qt::Key_Up        , "up" );
+  map (Qt::Key_Down      , "down" );
+  map (Qt::Key_Left      , "left" );
+  map (Qt::Key_Right     , "right" );
+  map (Qt::Key_F1        , "F1" );
+  map (Qt::Key_F2        , "F2" );
+  map (Qt::Key_F3        , "F3" );
+  map (Qt::Key_F4        , "F4" );
+  map (Qt::Key_F5        , "F5" );
+  map (Qt::Key_F6        , "F6" );
+  map (Qt::Key_F7        , "F7" );
+  map (Qt::Key_F8        , "F8" );
+  map (Qt::Key_F9        , "F9" );
+  map (Qt::Key_F10       , "F10" );
+  map (Qt::Key_F11       , "F11" );
+  map (Qt::Key_F12       , "F12" );
+  map (Qt::Key_F13       , "F13" );
+  map (Qt::Key_F14       , "F14" );
+  map (Qt::Key_F15       , "F15" );
+  map (Qt::Key_F16       , "F16" );
+  map (Qt::Key_F17       , "F17" );
+  map (Qt::Key_F18       , "F18" );
+  map (Qt::Key_F19       , "F19" );
+  map (Qt::Key_F20       , "F20" );
+  map (Qt::Key_F21       , "F21" );
+  map (Qt::Key_F22       , "F22" );
+  map (Qt::Key_F23       , "F23" );
+  map (Qt::Key_F24       , "F24" );
+  map (Qt::Key_F25       , "F25" );
+  map (Qt::Key_F26       , "F26" );
+  map (Qt::Key_F27       , "F27" );
+  map (Qt::Key_F28       , "F28" );
+  map (Qt::Key_F29       , "F29" );
+  map (Qt::Key_F30       , "F30" );
+  map (Qt::Key_F31       , "F31" );
+  map (Qt::Key_F32       , "F32" );
+  map (Qt::Key_F33       , "F33" );
+  map (Qt::Key_F34       , "F34" );
+  map (Qt::Key_F35       , "F35" );
+  map (Qt::Key_Insert    , "insert" );
+  map (Qt::Key_Delete    , "delete" );
+  map (Qt::Key_Home      , "home" );
+  map (Qt::Key_End       , "end" );
+  map (Qt::Key_PageUp    , "pageup" );
+  map (Qt::Key_PageDown  , "pagedown" );
+  map (Qt::Key_ScrollLock, "scrolllock" );
+  map (Qt::Key_Pause     , "pause" );
+  map (Qt::Key_SysReq    , "sysreq" );
+  map (Qt::Key_Stop      , "stop" );
+  map (Qt::Key_Menu      , "menu" );
+  map (Qt::Key_Print     , "print" );
+  map (Qt::Key_Select    , "select" );
+  map (Qt::Key_Execute   , "execute" );
+  map (Qt::Key_Help      , "help" );
 
-  map (Qt::Key_Up       ,"up" );
-  map (Qt::Key_Down     ,"down" );
-  map (Qt::Key_Left     ,"left" );
-  map (Qt::Key_Right    ,"right" );
-  map (Qt::Key_F1    ,"F1" );
-  map (Qt::Key_F2    ,"F2" );
-  map (Qt::Key_F3    ,"F3" );
-  map (Qt::Key_F4    ,"F4" );
-  map (Qt::Key_F5    ,"F5" );
-  map (Qt::Key_F6    ,"F6" );
-  map (Qt::Key_F7    ,"F7" );
-  map (Qt::Key_F8    ,"F8" );
-  map (Qt::Key_F9    ,"F9" );
-  map (Qt::Key_F10   ,"F10" );
-  map (Qt::Key_F11   ,"F11" );
-  map (Qt::Key_F12   ,"F12" );
-  map (Qt::Key_F13   ,"F13" );
-  map (Qt::Key_F14   ,"F14" );
-  map (Qt::Key_F15   ,"F15" );
-  map (Qt::Key_F16   ,"F16" );
-  map (Qt::Key_F17   ,"F17" );
-  map (Qt::Key_F18   ,"F18" );
-  map (Qt::Key_F19   ,"F19" );
-  map (Qt::Key_F20   ,"F20" );
-  map (Qt::Key_F21   ,"F21" );
-  map (Qt::Key_F22   ,"F22" );
-  map (Qt::Key_F23   ,"F23" );
-  map (Qt::Key_F24   ,"F24" );
-  map (Qt::Key_F25   ,"F25" );
-  map (Qt::Key_F26   ,"F26" );
-  map (Qt::Key_F27   ,"F27" );
-  map (Qt::Key_F28   ,"F28" );
-  map (Qt::Key_F29   ,"F29" );
-  map (Qt::Key_F30   ,"F30" );
-  map (Qt::Key_F31   ,"F31" );
-  map (Qt::Key_F32   ,"F32" );
-  map (Qt::Key_F33   ,"F33" );
-  map (Qt::Key_F34   ,"F34" );
-  map (Qt::Key_F35   ,"F35" );
-  map (Qt::Key_Insert        ,"insert" );
-  map (Qt::Key_Delete        ,"delete" );
-  map (Qt::Key_Home  ,"home" );
-  // map (Qt::Key_Begin         ,"begin" );
-  map (Qt::Key_End   ,"end" );
-  map (Qt::Key_PageUp        ,"pageup" );
-  map (Qt::Key_PageDown      ,"pagedown" );
-  // map (Qt::Key_PrintScreen   ,"printscreen" );
-  map (Qt::Key_ScrollLock    ,"scrolllock" );
-  map (Qt::Key_Pause         ,"pause" );
-  map (Qt::Key_SysReq        ,"sysreq" );
-  // map (Qt::Key_Break         ,"break" );
-  //  map (Qt::Key_Reset         ,"reset" );
-  map (Qt::Key_Stop  ,"stop" );
-  map (Qt::Key_Menu  ,"menu" );
-  //  map (Qt::Key_User  ,"user" );
-  //  map (Qt::Key_System        ,"system" );
-  map (Qt::Key_Print         ,"print" );
-  //  map (Qt::Key_ClearLine     ,"clear" );
-  // map (Qt::Key_ClearDisplay  ,"cleardisplay" );
-  // map (Qt::Key_InsertLine    ,"insertline" );
-  // map (Qt::Key_DeleteLine    ,"deleteline" );
-  // map (Qt::Key_InsertChar    ,"insert" );
-  // map (Qt::Key_DeleteChar    ,"delete" );
-  // map (Qt::Key_Prev  ,"prev" );
-  // map (Qt::Key_Next  ,"next" );
-  map (Qt::Key_Select        ,"select" );
-  map (Qt::Key_Execute       ,"execute" );
-  // map (Qt::Key_Undo  ,"undo" );
-  // map (Qt::Key_Redo  ,"redo" );
-  // map (Qt::Key_Find  ,"find" );
-  map (Qt::Key_Help  ,"help" );
-  // map (Qt::Key_ModeSwitchFunctionKey    ,"modeswitch" );  
-  map (Qt::Key_Dead_Acute, "acute");
-  map (Qt::Key_Dead_Grave, "grave");
-  map (Qt::Key_Dead_Diaeresis, "umlaut");
+  map (Qt::Key_Dead_Acute     , "acute");
+  map (Qt::Key_Dead_Grave     , "grave");
+  map (Qt::Key_Dead_Diaeresis , "umlaut");
   map (Qt::Key_Dead_Circumflex, "hat");
-  map (Qt::Key_Dead_Tilde, "tilde");
+  map (Qt::Key_Dead_Tilde     , "tilde");
+
+  // map (0x0003              , "K-enter");
+  // map (Qt::Key_Begin       , "begin" );
+  // map (Qt::Key_PrintScreen , "printscreen" );
+  // map (Qt::Key_Break       , "break" );
+  // map (Qt::Key_User        , "user" );
+  // map (Qt::Key_System      , "system" );
+  // map (Qt::Key_Reset       , "reset" );
+  // map (Qt::Key_ClearLine   , "clear" );
+  // map (Qt::Key_ClearDisplay, "cleardisplay" );
+  // map (Qt::Key_InsertLine  , "insertline" );
+  // map (Qt::Key_DeleteLine  , "deleteline" );
+  // map (Qt::Key_InsertChar  , "insert" );
+  // map (Qt::Key_DeleteChar  , "delete" );
+  // map (Qt::Key_Prev        , "prev" );
+  // map (Qt::Key_Next        , "next" );
+  // map (Qt::Key_Undo        , "undo" );
+  // map (Qt::Key_Redo        , "redo" );
+  // map (Qt::Key_Find        , "find" );
+  // map (Qt::Key_ModeSwitchFunctionKey, "modeswitch" );  
 }
 
 void
@@ -132,12 +133,12 @@ QTMWidget::postponedUpdate () {
 
 void
 QTMWidget::paintEvent (QPaintEvent* event) {
-  QRect rect = event->rect();
+  QRect rect = event->rect ();
   if (DEBUG_EVENTS) {
     QPainter p(this);
-    QBrush brush(QColor("red"));
-    p.fillRect(rect,brush);
-    p.end();
+    QBrush brush (QColor ("red"));
+    p.fillRect (rect, brush);
+    p.end ();
   }
 
   if (qt_update_flag)
@@ -343,9 +344,9 @@ QTMWidget::mouseMoveEvent (QMouseEvent* event) {
 bool
 QTMWidget::event (QEvent* event) {
   if (event->type() == QEvent::KeyPress) {
-    QKeyEvent *ke = static_cast<QKeyEvent *>(event);
+    QKeyEvent *ke = static_cast<QKeyEvent*> (event);
     keyPressEvent (ke);
     return true;
   }  
-  return QWidget::event(event);
+  return QWidget::event (event);
 }
