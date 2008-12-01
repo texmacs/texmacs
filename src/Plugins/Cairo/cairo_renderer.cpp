ed93aeb077a0f473be5dd88c02a86cba2d78f85a
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2008-12-01 16:01:51 +0000

    cairo support fixes

--- src/Plugins/Cairo/cairo_renderer.cpp
+++ src/Plugins/Cairo/cairo_renderer.cpp
@@ -10,6 +10,8 @@
 * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 ******************************************************************************/
 
+#include "config.h"
+
 #ifdef USE_CAIRO
 #include "cairo_renderer.hpp"
 #include "analyze.hpp"
