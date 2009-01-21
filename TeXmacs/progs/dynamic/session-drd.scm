23556fbeb664fceefce8765c23cbc368647cd3d3
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2009-01-21 20:52:03 +0000

    Add missing file

---
+++ TeXmacs/progs/dynamic/session-drd.scm
@@ -0,0 +1,20 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : session-drd.scm
+;; DESCRIPTION : data relation definitions for session tags
+;; COPYRIGHT   : (C) 2009  Joris van der Hoeven
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(texmacs-module (dynamic session-drd)
+  (:use (dynamic dynamic-drd)))
+
+;; fold <-> unfold toggles
+
+(define-fold folded-io unfolded-io)
+(define-fold folded-io-math unfolded-io-math)
