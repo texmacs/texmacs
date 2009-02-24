19e49d08930faa69d41e4d2509efa8918ebbb630
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2009-02-24 13:17:28 +0000

    add qt frameworks to bundle

--- misc/bundle/bundle-libs.sh
+++ misc/bundle/bundle-libs.sh
@@ -2,6 +2,7 @@
 
 EXECUTABLE=${1}
 BUNDLE_RESOURCES=${1%/*}/../Resources
+BUNDLE_FRAMEWORKS=${1%/*}/../Frameworks
 
 function bundle_install_lib {
   echo Bundling [$2] in [${BUNDLE_RESOURCES}/lib/$3] for [$1]
@@ -20,4 +21,36 @@ function bundle_all_libs {
   done
 }
 
+
+function bundle_framework {
+ if [ ! ${4##*/} == ${1##*/} ]; then
+  echo Bundling Framework [${2}] to [${3}/Versions/${4}] for [${1}]
+  if [ ! -e ${BUNDLE_FRAMEWORKS}/${3} ]; then
+    cp -R ${2} ${BUNDLE_FRAMEWORKS}
+    bundle_qt_frameworks ${BUNDLE_FRAMEWORKS}/${3}/Versions/${4}
+  fi
+  install_name_tool -id @executable_path/../Frameworks/${3}/Versions/${4} ${BUNDLE_FRAMEWORKS}/${3}/Versions/${4}
+  install_name_tool -change ${5} @executable_path/../Frameworks/${3}/Versions/${4} ${1} 
+ fi
+}
+
+function bundle_qt_frameworks {
+  if [ ! -e ${BUNDLE_FRAMEWORKS} ]; then
+    mkdir ${BUNDLE_FRAMEWORKS}
+  fi
+  for f in $( otool -L ${1}  | grep -o '\(.*Qt.*\.framework/Versions/[^: ]*\) ' ) ; do 
+    fname=${f%%/Versions/*}
+    if [ ! -e ${fname} ]; then
+      if [ -e /Library/Frameworks/${fname} ]; then
+        fname=/Library/Frameworks/${fname}
+      fi
+    fi 
+    if [ -e ${fname} ]; then
+  	  bundle_framework  ${1} ${fname} ${fname##*/} ${f#*Versions/} ${f}
+    fi
+  done
+}
+
+
 bundle_all_libs ${EXECUTABLE}
+bundle_qt_frameworks ${EXECUTABLE}
