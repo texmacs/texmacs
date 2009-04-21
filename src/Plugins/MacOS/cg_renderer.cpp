86d6b898734d5915b6bc77c4ac00626c2f401560
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2009-04-21 12:52:25 +0000

    remove system call to convert images

--- src/Plugins/MacOS/cg_renderer.cpp
+++ src/Plugins/MacOS/cg_renderer.cpp
@@ -20,6 +20,8 @@
 
 #include "Freetype/tt_file.hpp" // tt_font_find
 
+#include "mac_images.h"
+
 /******************************************************************************
 * CG images
 ******************************************************************************/
@@ -263,7 +265,8 @@ cg_renderer_rep::image (url u, SI w, SI h, SI x, SI y,
                suffix (u) == "eps" ||
                suffix (u) == "pdf") {
       url temp= url_temp (".png");
-      system ("convert", u, temp);
+//      system ("convert", u, temp);
+      mac_image_to_png (u, temp); 
       string suu = as_string (temp);
       char * buf = as_charp(suu); 
       //cout << suu << LF;
