248b385f3e71e9e7983263d477f1943161a4580d
Emmanuël Corcelle <corcelle>
2009-04-26 21:22:37 +0000

    Add Evaluation switch and menu with eukleides commands

--- plugins/eukleides/progs/init-eukleides.scm
+++ plugins/eukleides/progs/init-eukleides.scm
@@ -5,9 +5,10 @@
 ;; DESCRIPTION : Initialize Eukleides plugin
 ;; COPYRIGHT   : (C) 2003 Joris van der Hoeven.
 ;;
-;; This software falls under the GNU general public license version 3 or later.
-;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
-;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;; This software falls under the GNU general public license and comes WITHOUT
+;; ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for details.
+;; If you don't have this file, write to the Free Software Foundation, Inc.,
+;; 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -17,8 +18,18 @@
     (with s (texmacs->verbatim (stree->tree u))
       (string-append (escape-verbatim (string-replace s "\n" "~")) "\n"))))
 
+(define (eukleides-initialize)
+  (import-from (eukleides-menus))
+  (import-from (utils plugins plugin-convert))
+  (menu-extend texmacs-extra-menu
+	(if (or (in-eukleides?) (and (not-in-session?) (eukleides-scripts?)))
+		(=> "Eukleides" (link eukleides-functions-menu)))))
+
 (plugin-configure eukleides
   (:require (url-exists-in-path? "eukleides"))
+  (:initialize (eukleides-initialize))
   (:launch "tm_eukleides --texmacs")
   (:serializer ,eukleides-serialize)
-  (:session "Eukleides"))
+  (:session "Eukleides")
+  (:scripts "Eukleides"))
+
