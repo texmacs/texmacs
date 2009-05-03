bd1a377489cd61bf9d3f29b9c0dd0d96370b7689
Emmanuël Corcelle <corcelle>
2009-05-03 21:45:40 +0000

    Update to gpl-3.0

--- plugins/asymptote/progs/init-asymptote.scm
+++ plugins/asymptote/progs/init-asymptote.scm
@@ -5,10 +5,9 @@
 ;; DESCRIPTION : Initialize Asymptote plugin
 ;; COPYRIGHT   : (C) Yann Dirson <ydirson at altern dot org>.
 ;;
-;; This software falls under the GNU general public license and comes WITHOUT
-;; ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for details.
-;; If you don't have this file, write to the Free Software Foundation, Inc.,
-;; 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
