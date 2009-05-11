fcda55a9c75cbef3aab4e41b16afb5c95e491c18
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2009-05-11 07:29:00 +0000

    Minor fix

--- plugins/pari/packages/session/pari.ts
+++ plugins/pari/packages/session/pari.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.0.3.4>
+<TeXmacs|1.0.7.2>
 
 <style|source>
 
@@ -13,16 +13,15 @@
     <src-copyright|2002--2004|Joris van der Hoeven>
 
     <\src-license>
-      This software falls under the <hlink|GNU general public
-      license, version 3 or later|$TEXMACS_PATH/LICENSE>.
-      It comes WITHOUT ANY WARRANTY WHATSOEVER.
-      You should have received a copy of the license which the software.
-      If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      This software falls under the <hlink|GNU general public license,
+      version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+      WARRANTY WHATSOEVER. You should have received a copy of the license
+      which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
     </src-license>
   </src-title>>
 
-  <assign|pari-input|<macro|prompt|body|<style-with|src-compact|none|<generic-input|<arg|prompt>|<with|color|dark
-  green|<arg|body>>>>>>
+  <assign|pari-input|<macro|prompt|body|<style-with|src-compact|none|<generic-input|<with|color|dark
+  brown|<arg|prompt>>|<with|color|dark green|<arg|body>>>>>>
 
   <assign|pari-output|<macro|body|<style-with|src-compact|none|<generic-output|<with|math-display|false|<arg|body>>>>>>
 
@@ -31,18 +30,6 @@
 
 <\initial>
   <\collection>
-    <associate|page-bot|30mm>
-    <associate|page-even|30mm>
-    <associate|page-odd|30mm>
-    <associate|page-reduce-bot|15mm>
-    <associate|page-reduce-left|25mm>
-    <associate|page-reduce-right|25mm>
-    <associate|page-reduce-top|15mm>
-    <associate|page-right|30mm>
-    <associate|page-top|30mm>
-    <associate|page-type|a4>
-    <associate|par-width|150mm>
     <associate|preamble|true>
-    <associate|sfactor|4>
   </collection>
 </initial>
