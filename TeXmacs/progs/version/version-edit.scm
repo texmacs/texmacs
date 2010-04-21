0dbcb4b3c0520ff134b79702989b84c682762490
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2010-04-21 23:48:38 +0000

    Versioning actions on selections

--- TeXmacs/progs/version/version-edit.scm
+++ TeXmacs/progs/version/version-edit.scm
@@ -14,6 +14,10 @@
 (texmacs-module (version version-edit)
   (:use (version version-drd)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Important predicates
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (tm-define (version-context? t)
   (version-tag? (tree-label t)))
 
@@ -21,38 +25,81 @@
   (not (not (tree-innermost version-context?))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Subroutine for computing all subtrees in a selection
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (selection-subtrees t p1 p2)
+  (cond ((or (null? p1) (null? p2)) '())
+	((and (== p1 (list 0)) (== p2 (list (tree-right-index t)))) (list t))
+	((tree-atomic? t) '())
+	((== p1 (list (tree-right-index t))) '())
+	((== p2 (list 0)) '())
+	((== p1 (list 0))
+	 (selection-subtrees t (list 0 0) p2))
+	((== p2 (list (tree-right-index t)))
+	 (with n (- (tree-arity t) 1)
+	   (with c (tree-ref t n)
+	     (selection-subtrees t p1 (list n (tree-right-index c))))))
+	((== (car p1) (car p2))
+	 (selection-subtrees (tree-ref t (car p1)) (cdr p1) (cdr p2)))
+	((< (car p1) (car p2))
+	 (let* ((i1 (car p1))
+		(i2 (car p2))
+		(t1 (tree-ref t i1))
+		(t2 (tree-ref t i2))
+		(l1 (selection-subtrees t p1 (list i1 (tree-right-index t1))))
+		(l2 (selection-subtrees t (list i2 0) p2))
+		(ll (sublist (tree-children t) (+ i1 1) i2)))
+	   (append l1 ll l2)))
+	(else '())))
+
+(tm-define (selection-trees)
+  (let* ((p1 (selection-get-start))
+	 (p2 (selection-get-end)))
+    (selection-subtrees (root-tree) p1 p2)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Moving across the differences between both versions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define (version-empty?)
+  (and (inside-version?)
+       (with-innermost t version-context?
+	 (with u (tree-down t)
+	   (== u (tree ""))))))
+
 (tm-define (version-first-difference)
   (go-start)
-  (go-to-next-tag (group-resolve 'version-tag)))
+  (version-next-difference))
 
 (tm-define (version-previous-difference)
-  (go-to-previous-tag (group-resolve 'version-tag)))
+  (go-to-previous-tag (group-resolve 'version-tag))
+  (when (version-empty?)
+    (go-to-previous-tag (group-resolve 'version-tag))
+    (when (version-empty?)
+      (go-to-previous-tag (group-resolve 'version-tag)))))
 
 (tm-define (version-next-difference)
-  (go-to-next-tag (group-resolve 'version-tag)))
+  (go-to-next-tag (group-resolve 'version-tag))
+  (when (version-empty?)
+    (go-to-next-tag (group-resolve 'version-tag))
+    (when (version-empty?)
+      (go-to-next-tag (group-resolve 'version-tag)))))
 
 (tm-define (version-last-difference)
   (go-end)
-  (go-to-previous-tag (group-resolve 'version-tag)))
+  (version-previous-difference))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Specify which version to show
+;; Switch between different visualizations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(tm-define (version-show-both)
-  (:context version-context?)
-  (variant-replace version-context? 'version-both))
-
-(tm-define (version-show-old)
-  (:context version-context?)
-  (variant-replace version-context? 'version-old))
-
-(tm-define (version-show-new)
-  (:context version-context?)
-  (variant-replace version-context? 'version-new))
+(tm-define (version-show tag)
+  (cond ((selection-active-any?)
+	 (for-each (lambda (u) (tree-replace u version-context? tag))
+		   (selection-trees)))
+	((inside-version?)
+	 (variant-replace version-context? tag))))
 
 (tm-define (version-show-all tag)
   (tree-replace (buffer-tree) version-context? tag))
@@ -61,30 +108,8 @@
 ;; Retaining only one version
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(tm-define (version-retain-current)
-  (:context version-context?)
-  (variant-replace version-context? 'version-both))
-
-(tm-define (version-retain-old)
-  (with-innermost t version-context?
-    (tree-set t (tree-ref t 0))
-    (version-next-difference)))
-
-(tm-define (version-retain-new)
-  (with-innermost t version-context?
-    (tree-set t (tree-ref t 1))
-    (version-next-difference)))
-
-(tm-define (version-retain-current)
-  (:inside version-old)
-  (version-retain-old))
-
-(tm-define (version-retain-current)
-  (:inside version-new version-both)
-  (version-retain-new))
-
-(tm-define (version-retain-all which)
-  (tree-replace (buffer-tree) version-context?
+(define (version-retain-version where which)
+  (tree-replace where version-context?
 		(lambda (t)
 		  (cond ((number? which)
 			 (tree-set t (tree-ref t which)))
@@ -92,3 +117,15 @@
 			 (tree-set t (tree-ref t 0)))
 			(else
 			 (tree-set t (tree-ref t 1)))))))
+
+(tm-define (version-retain which)
+  (cond ((selection-active-any?)
+	 (for-each (lambda (u) (version-retain-version u which))
+		   (selection-trees)))
+	((inside-version?)
+	 (with-innermost t version-context?
+	   (version-retain-version t which)
+	   (version-next-difference)))))
+
+(tm-define (version-retain-all which)
+  (version-retain-version (buffer-tree) which))
