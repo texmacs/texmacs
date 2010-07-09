dc5233cf2c0d50092885976171f924ab13c5f6fa
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2010-07-09 10:15:50 +0000

    Nicer presentation

--- TeXmacs/styles/bibtex/bibliography.ts
+++ TeXmacs/styles/bibtex/bibliography.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.0.7.3>
+<TeXmacs|1.0.7.4>
 
 <style|source>
 
@@ -26,31 +26,19 @@
 
   <use-package|std|env>
 
+  \;
+
   <assign|color|red>
 
-  <assign|bib-comm-prefix|<with|font-series|bold|% >>
+  <assign|par-first|0fn>
 
-  <assign|bib-line-env|<\macro|cbody|pre|body>
-    <with|color|<arg|cbody>|<arg|pre><arg|body>>
-  </macro>>
+  <assign|par-par-sep|0.6666fn>
 
-  <assign|bib-line|<macro|body|<bib-line-env|red||<arg|body>>>>
-
-  <assign|bib-var|<macro|body|<with|color|red|<strong|<arg|body>>>>>
-
-  <assign|bib-latex|<macro|body|<bib-line-env|red||<arg|body>>>>
-
-  <assign|bib-field-env|<\macro|ctype|cval|mvar|pre|type|val>
-    <tabular|<tformat|<twith|table-width|1par>|<twith|table-hmode|min>|<cwith|1|-1|2|2|cell-halign|l>|<cwith|1|-1|2|2|cell-hyphen|t>|<cwith|1|-1|1|1|cell-width|10em>|<cwith|1|-1|1|1|cell-hmode|exact>|<cwith|1|-1|1|1|cell-lsep|1.5em>|<cwith|1|-1|2|2|cell-rsep|0em>|<table|<row|<cell|<with|color|<arg|ctype>|<arg|pre><strong|<arg|type>>>>|<\cell>
-      <with|color|<arg|cval>|bib-var|<arg|mvar>|<arg|val>>
-    </cell>>>>>
-  </macro>>
-
-  <assign|bib-field|<macro|type|val|<bib-field-env|dark
-  red|red|<macro|b|<with|color|dark red|<strong|<arg|b>>>>||<arg|type>|<arg|val>>>>
-
-  <assign|bib-assign|<macro|type|val|<bib-field-env|dark
-  red|red|<macro|b|<with|color|dark red|<strong|<arg|b>>>>||<arg|type>|<arg|val>>>>
+  <\active*>
+    <\src-comment>
+      Bibliography entries.
+    </src-comment>
+  </active*>
 
   <assign|bib-entry-env|<\macro|mfield|cfg|cbg|cid|pre|type|id|body>
     <\padded-normal|0.25em|1.5em>
@@ -59,17 +47,84 @@
         ><with|color|<arg|cid>|<arg|id>>>
       </wide-std-framed-colored>
 
-      <with|bib-field|<arg|mfield>|<arg|body>>
+      <\with|bib-field|<arg|mfield>|par-par-sep|0fn>
+        <arg|body>
+      </with>
     </padded-normal>
   </macro>>
 
   <assign|bib-entry|<\macro|type|id|body>
     <\with|mfield|<macro|t|v|<bib-field-env|dark
     blue|black|<macro|b|<with|color|dark green|<strong|<arg|b>>>>||<arg|t>|<arg|v>>>>
-      <bib-entry-env|<value|mfield>|dark blue|pastel blue|dark
-      green||<arg|type>|<arg|id>|<arg|body>>
+      <\bib-entry-env|<value|mfield>|dark blue|pastel blue|dark
+      green||<arg|type>|<arg|id>>
+        <arg|body>
+      </bib-entry-env>
     </with>
   </macro>>
+
+  <\active*>
+    <\src-comment>
+      Bibliography fields.
+    </src-comment>
+  </active*>
+
+  <assign|bib-field-env|<\macro|ctype|cval|mvar|pre|type|val>
+    <\surround||<right-flush>>
+      <tabular|<tformat|<twith|table-width|1par>|<twith|table-hmode|min>|<cwith|1|-1|2|2|cell-halign|l>|<cwith|1|-1|2|2|cell-hyphen|t>|<cwith|1|-1|1|1|cell-width|10em>|<cwith|1|-1|1|1|cell-hmode|exact>|<cwith|1|-1|1|1|cell-lsep|1.5em>|<cwith|1|-1|2|2|cell-rsep|0em>|<cwith|1|-1|2|2|cell-hpart|1>|<table|<row|<cell|<with|color|<arg|ctype>|<arg|pre><strong|<arg|type>>>>|<\cell>
+        <with|color|<arg|cval>|bib-var|<arg|mvar>|<arg|val>>
+      </cell>>>>>
+    </surround>
+  </macro>>
+
+  <assign|bib-field|<\macro|type|val>
+    <bib-field-env|dark red|red|<macro|b|<with|color|dark
+    red|<strong|<arg|b>>>>||<arg|type>|<arg|val>>
+  </macro>>
+
+  <\active*>
+    <\src-comment>
+      Markup for bibliography fields.
+    </src-comment>
+  </active*>
+
+  <assign|bib-name|<macro|f|v|l|j|<if|<unequal|<length|<arg|f>>|0>|<arg|f>
+  ><if|<unequal|<length|<arg|v>>|0>|<arg|><with|color|dark
+  grey|font-shape|small-caps|<arg|v>> ><with|font-shape|small-caps|<arg|l>><if|<unequal|<length|<arg|j>>|0>|,
+  <arg|><with|font-shape|italic|<arg|j>> >>>
+
+  <assign|bib-name-extra|<macro|x| <with|color|dark green|<strong|and>>
+  <arg|x>>>
+
+  <assign|bib-names|<xmacro|args|<style-with|src-compact|none|<arg|args|0><map-args|bib-name-extra|concat|args|1>>>>
+
+  <assign|bib-pages-extra|<macro|x|<with|color|dark
+  green|<space|0.5spc><strong|--><space|0.5spc>><arg|x>>>
+
+  <assign|bib-pages|<xmacro|args|<style-with|src-compact|none|<arg|args|0><map-args|bib-pages-extra|concat|args|1>>>>
+
+  <assign|keepcase|<macro|x|<underline|<arg|x>>>>
+
+  <\active*>
+    <\src-comment>
+      Markup for BibTeX compatibility
+    </src-comment>
+  </active*>
+
+  <assign|bib-comm-prefix|<with|font-series|bold|% >>
+
+  <assign|bib-var|<macro|body|<with|color|red|<strong|<arg|body>>>>>
+
+  <assign|bib-latex|<macro|body|<bib-line-env|red||<arg|body>>>>
+
+  <assign|bib-assign|<macro|type|val|<bib-field-env|dark
+  red|red|<macro|b|<with|color|dark red|<strong|<arg|b>>>>||<arg|type>|<arg|val>>>>
+
+  <assign|bib-line-env|<\macro|cbody|pre|body>
+    <with|color|<arg|cbody>|<arg|pre><arg|body>>
+  </macro>>
+
+  <assign|bib-line|<macro|body|<bib-line-env|red||<arg|body>>>>
 
   <assign|bib-string|<\macro|body>
     <\padded-normal|0.25em|1.5em>
@@ -105,21 +160,7 @@
     </with>
   </macro>>
 
-  <assign|bib-name|<macro|f|v|l|j|<if|<unequal|<length|<arg|f>>|0>|<arg|f>
-  ><if|<unequal|<length|<arg|v>>|0>|<arg|><with|font-shape|italic|<arg|v>>
-  ><with|font-shape|small-caps|<arg|l>><if|<unequal|<length|<arg|j>>|0>|,
-  <arg|><with|font-shape|italic|<arg|j>> >>>
-
-  <assign|bib-name-extra|<macro|x| <with|color|dark green|and> <arg|x>>>
-
-  <assign|bib-names|<xmacro|args|<style-with|src-compact|none|<arg|args|0><map-args|bib-name-extra|concat|args|1>>>>
-
-  <assign|bib-pages-extra|<macro|x|<with|color|dark green|--><arg|x>>>
-
-  <assign|bib-pages|<xmacro|args|<style-with|src-compact|none|<arg|args|0><map-args|bib-pages-extra|concat|args|1>>>>
-
-  <assign|keepcase|<macro|x|<with|color|dark green|{><arg|x><with|color|dark
-  green|}>>>
+  \;
 </body>
 
 <\initial>
