1e761cbce453e134dc0ca512a24226f231d097d7
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2010-09-07 08:55:35 +0000

    Add beamer style

---
+++ TeXmacs/styles/beamer.ts
@@ -0,0 +1,36 @@
+<TeXmacs|1.0.7.6>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|beamer|1.0>
+
+      <\src-purpose>
+        The beamer style for presentations from a laptop.
+      </src-purpose>
+
+      <\src-copyright|2010>
+        Joris van der Hoeven
+      </src-copyright>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
+    </src-title>
+  </active*>
+
+  <use-package|generic|ridged-paper>
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|sfactor|7>
+  </collection>
+</initial>
