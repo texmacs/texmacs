e89fd1ce483d157bfc5b0b631a946bc673733f6d
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2010-10-11 14:10:29 +0000

    Separate tree_brackets.cpp into several parts

---
+++ src/Data/Tree/tree_analyze.cpp
@@ -0,0 +1,210 @@
+
+/******************************************************************************
+* MODULE     : tree_analyze.cpp
+* DESCRIPTION: routines for analyzing trees
+* COPYRIGHT  : (C) 2010  Joris van der Hoeven
+*******************************************************************************
+* This software falls under the GNU general public license version 3 or later.
+* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+******************************************************************************/
+
+#include "tree_analyze.hpp"
+
+/******************************************************************************
+* Tokenize mathematical concats and recomposition
+******************************************************************************/
+
+array<tree>
+concat_tokenize (tree t) {
+  static language lan= math_language ("std-math");
+  array<tree> r;
+  if (is_atomic (t)) {
+    int i= 0;
+    while (i<N(t->label)) {
+      int start= i;
+      (void) lan->advance (t, i);
+      r << tree (t->label (start, i));
+    }
+  }
+  else if (is_concat (t))
+    for (int i=0; i<N(t); i++)
+      r << concat_tokenize (t[i]);
+  else if (is_func (t, BIG, 1) && t[0] == "."); // NOTE: discard old <big|.>
+  else r << t;
+  return r;
+}
+
+array<tree>
+concat_decompose (tree t) {
+  array<tree> r;
+  if (t == "");
+  else if (is_atomic (t)) r << t;
+  else if (is_concat (t))
+    for (int i=0; i<N(t); i++)
+      r << concat_decompose (t[i]);
+  else r << t;
+  return r;
+}
+
+tree
+concat_recompose (array<tree> a) {
+  array<tree> r;
+  string s;
+  for (int i=0; i<N(a); i++)
+    if (is_atomic (a[i])) s << a[i]->label;
+    else {
+      if (s != "") r << tree (s);
+      r << a[i];
+      s= "";
+    }
+  if (s != "") r << tree (s);
+  if (N(r) == 0) return "";
+  else if (N(r) == 1) return r[0];
+  else return tree (CONCAT, r);
+}
+
+/******************************************************************************
+* Determine symbol type
+******************************************************************************/
+
+int
+symbol_type (tree t) {
+  static language lan= math_language ("std-math");
+  if (is_atomic (t)) {
+    int pos= 0;
+    text_property prop= lan->advance (t, pos);
+    switch (prop->op_type) {
+    case OP_PREFIX:
+      return SYMBOL_PREFIX;
+    case OP_POSTFIX:
+      return SYMBOL_POSTFIX;
+    case OP_INFIX:
+    case OP_LEFT_ASS_INFIX:
+    case OP_RIGHT_ASS_INFIX:
+    case OP_ASS_INFIX:
+      return SYMBOL_INFIX;
+    case OP_SEPARATOR:
+      return SYMBOL_SEPARATOR;
+    case OP_OPENING_BRACKET:
+      return SYMBOL_PROBABLE_OPEN;
+    case OP_MIDDLE_BRACKET:
+      return SYMBOL_PROBABLE_MIDDLE;
+    case OP_CLOSING_BRACKET:
+      return SYMBOL_PROBABLE_CLOSE;
+    default:
+      return SYMBOL_BASIC;
+    }
+  }
+  else if (is_func (t, LEFT)) return SYMBOL_OPEN;
+  else if (is_func (t, MID)) return SYMBOL_MIDDLE;
+  else if (is_func (t, RIGHT)) return SYMBOL_CLOSE;
+  else if (is_func (t, BIG, 1) && t[0] == ".") return SYMBOL_CLOSE_BIG;
+  else if (is_func (t, BIG)) return SYMBOL_OPEN_BIG;
+  else if (is_func (t, LSUB)) return SYMBOL_SCRIPT;
+  else if (is_func (t, LSUP)) return SYMBOL_SCRIPT;
+  else if (is_func (t, LPRIME)) return SYMBOL_SCRIPT;
+  else if (is_func (t, RSUB)) return SYMBOL_SCRIPT;
+  else if (is_func (t, RSUP)) return SYMBOL_SCRIPT;
+  else if (is_func (t, RPRIME)) return SYMBOL_SCRIPT;
+  else if (is_func (t, SPACE)) return SYMBOL_SKIP;
+  else if (is_func (t, HSPACE)) return SYMBOL_SKIP;
+  else if (is_func (t, VSPACE)) return SYMBOL_SKIP;
+  else if (is_func (t, VAR_VSPACE)) return SYMBOL_SKIP;
+  else if (is_func (t, LABEL)) return SYMBOL_SKIP;
+  else if (is_compound (t, "text")) return SYMBOL_SKIP;
+  else if (is_compound (t, "eq-number")) return SYMBOL_SKIP;
+  else if (is_compound (t, "bl")) return SYMBOL_OPEN;
+  else if (is_compound (t, "br")) return SYMBOL_CLOSE;
+  else return SYMBOL_BASIC;
+}
+
+array<int>
+symbol_types (array<tree> a) {
+  array<int> tp (N(a));
+  for (int i=0; i<N(a); i++)
+    tp[i]= symbol_type (a[i]);
+  return tp;
+}
+
+/******************************************************************************
+* Determine symbol priority
+******************************************************************************/
+
+#define PRIORITY_SEPARATOR          0
+#define PRIORITY_ASSIGN             1
+#define PRIORITY_FLUX               2
+#define PRIORITY_MODELS             3
+#define PRIORITY_IMPLY              4
+#define PRIORITY_OR                 5
+#define PRIORITY_AND                6
+#define PRIORITY_RELATION           7
+#define PRIORITY_ARROW              8
+#define PRIORITY_UNION              9
+#define PRIORITY_INTERSECTION      10
+#define PRIORITY_PLUS              11
+#define PRIORITY_TIMES             12
+#define PRIORITY_POWER             13
+#define PRIORITY_RADICAL           14
+
+int
+symbol_priority (tree t) {
+  static language lan= math_language ("std-math");
+  if (is_atomic (t)) {
+    string g= lan->get_group (t->label);
+    if (starts (g, "Separator")) return PRIORITY_ASSIGN;
+    if (starts (g, "Assign")) return PRIORITY_ASSIGN;
+    if (starts (g, "Flux")) return PRIORITY_FLUX;
+    if (starts (g, "Models")) return PRIORITY_MODELS;
+    if (starts (g, "Imply")) return PRIORITY_IMPLY;
+    if (starts (g, "Or")) return PRIORITY_OR;
+    if (starts (g, "And")) return PRIORITY_AND;
+    if (starts (g, "Relation")) return PRIORITY_RELATION;
+    if (starts (g, "Arrow")) return PRIORITY_ARROW;
+    if (starts (g, "Union")) return PRIORITY_UNION;
+    if (starts (g, "Exclude")) return PRIORITY_UNION;
+    if (starts (g, "Intersection")) return PRIORITY_INTERSECTION;
+    if (starts (g, "Plus")) return PRIORITY_PLUS;
+    if (starts (g, "Minus")) return PRIORITY_PLUS;
+    if (starts (g, "Times")) return PRIORITY_TIMES;
+    if (starts (g, "Over")) return PRIORITY_TIMES;
+    if (starts (g, "Power")) return PRIORITY_POWER;
+    return PRIORITY_RADICAL;
+  }
+  else if (is_func (t, BIG, 1) and is_atomic (t[0])) {
+    string s= t[0]->label;
+    if (s == "parallel") return PRIORITY_SEPARATOR;
+    if (s == "interleave") return PRIORITY_SEPARATOR;
+    if (s == "vee") return PRIORITY_OR;
+    if (s == "curlyvee") return PRIORITY_OR;
+    if (s == "wedge") return PRIORITY_AND;
+    if (s == "curlywedge") return PRIORITY_AND;
+    if (s == "cup") return PRIORITY_UNION;
+    if (s == "sqcup") return PRIORITY_UNION;
+    if (s == "amalg") return PRIORITY_UNION;
+    if (s == "uplus") return PRIORITY_UNION;
+    if (s == "box") return PRIORITY_UNION;
+    if (s == "cap") return PRIORITY_INTERSECTION;
+    if (s == "sqcap") return PRIORITY_INTERSECTION;
+    if (s == "int") return PRIORITY_PLUS;
+    if (s == "oint") return PRIORITY_PLUS;
+    if (s == "intlim") return PRIORITY_PLUS;
+    if (s == "ointlim") return PRIORITY_PLUS;
+    if (s == "sum") return PRIORITY_PLUS;
+    if (s == "oplus") return PRIORITY_PLUS;
+    if (s == "triangledown") return PRIORITY_PLUS;
+    if (s == "prod") return PRIORITY_TIMES;
+    if (s == "otimes") return PRIORITY_TIMES;
+    if (s == "odot") return PRIORITY_TIMES;
+    if (s == "triangleup") return PRIORITY_TIMES;
+  }
+  else return PRIORITY_RADICAL;
+}
+
+array<int>
+symbol_priorities (array<tree> a) {
+  array<int> tp (N(a));
+  for (int i=0; i<N(a); i++)
+    tp[i]= symbol_priority (a[i]);
+  return tp;
+}
---
+++ src/Data/Tree/tree_analyze.hpp
@@ -0,0 +1,79 @@
+
+/******************************************************************************
+* MODULE     : tree_analyze.hpp
+* DESCRIPTION: routines for analyzing trees
+* COPYRIGHT  : (C) 2010  Joris van der Hoeven
+*******************************************************************************
+* This software falls under the GNU general public license version 3 or later.
+* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+******************************************************************************/
+
+#ifndef TREE_ANALYZE_H
+#define TREE_ANALYZE_H
+#include "drd_std.hpp"
+#include "language.hpp"
+#include "analyze.hpp"
+#include "vars.hpp"
+
+drd_info get_style_drd (tree style);
+
+/******************************************************************************
+* Concatenations
+******************************************************************************/
+
+array<tree> concat_tokenize (tree t);
+array<tree> concat_decompose (tree t);
+tree concat_recompose (array<tree> a);
+
+/******************************************************************************
+* Symbol types
+******************************************************************************/
+
+#define SYMBOL_DELETED           -1
+#define SYMBOL_BASIC              0
+#define SYMBOL_PREFIX             1
+#define SYMBOL_POSTFIX            2
+#define SYMBOL_INFIX              3
+#define SYMBOL_SEPARATOR          4
+#define SYMBOL_SKIP               5
+#define SYMBOL_SCRIPT             6
+#define SYMBOL_OPEN_BIG           7
+#define SYMBOL_CLOSE_BIG          8
+#define SYMBOL_OPEN               9
+#define SYMBOL_MIDDLE            10
+#define SYMBOL_CLOSE             11
+#define SYMBOL_PROBABLE_OPEN     12
+#define SYMBOL_PROBABLE_MIDDLE   13
+#define SYMBOL_PROBABLE_CLOSE    14
+#define SYMBOL_DUBIOUS_OPEN      15
+#define SYMBOL_DUBIOUS_MIDDLE    16
+#define SYMBOL_DUBIOUS_CLOSE     17
+
+int symbol_type (tree t);
+array<int> symbol_types (array<tree> a);
+
+/******************************************************************************
+* Symbol priorities
+******************************************************************************/
+
+#define PRIORITY_SEPARATOR          0
+#define PRIORITY_ASSIGN             1
+#define PRIORITY_FLUX               2
+#define PRIORITY_MODELS             3
+#define PRIORITY_IMPLY              4
+#define PRIORITY_OR                 5
+#define PRIORITY_AND                6
+#define PRIORITY_RELATION           7
+#define PRIORITY_ARROW              8
+#define PRIORITY_UNION              9
+#define PRIORITY_INTERSECTION      10
+#define PRIORITY_PLUS              11
+#define PRIORITY_TIMES             12
+#define PRIORITY_POWER             13
+#define PRIORITY_RADICAL           14
+
+int symbol_priority (tree t);
+array<int> symbol_priorities (array<tree> a);
+
+#endif // defined TREE_ANALYZE_H
