f5f357cb5c03d8176368c75def7519c40d3274ed
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2010-10-15 20:40:51 +0000

    Fix for the Qt/Cocoa Ctrl+Tab bug

--- src/Plugins/MacOS/mac_app.mm
+++ src/Plugins/MacOS/mac_app.mm
@@ -14,6 +14,7 @@
 #include "converter.hpp"
 #include "../Guile/scheme.hpp"
 
+#include <QtGui>
 
 static string 
 from_nsstring (NSString *s) {
@@ -73,3 +74,64 @@ void process_mac_events ()
   } while (true);
   
 }
+
+#if 1
+//HACK:
+// the following code fixes a bug in Qt/Cocoa which do not correctly handle
+// Ctrl+Tab key combination. In particular no QKeyDown event is generated for
+// Shift+Tab and Ctrl+Tab. For this reason we intercept the event at the Cocoa
+// level and just perform manually the translation to equivalent Qt event which
+// is then sent directly to the focused widget. 
+// It is rather simplistic approach but seems to work.
+// Since it is an hack, the filter is  installed  only if we link againts the 
+// bugged version of Qt.
+// This filter is installed in qt_gui.cpp
+// To use the API we need to compile in ObjC 2.0 since blocks are required
+
+NSEvent *mac_handler_body(NSEvent *event) 
+{
+  if (([event type] == NSKeyDown) || ([event type] == NSKeyUp)) {
+    unichar key = [[event charactersIgnoringModifiers] characterAtIndex:0];
+    if ((key == NSTabCharacter) || (key == NSBackTabCharacter)) {
+      NSUInteger nsmods = [event modifierFlags];
+      Qt::KeyboardModifiers modifs = 0;
+      if (key == NSBackTabCharacter) modifs |= Qt::ShiftModifier;
+      if (nsmods &  NSControlKeyMask) modifs |= Qt::MetaModifier;
+      if (nsmods &  NSAlternateKeyMask) modifs |= Qt::AltModifier;
+      if (nsmods &  NSCommandKeyMask) modifs |= Qt::ControlModifier;
+
+#if 0 // DEBUGGING CODE
+      QString str;
+      if (key == NSBackTabCharacter) str.append("Shift+");
+      if (nsmods &  NSControlKeyMask) str.append("Ctrl+");
+      if (nsmods &  NSAlternateKeyMask) str.append("Alt+");
+      if (nsmods &  NSCommandKeyMask) str.append("Meta+");
+      str.append("Tab");
+      cout << str.toAscii().constData() << LF;
+#endif      
+      
+      QKeyEvent *qe = new QKeyEvent(([event type] == NSKeyDown) ? 
+                                        QEvent::KeyPress : QEvent::KeyRelease, 
+                                    Qt::Key_Tab, modifs);
+      QApplication::postEvent(qApp->focusWidget(), qe);
+      return nil;
+      }
+  }
+  return event;
+}
+
+void mac_install_filter() {
+#if NS_BLOCKS_AVAILABLE
+  pool = [[NSAutoreleasePool alloc] init];
+
+  NSEvent * (^mac_handler)(NSEvent * ) = ^ (NSEvent *event) {
+    return mac_handler_body(event);
+  };
+  [NSEvent addLocalMonitorForEventsMatchingMask: NSKeyDownMask | NSKeyUpMask 
+                                        handler:mac_handler];
+  [pool release];
+  
+#endif
+}
+
+#endif // HACK
