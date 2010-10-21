4b4bdedc82b74e614515505bf2739e90c958d73a
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2010-10-21 20:28:38 +0000

    Math-brackets package

---
+++ TeXmacs/packages/utilities/math-brackets.ts
@@ -0,0 +1,32 @@
+<TeXmacs|1.0.7.7>
+
+<style|source>
+
+<\body>
+  <active*|<\src-title>
+    <src-package|math-brackets|1.0>
+
+    <\src-purpose>
+      Semantic checking of brackets
+    </src-purpose>
+
+    <src-copyright|2010|Joris van der Hoeven>
+
+    <\src-license>
+      This software falls under the <hlink|GNU general public license,
+      version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+      WARRANTY WHATSOEVER. You should have received a copy of the license
+      which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+    </src-license>
+  </src-title>>
+
+  <assign|math-nesting-mode|colored>
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
