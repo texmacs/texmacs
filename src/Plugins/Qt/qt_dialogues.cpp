c52ec2994220eadbbf403fc91436ee799f06a55c
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2010-11-07 22:15:51 +0000

    Update

--- src/Plugins/Qt/qt_dialogues.cpp
+++ src/Plugins/Qt/qt_dialogues.cpp
@@ -596,7 +596,8 @@ inputs_list_widget (command call_back, array<string> prompts) {
 }
 
 widget
-input_text_widget (command call_back, string type, array<string> def) {
+input_text_widget (command call_back, string type,
+		   array<string> def, string width) {
   // a textual input widget for input of a given type and a list of suggested
   // default inputs (the first one should be displayed, if there is one)
   return tm_new<qt_input_text_widget_rep> (call_back, type, def);
