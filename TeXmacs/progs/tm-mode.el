7bbd620fee8c281ba4bdbb6ca455dc91c555bacb
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2011-09-04 22:32:47 +0000

    Add minibar for cell actions

--- TeXmacs/progs/tm-mode.el
+++ TeXmacs/progs/tm-mode.el
@@ -27,6 +27,7 @@
     delayed dialogue on-entry on-exit widget-delayed
     association-tile bar concat dense-bar dense-tile document
     header-bar sequence short-bar short-tile
+    minibar
     wrap-selection-any wrap-selection-small
     try-modification
     $begin $cond))
