c313312bbd7b8095893b741e0cf9e8a20c5bcba6
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2011-09-20 17:10:24 +0000

    Extra math and graphics icons

--- src/System/Boot/init_texmacs.cpp
+++ src/System/Boot/init_texmacs.cpp
@@ -250,8 +250,8 @@ init_env_vars () {
   (void) get_env_path ("TEXMACS_PIXMAP_PATH",
 		       "$TEXMACS_HOME_PATH/misc/pixmaps" |
 		       url ("$TEXMACS_PATH/misc/pixmaps/modern/24x24/main") |
-		       //url ("$TEXMACS_PATH/misc/pixmaps/modern/20x20/mode") |
-		       //url ("$TEXMACS_PATH/misc/pixmaps/modern/16x16/focus") |
+		       url ("$TEXMACS_PATH/misc/pixmaps/modern/20x20/mode") |
+		       url ("$TEXMACS_PATH/misc/pixmaps/modern/16x16/focus") |
 		       url ("$TEXMACS_PATH/misc/pixmaps/traditional/--x17") |
 		       plugin_path ("misc/pixmaps"));
   (void) get_env_path ("TEXMACS_DIC_PATH",
