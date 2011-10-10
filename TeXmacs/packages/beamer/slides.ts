11a1b064c86d83dbc7f43cf9d02c97affd66308f
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2011-10-10 20:01:00 +0000

    Remove background images when making slides

--- TeXmacs/packages/beamer/slides.ts
+++ TeXmacs/packages/beamer/slides.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.0.7.6>
+<TeXmacs|1.0.7.13>
 
 <style|source>
 
@@ -29,6 +29,45 @@
   <assign|bg-color|white>
 
   <assign|page-medium|paper>
+
+  <\active*>
+    <\src-comment>
+      Ornaments with no images
+    </src-comment>
+  </active*>
+
+  <assign|ornament-border|2ln>
+
+  <assign|ornament-color|#e0b050>
+
+  <assign|ornament-sunny-color|#f0e0c0>
+
+  <assign|ornament-shadow-color|#c07055>
+
+  <assign|ornament-hpadding|1spc>
+
+  <assign|ornament-vpadding|1spc>
+
+  \;
+
+  <assign|granite|<macro|x|<with|ornament-color|#101010|color|white|strong-color|#f0ffb0|math-color|#ffd4c0|ornament-sunny-color|light
+  grey|ornament-shadow-color|dark grey|<ornament|<arg|x>>>>>
+
+  <assign|manila-paper|<macro|x|<with|ornament-color|#d0d0c0|ornament-sunny-color|#e8e8e0|ornament-shadow-color|#acac90|<ornament|<arg|x>>>>>
+
+  <assign|metal|<macro|x|<with|ornament-color|#c0c0d0|ornament-sunny-color|#e0e0e8|ornament-shadow-color|#9090ac|<ornament|<arg|x>>>>>
+
+  <assign|pine|<macro|x|#0c3000>>
+
+  <assign|ridged-paper|<macro|x|<with|ornament-color|#e8dcdc|ornament-sunny-color|#f0e0e0|ornament-shadow-color|#d0a0a0|<ornament|<arg|x>>>>>
+
+  <assign|rough-paper|<macro|x|<with|ornament-color|#dcdcdc|ornament-sunny-color|#e0e0e0|ornament-shadow-color|#a0a0a0|<ornament|<arg|x>>>>>
+
+  \;
+
+  <assign|tit|<macro|body|<with|ornament-color|#f0f0d8|<ornament|<with|font-series|bold|color|dark
+  brown|><htab|5mm><move|<with|font-series|bold|math-font-series|bold|<large|<with|color|dark
+  brown|<arg|body>>>>|0fn|0.333fn><htab|5mm>>>>>
 
   <\active*>
     <\src-comment>
