84a8777eb08e2356081c090d3d682ad1a9c2371d
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2011-11-04 18:04:30 +0000

    more fixes, now guile should always compile...

--- misc/tm-devel-mac/src/guile.mk
+++ misc/tm-devel-mac/src/guile.mk
@@ -7,11 +7,13 @@ PKG             := guile
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.8.8
 $(PKG)_CHECKSUM := 548d6927aeda332b117f8fc5e4e82c39a05704f9
+#$(PKG)_VERSION  := 1.8.7
+#$(PKG)_CHECKSUM := 24cd2f06439c76d41d982a7384fe8a0fe5313b54
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_WEBSITE  := http://www.gnu.org/software/$(PKG)/
 $(PKG)_URL      := http://ftp.gnu.org/gnu/$(PKG)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gmp gettext libtool
+$(PKG)_DEPS     := gmp gettext libtool libiconv libffi readline
 #gcc libtool gmp libiconv gettext libunistring gc libffi readline
 
 # these options prevents some misbehaviour in configure due to cross-compilation to ppc from i386
@@ -32,22 +34,26 @@ define $(PKG)_BUILD_ARCH
     # The setting "scm_cv_struct_timespec=no" ensures that Guile
     # won't try to use the "struct timespec" from <pthreads.h>,
     # which would fail because we tell Guile not to use Pthreads.
+#        scm_cv_struct_timespec=no \
+#        --build='$(HOST)' \
+
     cd '$(1)' && \
-        PKG_CONFIG_PATH='$(PREFIX)/lib/pkgconfig/'  \
+        PKG_CONFIG_PATH='$(PREFIX)/lib/pkgconfig/' \
         ./configure \
         --host='$(TARGET_$(3))' \
-        --build=$(HOST) \
         --prefix='$(PREFIX)' \
         --disable-shared \
         --without-threads \
 	    --with-sysroot='$(MACOS_SDK)'  \
-        scm_cv_struct_timespec=no \
+	    CC="gcc -arch i386" CXX="g++ -arch i386" CPP="gcc -E" CXXCPP="g++ -E" \
         CFLAGS='$(BASE_CFLAGS)'\
-        LDFLAGS='$(BASE_LDFLAGS)'\
+        LDFLAGS='$(BASE_LDFLAGS)' \
         $(MORE_OPT_$(3))
 
-    $(MAKE) -C '$(1)' -j '$(JOBS)' schemelib_DATA=
-    $(MAKE) -C '$(1)' -j 1 install schemelib_DATA=
+#    $(MAKE) -C '$(1)' -j '$(JOBS)' schemelib_DATA=
+#    $(MAKE) -C '$(1)' -j 1 install schemelib_DATA=
+    $(MAKE) -C '$(1)' -j '$(JOBS)' 
+    $(MAKE) -C '$(1)' -j 1 install 
 
 #         LIBS='-lunistring -lintl -liconv' \
 
