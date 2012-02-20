3a9845955752545f33bb840d2a25ea653f09893f
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2012-02-20 22:54:42 +0000

    Several well separated glyphs at once

--- src/Plugins/Widkit/Misc/ink_widget.cpp
+++ src/Plugins/Widkit/Misc/ink_widget.cpp
@@ -203,8 +203,8 @@ register_glyph (string name, contours gl) {
   //cout << "Added " << name << ", " << disc1 << "\n";
 }
 
-string
-recognize_glyph (contours gl) {
+void
+recognize_glyph_one (contours gl, int& level, string& best, double& best_rec) {
   array<tree>   disc1;
   array<double> cont1;
   invariants (gl, 1, disc1, cont1);
@@ -212,9 +212,9 @@ recognize_glyph (contours gl) {
   array<double> cont2;
   invariants (gl, 2, disc2, cont2);
 
-  string best= "";
+  best= "";
+  best_rec= -100.0;
   int    best_i= -1;
-  double best_rec= -100.0;
   for (int i=0; i<N(learned_names); i++)
     if (N(learned_glyphs[i]) == N(gl) && disc1 == learned_disc1[i]) {
       string        name= learned_names[i];
@@ -227,7 +227,8 @@ recognize_glyph (contours gl) {
   if (best != "") {
     //cout << "disc= " << disc1 << "\n";
     //cout << "cont= " << cont1 << "\n";
-    return best;
+    level= 1;
+    return;
   }
 
   for (int i=0; i<N(learned_names); i++)
@@ -239,7 +240,35 @@ recognize_glyph (contours gl) {
       if (rec > best_rec) { best_rec= rec; best= name; }
       //cout << name << ": " << 100.0 * rec << "%\n";
     }
-  return best;
+  level= 2;
+}
+
+bool
+attached (poly_line pl1, poly_line pl2) {
+  point p1= inf (pl1), q1= sup (pl1);
+  point p2= inf (pl2), q2= sup (pl2);
+  //cout << "<< " << p1 << ", " << q1 << "\n";
+  //cout << ">> " << p2 << ", " << q2 << "\n";
+  if (p2[1] > q1[1]) return true;
+  if (p2[0] > q1[0]) return false;
+  return true;
+}
+
+string
+recognize_glyph (contours gl) {
+  string r;
+  for (int i=0; i<N(gl); ) {
+    int eat= 1;
+    while (i+eat < N(gl) && attached (gl[i+eat-1], gl[i+eat])) eat++;
+    int    lev= 3;
+    string subr= "";
+    double rec= -100.0;
+    recognize_glyph_one (range (gl, i, i+eat), lev, subr, rec);
+    r << subr;
+    i += eat;
+    //cout << "Add " << eat << "\n";
+  }
+  return r;
 }
 
 /******************************************************************************
