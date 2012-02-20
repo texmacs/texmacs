92a19ebd982e4ed3deda0784d0ce6676bfba501f
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2012-02-20 10:53:24 +0000

    Simplifications

--- src/Plugins/Widkit/Misc/ink_widget.cpp
+++ src/Plugins/Widkit/Misc/ink_widget.cpp
@@ -14,41 +14,11 @@
 #include "Scheme/object.hpp"
 
 /******************************************************************************
-* Ink points
+* Poly lines
 ******************************************************************************/
 
-class ink_point_rep: concrete_struct {
-public:
-  double x;
-  double y;
-  double p; // pressure
-  inline ink_point_rep (double x2, double y2, double p2):
-    x (x2), y (y2), p (p2) {}
-  friend class ink_point;
-};
-
-class ink_point {
-CONCRETE(ink_point);
-  inline ink_point (double x= 0.0, double y= 0.0, double p= 1.0) {
-    rep= tm_new<ink_point_rep> (x, y, p); }
-  inline operator tree () {
-    return tree (TUPLE,
-                 as_string (rep->x), as_string (rep->y),
-                 as_string (rep->p)); }
-};
-CONCRETE_CODE(ink_point);
-
-tm_ostream&
-operator << (tm_ostream& out, ink_point p) {
-  out << "[ " << p->x << ", " << p->y << " ]";
-  return out;
-}
-
-/******************************************************************************
-* Ink shapes
-******************************************************************************/
-
-typedef array<ink_point> ink_shape;
+typedef array<double> point;
+typedef array<point> poly_line;
 
 inline bool
 small (double x, double y) {
@@ -56,12 +26,12 @@ small (double x, double y) {
 }
 
 bool
-nearby (ink_shape sh, double x, double y) {
+nearby (poly_line sh, double x, double y) {
   //cout << "Check " << x << ", " << y << "\n";
   for (int i=0; i<N(sh)-1; i++) {
     double t = 0.0;
-    double x1= sh[i]->x, x2= sh[i+1]->x;
-    double y1= sh[i]->y, y2= sh[i+1]->y;
+    double x1= sh[i][0], x2= sh[i+1][0];
+    double y1= sh[i][1], y2= sh[i+1][1];
     //cout << "  " << x1 << ", " << y1 << "; " << x2 << ", " << y2 << "\n";
     if (x1 == x2 && y1 == y2 && small (x - x1, y - y1)) return true;
     if (abs (x2 - x1) >= abs (y2 - y1)) t= (x - x1) / (x2 - x1);
@@ -84,7 +54,7 @@ nearby (ink_shape sh, double x, double y) {
 
 class ink_widget_rep: public attribute_widget_rep {
   command cb;
-  array<ink_shape> shs;
+  array<poly_line> shs;
   bool dragging;
 
 public:
@@ -126,20 +96,20 @@ ink_widget_rep::handle_repaint (repaint_event ev) { (void) ev;
   ren->set_color (black);
   ren->set_line_style (2 * PIXEL);
   for (int i=0; i<N(shs); i++) {
-    ink_shape sh= shs[i];
+    poly_line sh= shs[i];
     int n= N(sh);
     if (n == 1) {
       array<SI> x (2);
       array<SI> y (2);
-      x[0]= x[1]= (SI) (sh[0]->x * PIXEL);
-      y[0]= y[1]= (SI) (sh[0]->y * PIXEL);
+      x[0]= x[1]= (SI) (sh[0][0] * PIXEL);
+      y[0]= y[1]= (SI) (sh[0][1] * PIXEL);
     }
     else if (n>1) {
       array<SI> x (n);
       array<SI> y (n);
       for (int j=0; j<n; j++) {
-        x[j]= (SI) (sh[j]->x * PIXEL);
-        y[j]= (SI) (sh[j]->y * PIXEL);
+        x[j]= (SI) (sh[j][0] * PIXEL);
+        y[j]= (SI) (sh[j][1] * PIXEL);
       }
       ren->lines (x, y);
     }
@@ -149,13 +119,13 @@ ink_widget_rep::handle_repaint (repaint_event ev) { (void) ev;
 void
 ink_widget_rep::refresh_last () {
   if (N(shs) > 0) {
-    ink_shape& sh= shs [N(shs)-1];
-    ink_point& p = sh [max (0, N(sh)-2)];
-    ink_point& q = sh [N(sh)-1];
-    SI x1= min (p->x, q->x) * PIXEL;
-    SI y1= min (p->y, q->y) * PIXEL;
-    SI x2= max (p->x, q->x) * PIXEL;
-    SI y2= max (p->y, q->y) * PIXEL;
+    poly_line& sh= shs [N(shs)-1];
+    point& p = sh [max (0, N(sh)-2)];
+    point& q = sh [N(sh)-1];
+    SI x1= min (p[0], q[0]) * PIXEL;
+    SI y1= min (p[1], q[1]) * PIXEL;
+    SI x2= max (p[0], q[0]) * PIXEL;
+    SI y2= max (p[1], q[1]) * PIXEL;
     this << emit_invalidate (x1 - PIXEL, y1 - PIXEL, x2 + PIXEL, y2 + PIXEL);
   }
 }
@@ -169,7 +139,7 @@ ink_widget_rep::handle_mouse (mouse_event ev) {
   //cout << type << ", " << x/PIXEL << ", " << y/PIXEL << "\n";
   if (erase) {
     int n= N(shs);
-    array<ink_shape> nshs;
+    array<poly_line> nshs;
     for (int i=0; i<N(shs); i++)
       if (!nearby (shs[i], x/PIXEL, y/PIXEL))
         nshs << shs[i];
@@ -180,24 +150,24 @@ ink_widget_rep::handle_mouse (mouse_event ev) {
     }
   }
   else if (type == "press-left") {
-    ink_shape sh (0);
-    sh << ink_point (x/PIXEL, y/PIXEL);
+    poly_line sh (0);
+    sh << point (x/PIXEL, y/PIXEL);
     shs << sh;
     refresh_last ();
     dragging= true;
   }
   else if (type == "leave" && (ev->x < 0 || ev->x >= w)) {
     if (ev->x >= w) cb (list_object (object (true)));
-    shs= array<ink_shape> (0);
+    shs= array<poly_line> (0);
     this << emit_invalidate_all ();
     if (ev->x < 0) commit ();
   }
   else if (type == "move" || type == "release-left" || type == "leave")
     if (dragging && N(shs) > 0) {
-      ink_shape& sh= shs [N(shs)-1];
-      ink_point& p = sh [N(sh)-1];
-      if (p->x != (x/PIXEL) || p->y != (y/PIXEL)) {
-        sh << ink_point (x/PIXEL, y/PIXEL);
+      poly_line& sh= shs [N(shs)-1];
+      point& p = sh [N(sh)-1];
+      if (p[0] != (x/PIXEL) || p[1] != (y/PIXEL)) {
+        sh << point (x/PIXEL, y/PIXEL);
         refresh_last ();
       }
       if (type != "move") {
@@ -211,10 +181,10 @@ void
 ink_widget_rep::commit () {
   object l= null_object ();
   for (int k= N(shs)-1; k>=0; k--) {
-    ink_shape sh= shs[k];
+    poly_line sh= shs[k];
     object obj= null_object ();
     for (int i=N(sh)-1; i>=0; i--) {
-      object p= list_object (object (sh[i]->x), object (sh[i]->y));
+      object p= list_object (object (sh[i][0]), object (sh[i][1]));
       obj= cons (p, obj);
     }
     l= cons (obj, l);
