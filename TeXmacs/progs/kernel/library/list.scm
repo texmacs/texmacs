da9276e992ddde88867b8090763bb839773e8971
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2012-03-22 16:55:15 +0000

    Add routine list-delete

--- TeXmacs/progs/kernel/library/list.scm
+++ TeXmacs/progs/kernel/library/list.scm
@@ -149,6 +149,12 @@
   "Extract sublist from @l beginning at @i and ending at @j."
   (list-head (list-tail l i) (- j i)))
 
+(define-public (list-delete l x)
+  "Remove all occurrences of @x from list @l."
+  (cond ((null? l) l)
+        ((== (car l) x) (list-delete (cdr l) x))
+        (else (cons (car l) (list-delete (cdr l) x)))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Circulating lists
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
