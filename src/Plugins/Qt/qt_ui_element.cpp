e8adea513906b66ed2d306b0c9a30bc9da4f9cba
Miguel de Benito <nonick@8027.org>
2012-08-29 13:18:13 +0000

    Qt: Fix size of labels to allow centering.

--- src/Plugins/Qt/qt_ui_element.cpp
+++ src/Plugins/Qt/qt_ui_element.cpp
@@ -1140,7 +1140,7 @@ qt_ui_element_rep::as_qwidget () {
       //w->setTextFormat(Qt::RichText);
       w->setText(to_qstring (t));
       w->setStyleSheet(style);
-      w->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
+      w->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
       // Workaround too small sizeHint() when the text has letters with descent:
       w->setMinimumHeight(w->fontMetrics().height());
       qwid = w;
