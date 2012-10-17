1d0f36b20f30f8db78af49b91bab00bd68aa7db5
Miguel de Benito <nonick@8027.org>
2012-10-17 12:01:44 +0000

    Fix default save format (thanks to Simon Zack)

--- TeXmacs/progs/texmacs/texmacs/tm-files.scm
+++ TeXmacs/progs/texmacs/texmacs/tm-files.scm
@@ -105,7 +105,7 @@
     (cond ((url-scratch? name)
            (choose-file
              (lambda (x) (apply save-buffer-as-main (cons x opts)))
-             "Save TeXmacs file" ""))
+             "Save TeXmacs file" "texmacs"))
           ((not (buffer-exists? name))
            (with msg `(concat "The buffer " ,vname " does not exist")
              (set-message msg "Save file")))
