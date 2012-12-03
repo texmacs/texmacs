bfe0d6d97c31d5f8ec857b01ea4c66d77879a79e
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2012-12-03 11:08:08 +0000

    Temporary fix to limit of 256 open file descriptors under MacOS

--- src/Texmacs/Texmacs/texmacs.cpp
+++ src/Texmacs/Texmacs/texmacs.cpp
@@ -396,6 +396,32 @@ TeXmacs_main (int argc, char** argv) {
 * Main program
 ******************************************************************************/
 
+#ifdef OS_MACOS
+#include <sys/resource.h>
+#endif
+
+void
+boot_hacks () {
+#ifdef OS_MACOS
+// NOTE: under MACOS, there is a limited number of open file descriptors,
+// by default 256.  Any open file descriptor can actually count several times
+// whenever the files is stored in various chunks on disk.  Hence, the limit
+// is easily exceeded, although this situation cannot easily be debugged.
+// Our current hack is to allow for at least 4096 open file descriptors.
+  rlimit lims;
+  getrlimit (RLIMIT_NOFILE, &lims);
+  lims.rlim_cur= max (lims.rlim_cur, 4096);
+  setrlimit (RLIMIT_NOFILE, &lims);
+  //getrlimit (RLIMIT_NOFILE, &lims);
+  //printf ("cur: %i\n", lims.rlim_cur);
+  //printf ("max: %i\n", lims.rlim_max);
+#endif
+}
+
+/******************************************************************************
+* Main program
+******************************************************************************/
+
 void
 immediate_options (int argc, char** argv) {
   if (get_env ("TEXMACS_HOME_PATH") == "")
@@ -445,6 +471,7 @@ immediate_options (int argc, char** argv) {
 
 int
 main (int argc, char** argv) {
+  boot_hacks ();
   windows_delayed_refresh (1000000000);
   immediate_options (argc, argv);
   set_env ("LC_NUMERIC", "POSIX");
