4719c6db160cc0f6c6cd11e5b22afcc6f057885c
Denis Raux <raux@texmacs.org>
2013-01-30 15:58:48 +0000

    remove echo display

--- plugins/python/bin/tm_python.bat
+++ plugins/python/bin/tm_python.bat
@@ -1,3 +1,4 @@
-: denis raux LIX 2013
-: windows launcher python interpreter with wrapper script
+@echo off
+rem denis raux LIX 2013
+rem windows launcher python interpreter with wrapper script
 python %~p0\tm_python %1 %2 %3 %4
