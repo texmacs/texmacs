e7e2b300e23a8c460daf5956cda055ca59c24e57
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2013-02-08 20:52:53 +0000

    Fixes

--- plugins/r/progs/init-r.scm
+++ plugins/r/progs/init-r.scm
@@ -13,16 +13,15 @@
 
 (texmacs-modes
   (in-r% (== (get-env "prog-language") "r"))
-  (in-prog-r% #t in-prog% in-r%)
-)
+  (in-prog-r% #t in-prog% in-r%))
 
 (lazy-keyboard (r-edit) in-prog-r?)
 
 (define (r-initialize)
   (import-from (utils plugins plugin-convert))
   (lazy-input-converter (r-input) r)
-  (plugin-eval "r" "default" "b=1:100\n")
-)
+  ;;(plugin-eval "r" "default" "b=1:100\n")
+  )
 
 (define (r-serialize lan t)
   (import-from (utils plugins plugin-cmd))
@@ -31,41 +30,11 @@
       (string-append (escape-verbatim 
 		      (string-replace s "\n" ";;")) "\n"))))
 
-
-(define (maxima-versions)
-  ;;(system "maxima_detect")
-  (with version-list (string->object (maxima-detect))
-    ;;(display* "Maxima versions -> " version-list "\n")
-    (if (list? version-list)
-        (let* ((default (car version-list))
-               (rest (cdr version-list))
-               (launch-default
-                (list :launch (string-append "tm_maxima " default)))
-               (launch-rest
-                (map
-                 (lambda (version-name)
-                   (list :launch version-name
-                         (string-append "tm_maxima " version-name)))
-                 rest)))
-          (cons launch-default launch-rest))
-        '())))
-
-
-
-
-
-(define r-session-to-launch "1")
-
-(define (r-sessions)
-  (cons (list :launch (string-append "tm_r " r-session-to-launch)) '() ) )
-
-
 (plugin-configure r
   (:require (url-exists-in-path? "R"))
   (:initialize (r-initialize))
   (:serializer ,r-serialize)
   (:launch "tm_r")
-;;  ,@(r-sessions)
   (:tab-completion #t)
   (:session "R")
   (:scripts "R"))
@@ -73,9 +42,9 @@
 (menu-bind r-menu
   ("update menu" (insert "t.update.menus(max.len=30)"))
   ("R help in TeXmacs" (insert "t.start.help()"))
-  ("R help in TeXmacs 2" (plugin-eval "r" "default" "1:100\n"))
-)
+  ;;("R help in TeXmacs 2" (plugin-eval "r" "default" "1:100\n"))
+  )
 
 (menu-bind plugin-menu
-;;  (:require (in-r?))
+  (:require (in-r?))
   (=> "R" (link r-menu)))
