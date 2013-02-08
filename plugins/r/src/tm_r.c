068274a6b83e2050c020ddcd3a3cdd753773ad25
Michael Lachmann Tamarlin <ghostwheel>
2013-02-08 17:04:45 +0000

    Just a test of svn

--- plugins/r/src/tm_r.c
+++ plugins/r/src/tm_r.c
@@ -7,6 +7,7 @@
 * This software falls under the GNU general public license version 3 or later.
 * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
 * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+*
 ******************************************************************************/
 
 #include <stdio.h>
