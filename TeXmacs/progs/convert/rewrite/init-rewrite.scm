7ad4d0988c76bac5e801b3b85bd00298ce76df99
Miguel de Benito <nonick@8027.org>
2013-02-08 00:55:19 +0000

    New cpp file format fixes loading of C++ files

--- TeXmacs/progs/convert/rewrite/init-rewrite.scm
+++ TeXmacs/progs/convert/rewrite/init-rewrite.scm
@@ -100,6 +100,36 @@
 
 (converter scheme-snippet texmacs-tree
   (:function scheme-snippet->texmacs))
+  
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C++ source files
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-format cpp
+  (:name "C++ source")
+  (:suffix "cpp" "cc" "hpp" "hh"))
+
+(define (texmacs->cpp x . opts)
+  (texmacs->verbatim x (acons "texmacs->verbatim:encoding" "SourceCode" '())))
+
+(define (cpp->texmacs x . opts)
+  (verbatim->texmacs x (acons "verbatim->texmacs:encoding" "SourceCode" '())))
+
+(define (cpp-snippet->texmacs x . opts)
+  (verbatim-snippet->texmacs x 
+    (acons "verbatim->texmacs:encoding" "SourceCode" '())))
+
+(converter texmacs-tree cpp-document
+  (:function texmacs->cpp))
+
+(converter cpp-document texmacs-tree
+  (:function cpp->texmacs))
+  
+(converter texmacs-tree cpp-snippet
+  (:function texmacs->cpp))
+
+(converter cpp-snippet texmacs-tree
+  (:function cpp-snippet->texmacs))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Verbatim
