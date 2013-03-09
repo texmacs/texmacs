576d696adbf0c153bb38ea77144a0906ab3550dc
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2013-03-09 22:14:55 +0000

    Various debugging facilities

--- TeXmacs/progs/fonts/font-selector.scm
+++ TeXmacs/progs/fonts/font-selector.scm
@@ -17,6 +17,48 @@
   (:use (kernel gui menu-widget)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Inserting font tables
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (build-font-cells r c1 c2 c)
+  (cond ((== c -1)
+         (cons `(cell ,(integer->hexadecimal r))
+               (build-font-cells r c1 c2 0)))
+        ((>= c 16) (list))
+        ((or (< c c1) (> c c2))
+         (cons `(cell "") (build-font-cells r c1 c2 (+ c 1))))
+        (else
+          (let* ((i (+ (* 16 r) c))
+                 (hex (integer->hexadecimal i))
+                 (s (string-append "<#" hex ">")))
+            (cons `(cell ,s) (build-font-cells r c1 c2 (+ c 1)))))))
+
+(define (build-font-row r c1 c2)
+  `(row ,@(build-font-cells r c1 c2 -1)))
+
+(define (build-font-table i1 i2)
+  (let* ((r1 (quotient i1 16))
+         (r2 (quotient i2 16))
+         (c1 (remainder i1 16))
+         (c2 (remainder i2 16))
+         (hc (lambda (i) `(cell ,(integer->hexadecimal i))))
+         (fr `(row (cell "") ,@(map hc (.. 0 16)))))
+    (if (== r1 r2)
+        `(block (tformat (table ,fr
+                                ,(build-font-row r1 c1 c2))))
+        `(block (tformat (table ,fr
+                                ,(build-font-row r1 c1 15)
+                                ,@(map (cut build-font-row <> 0 15)
+                                       (.. (+ r1 1) r2))
+                                ,(build-font-row r2 0 c2)))))))
+
+(tm-define (insert-font-table hexa-start hexa-end)
+  (:argument hexa-start "First unicode character in hexadecimal")
+  (:argument hexa-end "Last unicode character in hexadecimal")
+  (insert (build-font-table (hexadecimal->integer hexa-start)
+                            (hexadecimal->integer hexa-end))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Global state of font selector
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -84,6 +126,30 @@
 	  (else
 	   (string-append "  (closest match: " (car fn2) " " (cadr fn2) ")")))))
 
+(define standard-selector-text
+  `(document
+     "abcdefghij, ABCDEFGHIJ, 0123456789"
+     ,(string-append "+-*/=<less><gtr> ([{|}]) :;!? "
+                     "אחימץü <alpha><beta><gamma><delta><epsilon> "
+                     "<leq><geq><leqslant><geqslant><prec><succ> "
+                     "<rightarrow><Rightarrow><mapsto> "
+                     "<times><cdot><oplus><otimes>")))
+
+(define current-selector-text standard-selector-text)
+
+(tm-define (set-selector-text)
+  (set! current-selector-text
+        (or (and (selection-active-any?)
+                 (tree->stree (selection-tree)))
+            (tree->stree (buffer-tree)))))
+
+(tm-define (set-selector-table hexa-start hexa-end)
+  (:argument hexa-start "First unicode character in hexadecimal")
+  (:argument hexa-end "Last unicode character in hexadecimal")
+  (set! current-selector-text
+        (build-font-table (hexadecimal->integer hexa-start)
+                          (hexadecimal->integer hexa-end))))
+
 (define (selector-font-demo-text)
   (with fn (selector-get-font)
     ;;(display* "Font: " fn "\n")
@@ -99,13 +165,7 @@
          "font-series" ,(logical-font-series fn)
          "font-shape" ,(logical-font-shape fn)
          "font-base-size" ,selector-font-size
-         (document
-           "abcdefghij, ABCDEFGHIJ, 0123456789"
-           ,(string-append "+-*/=<less><gtr> ([{|}]) :;!? "
-                           "אחימץü <alpha><beta><gamma><delta><epsilon> "
-                           "<leq><geq><leqslant><geqslant><prec><succ> "
-                           "<rightarrow><Rightarrow><mapsto> "
-                           "<times><cdot><oplus><otimes>"))))))
+         ,current-selector-text))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Global state for font searching
@@ -252,9 +312,14 @@
     (text (selector-font-simulate-comment))
     >>>)
   ===
-  (resize ("300px" "300px" "2000px") ("100px" "100px" "100px")
-    (scrollable
-      (link font-sample-text))))
+  (if (== current-selector-text standard-selector-text)
+      (resize ("300px" "300px" "2000px") ("100px" "100px" "100px")
+        (scrollable
+          (link font-sample-text))))
+  (if (!= current-selector-text standard-selector-text)
+      (resize ("300px" "300px" "2000px") ("500px" "500px" "500px")
+        (scrollable
+          (link font-sample-text)))))
 
 (tm-widget (font-selector quit)
   (padded
