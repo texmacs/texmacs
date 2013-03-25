21bbf9b0e8f54e9f540deab18e6734d34bf33f92
François Poulain <fpoulain@metrodore.fr>
2013-03-25 15:20:27 +0000

    TeXmacs styles: adding dummy IEEEconf and IEEEtran, for LaTeX export

---
+++ TeXmacs/styles/ieee/ieeeconf.ts
@@ -0,0 +1,38 @@
+<TeXmacs|1.0.7.17>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|ieeeconf|0.1>
+
+      <\src-purpose>
+        The IEEEconf style.
+      </src-purpose>
+
+      <\src-copyright|2013>
+        François Poulain, Joris van der Hoeven
+      </src-copyright>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
+    </src-title>
+  </active*>
+
+  <active*|<src-comment|IEEEconf is temporarily a version of article.>>
+
+  <use-package|article>
+
+  \ <active*|>
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
