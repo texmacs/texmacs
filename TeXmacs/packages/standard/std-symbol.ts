31d8e4b7a7847dd2023797069f333e14441c9099
François Poulain <fpoulain@metrodore.fr>
2013-04-16 09:10:22 +0000

    std-symbol.ts: Making them working in math mode

--- TeXmacs/packages/standard/std-symbol.ts
+++ TeXmacs/packages/standard/std-symbol.ts
@@ -21,41 +21,41 @@
     </src-license>
   </src-title>>
 
-  <assign|cent|<macro|<active*|<with|font|tc|¢>>>>
+  <assign|cent|<macro|<active*|<with|mode|text|font|tc|¢>>>>
 
-  <assign|currency|<macro|<active*|<with|font|tc|¤>>>>
+  <assign|currency|<macro|<active*|<with|mode|text|font|tc|¤>>>>
 
-  <assign|yen|<macro|<active*|<with|font|tc|¥>>>>
+  <assign|yen|<macro|<active*|<with|mode|text|font|tc|¥>>>>
 
-  <assign|copyright|<macro|<active*|<with|font|tcx|©>>>>
+  <assign|copyright|<macro|<active*|<with|mode|text|font|tcx|©>>>>
 
-  <assign|copyleft|<macro|<active*|<with|font|tcx|«>>>>
+  <assign|copyleft|<macro|<active*|<with|mode|text|font|tcx|«>>>>
 
-  <assign|registered|<macro|<active*|<with|font|tcx|®>>>>
+  <assign|registered|<macro|<active*|<with|mode|text|font|tcx|®>>>>
 
-  <assign|degreesign|<macro|<active*|<with|font|tc|°>>>>
+  <assign|degreesign|<macro|<active*|<with|mode|text|font|tc|°>>>>
 
-  <assign|twosuperior|<macro|<active*|<with|font|tc|²>>>>
+  <assign|twosuperior|<macro|<active*|<with|mode|text|font|tc|²>>>>
 
-  <assign|threesuperior|<macro|<active*|<with|font|tc|³>>>>
+  <assign|threesuperior|<macro|<active*|<with|mode|text|font|tc|³>>>>
 
-  <assign|onesuperior|<macro|<active*|<with|font|tc|¹>>>>
+  <assign|onesuperior|<macro|<active*|<with|mode|text|font|tc|¹>>>>
 
-  <assign|mu|<macro|<active*|<with|font|tcx|µ>>>>
+  <assign|mu|<macro|<active*|<with|mode|text|font|tcx|µ>>>>
 
-  <assign|paragraphsign|<active*|<macro|<with|font|tc|¶>>>>
+  <assign|paragraphsign|<active*|<macro|<with|mode|text|font|tc|¶>>>>
 
-  <assign|onequarter|<macro|<active*|<with|font|tc|¼>>>>
+  <assign|onequarter|<macro|<active*|<with|mode|text|font|tc|¼>>>>
 
-  <assign|onehalf|<macro|<active*|<with|font|tc|½>>>>
+  <assign|onehalf|<macro|<active*|<with|mode|text|font|tc|½>>>>
 
-  <assign|threequarters|<macro|<active*|<with|font|tc|¾>>>>
+  <assign|threequarters|<macro|<active*|<with|mode|text|font|tc|¾>>>>
 
-  <assign|euro|<macro|<active*|<with|font|tcx|¿>>>>
+  <assign|euro|<macro|<active*|<with|mode|text|font|tcx|¿>>>>
 
-  <assign|trademark|<macro|<active*|<with|font|tcx|—>>>>
+  <assign|trademark|<macro|<active*|<with|mode|text|font|tcx|—>>>>
 
-  <assign|emdash|<macro|<active*|<with|font|roman|\V>>>>
+  <assign|emdash|<macro|<active*|<with|mode|text|font|roman|\V>>>>
 
   <assign|masculine|<active*|<rsup|<wide*|o|\<wide-bar\>>>>>
 
