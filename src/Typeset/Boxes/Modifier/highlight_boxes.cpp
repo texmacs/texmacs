fb6f061b6d300a0df97a9f6ee34881ff888119b4
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2013-09-16 16:27:54 +0000

    Further preparations for other ornaments

--- src/Typeset/Boxes/Modifier/highlight_boxes.cpp
+++ src/Typeset/Boxes/Modifier/highlight_boxes.cpp
@@ -22,12 +22,13 @@
 struct highlight_box_rep: public change_box_rep {
   SI w, xpad, ypad;
   brush bg, sunc, shad, old_bg;
+  pencil old_pen;
   highlight_box_rep (path ip, box b, SI w, SI xpad, SI ypad,
 		     brush bg, brush sunc, brush shad);
   operator tree () { return tree (TUPLE, "highlight", (tree) bs[0]); }
   void pre_display (renderer &ren);
   void post_display (renderer &ren);
-  void display (renderer ren);
+  void display_classic (renderer& ren);
 };
 
 highlight_box_rep::highlight_box_rep (
@@ -51,28 +52,30 @@ highlight_box_rep::highlight_box_rep (
 
 void
 highlight_box_rep::pre_display (renderer& ren) {
-  old_bg= ren->get_background ();
-  ren->set_background (bg);
-  SI W= w;
-  if (!ren->is_printer ()) {
-    SI pixel= ren->pixel;
-    W= ((w + pixel - 1) / pixel) * pixel;
-  }
-  ren->clear_pattern (x1+W, y1+W, x2-W, y2-W);
+  old_bg = ren->get_background ();
+  old_pen= ren->get_pencil ();
+  display_classic (ren);
 }
 
 void
 highlight_box_rep::post_display (renderer &ren) {
   ren->set_background (old_bg);
+  ren->set_pencil (old_pen);
 }
 
+/******************************************************************************
+* Classic ornaments
+******************************************************************************/
+
 void
-highlight_box_rep::display (renderer ren) {
+highlight_box_rep::display_classic (renderer& ren) {
   SI W= w;
   if (!ren->is_printer ()) {
     SI pixel= ren->pixel;
     W= ((w + pixel - 1) / pixel) * pixel;
   }
+  ren->set_background (bg);
+  ren->clear_pattern (x1+W, y1+W, x2-W, y2-W);
   ren->set_pencil (pencil (sunc, 0));
   ren->fill (x1  , y2-W, x2  , y2  );
   ren->fill (x1  , y1  , x1+W, y2  );
@@ -82,7 +85,6 @@ highlight_box_rep::display (renderer ren) {
   ren->draw_triangle (x1, y1, x1+W, y1, x1+W, y1+W);
   ren->draw_triangle (x2, y2, x2, y2-W, x2-W, y2-W);
 }
-
 
 /******************************************************************************
 * box construction routines
