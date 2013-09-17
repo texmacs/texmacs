2881ed3048d113d2a90b38ab48b4045d5722ddbc
François Poulain <fpoulain@metrodore.fr>
2013-09-17 08:34:02 +0000

    LaTeX import: temporary removing option for mixed tree/source importation in preferences: it does not work as is

--- TeXmacs/progs/texmacs/menus/preferences-menu.scm
+++ TeXmacs/progs/texmacs/menus/preferences-menu.scm
@@ -143,8 +143,9 @@
         (-> "LaTeX -> TeXmacs"
             (toggle ("Import sophisticated objects as pictures"
                      "latex->texmacs:fallback-on-pictures"))
-            (toggle ("Keep track of the LaTeX source code"
-                     "latex->texmacs:preserve-source")))
+          )
+          ;;(toggle ("Keep track of the LaTeX source code"
+          ;;         "latex->texmacs:preserve-source")))
         (-> "TeXmacs -> LaTeX"
             (toggle ("Replace unrecognized styles"
                      "texmacs->latex:replace-style"))
@@ -460,9 +461,10 @@
     (meti (text "Import sophisticated objects as pictures")
       (toggle (set-boolean-preference "latex->texmacs:fallback-on-pictures" answer)
               (get-boolean-preference "latex->texmacs:fallback-on-pictures")))
-    (meti (text "Keep track of the LaTeX source code")
-      (toggle (set-boolean-preference "latex->texmacs:preserve-source" answer)
-              (get-boolean-preference "latex->texmacs:preserve-source"))))
+  )
+  ;;(meti (text "Keep track of the LaTeX source code")
+  ;;  (toggle (set-boolean-preference "latex->texmacs:preserve-source" answer)
+  ;;          (get-boolean-preference "latex->texmacs:preserve-source"))))
   ======
   (bold (text "TeXmacs -> LaTeX"))
   ===
