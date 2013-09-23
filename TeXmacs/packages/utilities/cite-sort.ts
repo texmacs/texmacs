d28c2b58169b382da52ca19b19a9157d5fce9b19
François Poulain <fpoulain@metrodore.fr>
2013-09-23 09:00:55 +0000

    Cite-sort: first run

---
+++ TeXmacs/packages/utilities/cite-sort.ts
@@ -0,0 +1,33 @@
+<TeXmacs|1.0.7.19>
+
+<style|<tuple|source|std>>
+
+<\body>
+  <active*|<\src-title>
+    <src-package|cite-sort|1.0>
+
+    <\src-purpose>
+      This package contains macros for the automatic sorting of citations.
+    </src-purpose>
+
+    <src-copyright|2013|Fran<E7>ois Poulain, Joris van der Hoeven>
+
+    <\src-license>
+      This software falls under the <hlink|GNU general public license,
+      version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+      WARRANTY WHATSOEVER. You should have received a copy of the license
+      which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+    </src-license>
+  </src-title>>
+
+  <use-module|(utils cite cite-sort)>
+
+  <assign|cite-sort|<macro|body|<extern|cite-sort|<arg|body>>>>
+
+  <assign|cite-raw|<xmacro|keys|<cite-sort|<map-args|cite-arg|tuple|keys>>>>
+
+  <assign|cite|<xmacro|keys|<render-cite|<cite-sort|<map-args|cite-arg|tuple|keys>>>>>
+</body>
+
+<initial|<\collection>
+</collection>>
---
+++ TeXmacs/progs/utils/cite/cite-sort.scm
@@ -0,0 +1,45 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : cite-sort.scm
+;; DESCRIPTION : support utilities for sorting citations
+;; COPYRIGHT   : (C) 2013 François Poulain, Joris van der Hoeven
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(texmacs-module (utils cite cite-sort))
+
+(define (compare-string s1 s2)
+  (if (and (string->number s1) (string->number s2))
+    (< (string->number s1) (string->number s2))
+    (string<? s1 s2)))
+
+(define (compare-cite-keys t1 t2)
+  (let*
+    ((t1 (car t1))
+     (t2 (car t2))
+     (s1 (if (string? t1) t1 (convert t1 "stm-snippet" "verbatim-snippet")))
+     (s2 (if (string? t2) t2 (convert t2 "stm-snippet" "verbatim-snippet"))))
+    (compare-string s1 s2)))
+
+(define (expand-references k)
+  (with key (stree->tree `(get-binding ,(cadr k)))
+    (with ret (tree->stree (texmacs-exec key))
+      (if (!= ret '(uninit)) ret ""))))
+
+(tm-define (cite-sort args)
+  ;; get a (tuple (tuple key_1 value_1) ... (tuple key_n value_n))
+  ;; and sort it according to values.
+  (:secure #t)
+  (let* ((args (map tree->stree (tree-children args)))
+         (keys (map expand-references (map caddr args)))
+         (tup (map list keys args))
+         (sorted-tup (list-sort tup compare-cite-keys))
+         ;; we should merge contiguous number series here...
+         (sorted-args (map cadr sorted-tup))
+         (ret `(concat ,@(list-intersperse sorted-args '(cite-sep)))))
+    ret))
