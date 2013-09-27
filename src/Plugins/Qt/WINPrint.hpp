fc5d12acc5029c49cd105e30d70a48e4cb54baaf
Denis Raux <raux@texmacs.org>
2013-09-27 12:16:18 +0000

    windows printing

---
+++ src/Plugins/Qt/WINPrint.hpp
@@ -0,0 +1,27 @@
+
+/******************************************************************************
+* MODULE     : WINPrint.hpp
+* DESCRIPTION: routines for the editor
+* COPYRIGHT  : (C) 2013  Denis Raux
+*  printing for windows using poppler and Qt
+*******************************************************************************
+* This software falls under the GNU general public license version 3 or later.
+* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+******************************************************************************/
+
+
+#if defined (QTTEXMACS) && (defined (__MINGW__) || defined (__MINGW32__))
+
+class WINPrint {
+public:
+   WINPrint(QString url,bool &IsLandscape);
+   ~WINPrint();
+   int first_page,last_page;
+   bool doit;   
+private:
+   static QPrinter *Prt;  // static to keep user choice along texmacs session
+   QString file;
+   
+};
+ #endif
