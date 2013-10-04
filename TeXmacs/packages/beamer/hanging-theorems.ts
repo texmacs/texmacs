f9d1f774b6fe2fcffe758b3b4ac8c16f41f801aa
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2013-10-04 15:15:23 +0000

    Editable hang-length

--- TeXmacs/packages/beamer/hanging-theorems.ts
+++ TeXmacs/packages/beamer/hanging-theorems.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.0.7.19>
+<TeXmacs|1.0.7.20>
 
 <style|source>
 
@@ -20,7 +20,15 @@
     </src-license>
   </src-title>>
 
+  <\active*>
+    <\src-comment>
+      Extra style parameter
+    </src-comment>
+  </active*>
+
   <assign|hang-length|0.15em>
+
+  <drd-props|hang-length|parameter|length>
 
   <\active*>
     <\src-comment>
@@ -36,7 +44,7 @@
 
   <assign|render-enunciation|<\macro|which|body>
     <\ornament>
-      <surround|<resize|<with|ornament-color|<value|ornament-extra-color>|<ornament|<resize|<arg|which>||<minus|0ex|0.5hang>||<plus|1t|1hang>>>>|<plus|1hang|<value|ornament-hpadding>|<value|ornament-border>>|<plus|1b|0.5hang>||<minus|<minus|1t|1hang>|<plus|<value|ornament-vpadding>|<value|ornament-border>>>>
+      <surround|<resize|<with|ornament-color|<value|ornament-extra-color>|dummy|<value|hang-length>|<ornament|<resize|<arg|which>||<minus|0ex|0.5hang>||<plus|1t|1hang>>>>|<plus|1hang|<value|ornament-hpadding>|<value|ornament-border>>|<plus|1b|0.5hang>||<minus|<minus|1t|1hang>|<plus|<value|ornament-vpadding>|<value|ornament-border>>>>
       |<right-flush>|<arg|body>>
     </ornament>
   </macro>>
