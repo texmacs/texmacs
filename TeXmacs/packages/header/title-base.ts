976118bb7285c85e00ac46957c76db50567aa13a
François Poulain <fpoulain@metrodore.fr>
2013-11-05 13:13:44 +0000

    Remove unwilled document tags

--- TeXmacs/packages/header/title-base.ts
+++ TeXmacs/packages/header/title-base.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.0.7.20>
+<TeXmacs|1.0.7.21>
 
 <style|<tuple|source|std-pattern>>
 
@@ -269,9 +269,7 @@
   <assign|abstract-acm|<xmacro|args|<style-with|src-compact|none|<no-indent><theorem-name|<ACM-class-text><localize|:>
   ><concat-tuple|<map|acm-ref|<quote-arg|args>>|, >>>>
 
-  <assign|arxiv-ref|<\macro|cat-id>
-    <style-with|src-compact|none|<hlink|<arg|cat-id>|<style-with|src-compact|none|<merge|http://arxiv.org/find/all/1/cat:+|<arg|cat-id>|/0/1/0/all/0/1>>>>
-  </macro>>
+  <assign|arxiv-ref|<macro|cat-id|<style-with|src-compact|none|<hlink|<arg|cat-id>|<style-with|src-compact|none|<merge|http://arxiv.org/find/all/1/cat:+|<arg|cat-id>|/0/1/0/all/0/1>>>>>>
 
   <assign|abstract-arxiv|<xmacro|args|<style-with|src-compact|none|<no-indent><theorem-name|<arXiv-class-text><localize|:>
   ><concat-tuple|<map|arxiv-ref|<quote-arg|args>>|, >>>>
@@ -281,9 +279,7 @@
   <assign|abstract-pacs|<xmacro|args|<style-with|src-compact|none|<no-indent><theorem-name|<PACS-class-text><localize|:>
   ><concat-tuple|<map|pacs-ref|<quote-arg|args>>|, >>>>
 
-  <assign|msc-ref|<\macro|msc-id>
-    <style-with|src-compact|none|<hlink|<arg|msc-id>|<style-with|src-compact|none|<merge|http://www.ams.org/mathscinet/search/mscbrowse.html?sk=|<arg|msc-id>|&submit=Search>>>>
-  </macro>>
+  <assign|msc-ref|<macro|msc-id|<style-with|src-compact|none|<hlink|<arg|msc-id>|<style-with|src-compact|none|<merge|http://www.ams.org/mathscinet/search/mscbrowse.html?sk=|<arg|msc-id>|&submit=Search>>>>>>
 
   <assign|abstract-msc|<xmacro|args|<style-with|src-compact|none|<no-indent><theorem-name|<AMS-class-text><localize|:>
   ><concat-tuple|<map|msc-ref|<quote-arg|args>>|, >>>>
