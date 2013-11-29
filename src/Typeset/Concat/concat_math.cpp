c1bebf3d3cc89bfee9fd7b661f5e997ef8a51531
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2013-11-29 22:15:54 +0000

    Fixed bug #21982, Underbrace placement

--- src/Typeset/Concat/concat_math.cpp
+++ src/Typeset/Concat/concat_math.cpp
@@ -271,6 +271,8 @@ concater_rep::typeset_wide (tree t, path ip, bool above) {
     s= "<" * s (6, N(s));
     request_wide= true;
   }
+  if (ends (s, "brace>") || ends (s, "brace*>"))
+    b= move_box (descend (ip, 0), b, 0, 0, true);
   print (wide_box (ip, b, s, env->fn, env->pen, request_wide, above));
   if ((s == "<underbrace>") || (s == "<overbrace>") ||
       (s == "<squnderbrace>") || (s == "<sqoverbrace>"))
