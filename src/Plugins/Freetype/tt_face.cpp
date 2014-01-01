99b34a51fe9e6198f23dc1070f64db9510d3c478
Miguel de Benito <nonick@8027.org>
2014-01-01 16:55:22 +0000

    Fix: don't crash in tt_font_glyphs_rep::get() if FT_CharMap is NULL

--- src/Plugins/Freetype/tt_face.cpp
+++ src/Plugins/Freetype/tt_face.cpp
@@ -161,8 +161,9 @@ tt_font_glyphs_rep::get (int i) {
     // mg:
     // the index variable is used by code who need the glyph_index for unicode characters
     // to locate the right glyph in the font file
-    G->index = (face->ft_face->charmap->encoding == FT_ENCODING_UNICODE ?
-                glyph_index : i);
+    G->index = (face->ft_face->charmap &&
+                face->ft_face->charmap->encoding == FT_ENCODING_UNICODE) ?
+                  glyph_index : i;
     G->lwidth= (tt_si (slot->metrics.horiAdvance)+(PIXEL>>1))/PIXEL;
 
     for (y=0; y<h; y++) {
