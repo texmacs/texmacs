8008bd9102f17d1567618982ad8833c43429bd46
François Poulain <fpoulain@metrodore.fr>
2014-01-30 10:40:41 +0000

    TeXmacs conservative LaTeX reimport: adding entry to the menu

--- TeXmacs/progs/texmacs/menus/preferences-menu.scm
+++ TeXmacs/progs/texmacs/menus/preferences-menu.scm
@@ -141,6 +141,8 @@
                      "texmacs->latex:indirect-bib"))
             (toggle ("Allow for macro definitions in preamble"
                      "texmacs->latex:use-macros"))
+            (toggle ("Dump TeXmacs document into LaTeX source"
+                     "texmacs->latex:preserve-source"))
             (enum ("Encoding" "texmacs->latex:encoding")
                   ("Strict Ascii" "ascii")
                   ("Cork charset with TeX catcode definition in preamble" "cork")
