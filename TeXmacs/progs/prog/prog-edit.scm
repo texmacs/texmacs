6034ecc516e7f9238f645b558c626dd2594c364c
Miguel de Benito <nonick@8027.org>
2014-02-11 18:16:49 +0000

    Scheme: Leave brackets highlighted until the cursor moves

--- TeXmacs/progs/prog/prog-edit.scm
+++ TeXmacs/progs/prog/prog-edit.scm
@@ -141,12 +141,8 @@
 
 (define (select-brackets* row col br ibr)
   (let* ((prev (program-bracket-backward row col br ibr ))
-         (next (program-bracket-forward row col br ibr ))
-         ; avoid too many delayed commands while moving fast
-         (sel? (nnull? (get-alt-selection "alternate"))))
-    (if sel? (cancel-alt-selection "alternate"))
-    (set-alt-selection "alternate" (append prev next))
-    (if (not sel?) (delayed (:pause 1000) (cancel-alt-selection "alternate")))))
+         (next (program-bracket-forward row col br ibr )))
+    (set-alt-selection "alternate" (append prev next))))
 
 (tm-define (select-brackets br ibr)
   (:synopsis "Highlights the innermost matching brackets around the cursor")
@@ -162,7 +158,9 @@
          (ch (program-character row col*)))
     (cond ((== esc ch) (noop))
           ((== ibr ch) (select-brackets* row col* br ibr))
-          ((== br ch) (select-brackets* row col br ibr)))))
+          ((== br ch) (select-brackets* row col br ibr))
+          ((nnull? (get-alt-selection "alternate"))
+           (cancel-alt-selection "alternate")))))
 
 (tm-define (bracket-open br ibr esc)
   (with sel? (selection-active-normal?)
