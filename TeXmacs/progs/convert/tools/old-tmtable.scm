6cefd29abe4c22e0cfc46d4f61c7968754786fc6
François Poulain <fpoulain@metrodore.fr>
2014-02-27 09:20:47 +0000

    LaTeX export: fix crash with misformed table formats

--- TeXmacs/progs/convert/tools/old-tmtable.scm
+++ TeXmacs/progs/convert/tools/old-tmtable.scm
@@ -169,7 +169,9 @@
 				  (string->number j2)
 				  name
 				  (stm-table-decode-format name value))))
-			(list-filter (cDdr x) (lambda (l) (= (length l) 7))))
+			(list-filter (cDdr x)
+                                     (lambda (l)
+                                        (and (list? l) (= (length l) 7)))))
 		   (stm-table-formats (cAr x))))
          (else '())))
 
