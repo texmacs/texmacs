02fde5958097229e614a11f105725e993af90662
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2014-03-06 13:25:24 +0000

    Minor reorganization

--- TeXmacs/progs/convert/latex/latex-texmacs-drd.scm
+++ TeXmacs/progs/convert/latex/latex-texmacs-drd.scm
@@ -13,7 +13,8 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(texmacs-module (convert latex latex-texmacs-drd))
+(texmacs-module (convert latex latex-texmacs-drd)
+  (:use (convert latex latex-symbol-drd)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Extra TeXmacs symbols
@@ -791,78 +792,3 @@
   (akey (!recurse (key (!append "alt-" 1))))
   (mkey (!recurse (key (!append "meta-" 1))))
   (hkey (!recurse (key (!append "hyper-" 1)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Page size settings
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(logic-table latex-paper-opts%
-  ("page-top"         "top")
-  ("page-bot"         "bottom")
-  ("page-odd"         "left")
-  ("page-even"        "left")
-  ("page-right"       "right")
-  ("page-height"      "paperheight")
-  ("page-width"       "paperwidth")
-  ("page-type"        "page-type")
-  ("page-orientation" "page-orientation"))
-
-(logic-table latex-paper-type%
-  ("a0" "a0paper")
-  ("a1" "a1paper")
-  ("a2" "a2paper")
-  ("a3" "a3paper")
-  ("a4" "a4paper")
-  ("a5" "a5paper")
-  ("a6" "a6paper")
-  ("a7" "papersize={74mm,105mm}")
-  ("a8" "papersize={52mm,74mm")
-  ("a9" "papersize={37mm,52mm}")
-  ("b0" "b0paper")
-  ("b1" "b1paper")
-  ("b2" "b2paper")
-  ("b3" "b3paper")
-  ("b4" "b4paper")
-  ("b5" "b5paper")
-  ("b6" "b6paper")
-  ("b7" "papersize={88mm,125mm}")
-  ("b8" "papersize={62mm,88mm}")
-  ("b9" "papersize={44mm,62mm}")
-  ("legal" "legalpaper")
-  ("letter" "letterpaper")
-  ("executive" "executivepaper")
-  ("archA" "papersize={9in,12in}")
-  ("archB" "papersize={12in,18in}")
-  ("archC" "papersize={18in,24in}")
-  ("archD" "papersize={24in,36in}")
-  ("archE" "papersize={36in,48in}")
-  ("10x14" "papersize={10in,14in}")
-  ("11x17" "papersize={11in,17in}")
-  ("C5" "papersize={162mm,229mm}")
-  ("Comm10" "papersize={297pt,684pt}")
-  ("DL" "papersize={110mm,220mm}")
-  ("halfletter" "papersize={140mm,216mm}")
-  ("halfexecutive" "papersize={133mm,184mm}")
-  ("ledger" "papersize={432mm,279mm}")
-  ("Monarch" "papersize={98mm,190mm}")
-  ("csheet" "papersize={432mm,559mm}")
-  ("dsheet" "papersize={559mm,864mm}")
-  ("esheet" "papersize={864mm,1118mm}")
-  ("flsa" "papersize={216mm,330mm}")
-  ("flse" "papersize={216mm,330mm}")
-  ("folio" "papersize={216mm,330mm}")
-  ("lecture note" "papersize={15.5cm,23.5cm}")
-  ("note" "papersize={216mm,279mm}")
-  ("quarto" "papersize={215mm,275mm}")
-  ("statement" "papersize={140mm,216mm}")
-  ("tabloid" "papersize={279mm,432mm}"))
-
-;; cpp interface with reversed access
-
-(tm-define (latex-paper-opts s)
-  (with r (query `(latex-paper-opts% 'x ,s))
-    (if (nnull? r) (cdaar r) "undefined")))
-
-(tm-define (latex-paper-type s)
-  (with r (query `(latex-paper-type% 'x ,s))
-    (if (nnull? r) (cdaar r) "undefined")))
