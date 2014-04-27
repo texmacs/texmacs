379783394b73d1a9eb6fa605481c0b6a7a8425f5
Miguel de Benito <nonick@8027.org>
2014-04-27 16:02:47 +0000

    Bibliography widget: using relative paths. Fixes.

--- TeXmacs/progs/bibtex/bib-widgets.scm
+++ TeXmacs/progs/bibtex/bib-widgets.scm
@@ -11,129 +11,147 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; TODO:
-;;  - Handle several bibliographies in a document.
 ;;  - Handle external BibTeX.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (texmacs-module (bibtex bib-widgets)
   (:use (bibtex bib-complete) (generic document-edit)))
 
-(define bib-widget-url (string->url ""))
-(define bib-widget-style "tm-plain")
-(define bib-widget-use-relative? #t)
-(define bib-widget-update-buffer? #t)
+(define bibwid-url (string->url ""))
+(define bibwid-style "tm-plain")
+(define bibwid-use-relative? #t)
+(define bibwid-update-buffer? #t)
+(define bibwid-cwd (string->url ""))
 
-(define (bib-widget-url-chosen answer)
-  (set! bib-widget-url answer)
-  (refresh-now "bib-widget-file-input")
-  (refresh-now "bib-widget-preview"))
+(define url-sep (if (os-win32?) "\\" "/"))
+(define (url-delta* u1 u2)
+  (with u (url-delta (url->string (url-glue u1 url-sep)) u2)
+    (if (or (url-rooted? u) (equal? (url-tail u) u))
+        u
+        (url-append (url-parent) u))))
 
-(define (bib-widget-style-chosen answer)
-  (set! bib-widget-style answer)
-  (refresh-now "bib-widget-preview"))
+;;;;; HACK!!! url-delta seems bogus
+(with u "/some/test/here.blah"
+  (if (== (url-delta (url-head u) u) (url-tail u))
+      (set! url-delta* url-delta)))
 
-(define (bib-widget-output-content t style)
-  (if (not (tree-is? t 'string))
-      `(with "bg-color" "#ffffff"
-         (paragraph-box "480px" ,(bibstyle style (tree->stree t))))
-      `(with "bg-color" "white"
+(define (bibwid-set-url u)
+  (cond ((and (== bibwid-use-relative? #t) (url-rooted? u))
+         (set! bibwid-url (url-delta* bibwid-cwd u)))
+        ((and (== bibwid-use-relative? #f) (not (url-rooted? u)))
+         (set! bibwid-url (url-append bibwid-cwd u)))
+        (else (set! bibwid-url u))))
+
+(define (bibwid-set-style answer)
+  (set! bibwid-style answer)
+  (refresh-now "bibwid-preview"))
+
+(define (bibwid-output-content t style)
+  (if (tree-is? t 'string) 
+      '(with "bg-color" "white"
          (paragraph-box "480px" 
-           (document (concat "Please choose a " (BibTeX) " file"))))))
+           (document (concat "Please choose a valid " (BibTeX) " file"))))
+      `(with "bg-color" "#ffffff"
+         (paragraph-box "480px" ,(bibstyle style (tree->stree t))))))
 
-(define (bib-widget-output)
-  (with style (if (== "tm-" (string-take bib-widget-style 3))
-                  (string-drop bib-widget-style 3)
-                  bib-widget-style)
+(define (bibwid-output)
+  (with style (if (== "tm-" (string-take bibwid-style 3))
+                  (string-drop bibwid-style 3)
+                  bibwid-style)
     (eval `(use-modules (bibtex ,(string->symbol style))))
-    (with t (parse-bib (string-load bib-widget-url))
-      (stree->tree (bib-widget-output-content t style)))))
+    (with u (if (and bibwid-use-relative? (not (url-rooted? bibwid-url)))
+                (url-append bibwid-cwd bibwid-url)
+                bibwid-url)
+      (with t (if (url-exists? u) 
+                  (parse-bib (string-load u)) 
+                  (tree ""))
+        (stree->tree (bibwid-output-content t style))))))
 
-(define (bib-widget-insert doit?)
-  (if doit?
-      (with file (if bib-widget-use-relative? 
-                     (url-delta (current-buffer) bib-widget-url)
-                     bib-widget-url)
-        (if (not (make-return-after))
-            (insert 
-             (list 'bibliography "bib" bib-widget-style (url->string file)
-                   '(document ""))))
-        (if bib-widget-update-buffer? (update-document "bibliography")))))
+(define (bibwid-insert doit?)
+  (when doit?
+    (if (not (make-return-after))
+        (insert 
+         (list 'bibliography "bib" bibwid-style (url->string bibwid-url)
+               '(document ""))))
+    (if bibwid-update-buffer? (update-document "bibliography"))))
 
-(define (bib-widget-modify doit?)
-  (if doit?
-      (with l (select (buffer-tree) '(:* bibliography))
-        (if (== 1 (length l))
-            (with t (car l)
-              (tree-set! t 1 bib-widget-style)
-              (tree-set! t 2 (url->string bib-widget-url))
-              (if bib-widget-update-buffer? (update-document "bibliography"))
-              #t)
-            #f))))
+(define (bibwid-modify doit?)
+  (when doit?
+    (with l (select (buffer-tree) '(:* bibliography))
+      (when (> (length l) 0)
+        (with t (car l)
+          (tree-set! t 1 bibwid-style)
+          (tree-set! t 2 (url->string bibwid-url))
+          (if bibwid-update-buffer? (update-document "bibliography")))))))
 
-(tm-widget (bib-widget-preview)
+(define (bibwid-set-filename u)
+  (bibwid-set-url u)
+  (refresh-now "bibwid-file-input")
+  (refresh-now "bibwid-preview"))
+
+(define (bibwid-set-relative val)
+  (set! bibwid-use-relative? val)
+  (bibwid-set-filename bibwid-url))
+
+(tm-widget (bibwid-preview)
   (resize ("520px" "520px" "9999px") ("100px" "100px" "9999px")
     (scrollable 
-      (refreshable "bib-widget-preview"
+      (refreshable "bibwid-preview"
         (texmacs-output
-         (bib-widget-output)
+         (bibwid-output)
          '(style "generic"))))))
 
 (tm-widget ((bibliography-widget modify?) cmd)
   (padded
     (hlist 
       (text "File:") // //
-      (refreshable "bib-widget-file-input"
+      (refreshable "bibwid-file-input"
         (hlist
-          (input (if answer
-                     (begin (set! bib-widget-url (string->url answer))
-                            (refresh-now "bib-widget-preview")))
-                 "file" (list (url->string bib-widget-url)) "40em")
+          (input (when (and answer (!= answer (url->string bibwid-url)))
+                   (bibwid-set-url (string->url answer))
+                   (refresh-now "bibwid-preview"))
+                 "file" (list (url->string bibwid-url)) "40em")
           // //
           (explicit-buttons 
-            ("" 
-             (choose-file 
-              (lambda (answer)
-                (set! bib-widget-url answer)
-                (refresh-now "bib-widget-file-input")
-                (refresh-now "bib-widget-preview"))
-              "Choose" "bibtex"))))))
+            ("" (choose-file bibwid-set-filename "Choose" "bibtex"))))))
     ===
     (hlist
       ;(balloon "Use relative path:" "Select this to use a path relative to the current document. You can use this to be able to move around the folder containing your document and the bibliography.")
       (text "Use relative path:") //
-      (toggle (set! bib-widget-use-relative? answer)
-              bib-widget-use-relative?)
+      (toggle (bibwid-set-relative answer) bibwid-use-relative?)
       // //
       (text "Update buffer:") // 
-      (toggle (set! bib-widget-update-buffer? answer)
-              bib-widget-update-buffer?)
+      (toggle (set! bibwid-update-buffer? answer)
+              bibwid-update-buffer?)
       ///
       (text "Style:") // //
-      (enum (bib-widget-style-chosen answer)
+      (enum (bibwid-set-style answer)
             '("tm-plain" "tm-alpha" "tm-acm" "tm-ieeetr" "tm-siam")
-            bib-widget-style "10em"))
+            bibwid-style "10em"))
     === === ===
-    (hlist // (dynamic (bib-widget-preview)) //)
+    (hlist // (dynamic (bibwid-preview)) //)
     ===
-    (bottom-buttons 
-      >>> ("Cancel" (cmd #f)) // //
-      (if modify? ("Modify current" (cmd #t)) // //)
-      ("Insert" (cmd #t)))))
+    (bottom-buttons >>>
+      ("Cancel" (cmd #f)) // //
+      (if modify? ("Modify" (cmd #t)))
+      (if (not modify?) ("Insert" (cmd #t))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Interface
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (tm-define (open-bibliography-inserter)
-  (set! bib-widget-url (string->url ""))
-  (set! bib-widget-style "tm-plain")
+  (set! bibwid-url (string->url ""))
+  (set! bibwid-style "tm-plain")
+  (set! bibwid-cwd (url-head (current-buffer)))
+
   (let ((u (current-bib-file #f))
         (style (current-bib-style #f)))
   (if (and (not (url-none? u)) (!= style ""))
       (begin
-        (set! bib-widget-url u)
-        (set! bib-widget-style style)
+        (bibwid-set-url u)
+        (set! bibwid-style style)
         (dialogue-window (bibliography-widget #t) 
-                         bib-widget-modify "Modify bibliography"))
+                         bibwid-modify "Modify bibliography"))
       (dialogue-window (bibliography-widget #f)
-                       bib-widget-insert "Insert bibliography"))))
+                       bibwid-insert "Insert bibliography"))))
