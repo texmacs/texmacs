85debf131fe635496e7afea90c03134ad5830172
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2014-06-05 17:19:06 +0000

    Make new style fonts the default

--- TeXmacs/progs/texmacs/texmacs/tm-server.scm
+++ TeXmacs/progs/texmacs/texmacs/tm-server.scm
@@ -96,7 +96,7 @@
   ("source tool" "off" notify-tool)
   ("versioning tool" "off" notify-tool)
   ("experimental alpha" "on" notify-tool)
-  ("new style fonts" "off" notify-new-fonts))
+  ("new style fonts" "on" notify-new-fonts))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Properties of some built-in routines
