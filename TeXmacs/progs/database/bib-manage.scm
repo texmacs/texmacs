edb8f6501ac1670b8d4b03cdc2f99de897518071
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2015-03-05 09:00:21 +0000

    Further preparations for bib management

--- TeXmacs/progs/database/bib-manage.scm
+++ TeXmacs/progs/database/bib-manage.scm
@@ -18,8 +18,8 @@
 ;; Importing BibTeX files
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define bib-master
-  (string->url "$TEXMACS_HOME_PATH/system/bib-cache/bib-master.tmdb"))
+(define bib-dir "$TEXMACS_HOME_PATH/system/bib-cache")
+(define bib-master (string-append bib-dir "/bib-master.tmdb"))
 
 (define (bib-cache-rid f)
   (with-database bib-master
@@ -30,12 +30,12 @@
 (define (bib-cache-stamp f)
   (and-with rid (bib-cache-rid f)
     (with-database bib-master
-      (db-get rid "stamp"))))
+      (db-get-first rid "stamp" #f))))
 
 (define (bib-cache-db f)
   (and-with rid (bib-cache-rid f)
     (with-database bib-master
-      (db-get rid "target"))))
+      (db-get-first rid "target" #f))))
 
 (define (bib-cache-up-to-date? f)
   (and-with stamp (bib-cache-stamp f)
@@ -49,9 +49,25 @@
       (with-database bib-master
         (db-reset-all rid)))))
 
-(tm-define (bib-import-bibtex f)
+(define (bib-cache-create f)
   (let* ((bib-doc (string-load f))
          (tm-doc (convert bib-doc "bibtex-document" "texmacs-document"))
-         (name (create-unique-id))
-         (fname (string-append "$TEXMACS_HOME_PATH/system/bib-cache/" name)))
-    (display* tm-doc "\n")))
+         (body (tmfile-extract tm-doc 'body))
+         (rid (create-unique-id))
+         (db (string-append bib-dir "/" rid ".tmdb")))
+    (with-database db
+      (bib-export body))
+    (with-database bib-master
+      (db-insert rid "source" f)
+      (db-insert rid "target" db)
+      (db-insert rid "stamp" (number->string (url-last-modified f))))))
+
+(tm-define (bib-import-bibtex f)
+  (when (not (bib-cache-up-to-date? f))
+    (bib-cache-remove f))
+  (when (not (bib-cache-rid f))
+    (bib-cache-create f))
+  (when (not (bib-cache-rid f))
+    (texmacs-error "failed to create bibliography database"
+                   "bib-import-bibtex"))
+  (bib-cache-rid f))
