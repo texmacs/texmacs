c0f996a1040602100e6f136ec8787c46e73c6efd
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2015-03-05 16:12:15 +0000

    Convenience fix

--- TeXmacs/styles/test/database.ts
+++ TeXmacs/styles/test/database.ts
@@ -91,6 +91,33 @@
 
   <drd-props|db-entry-alternative|arity|2|unaccessible|0|accessible|1>
 
+  <\active*>
+    <\src-comment>
+      Markup for bibliography fields.
+    </src-comment>
+  </active*>
+
+  <assign|bib-name|<macro|f|v|l|j|<if|<unequal|<length|<arg|f>>|0>|<arg|f>
+  ><if|<unequal|<length|<arg|v>>|0>|<arg|><with|color|dark
+  grey|font-shape|small-caps|<arg|v>> ><with|font-shape|small-caps|<arg|l>><if|<unequal|<length|<arg|j>>|0>|,
+  <arg|><with|font-shape|italic|<arg|j>> >>>
+
+  <assign|bib-name-extra|<macro|x| <with|color|dark green|<strong|and>>
+  <arg|x>>>
+
+  <assign|bib-names|<xmacro|args|<style-with|src-compact|none|<arg|args|0><map-args|bib-name-extra|concat|args|1>>>>
+
+  <assign|bib-pages-extra|<macro|x|<with|color|dark
+  green|<space|0.5spc><strong|--><space|0.5spc>><arg|x>>>
+
+  <assign|bib-pages|<xmacro|args|<style-with|src-compact|none|<arg|args|0><map-args|bib-pages-extra|concat|args|1>>>>
+
+  <assign|keepcase|<macro|x|<underline|<arg|x>>>>
+
+  <assign|first-last|<macro|x|<extern|ext-first-last|<arg|x>>>>
+
+  <drd-props|first-last|arity|1|accessible|0>
+
   \;
 </body>
 
