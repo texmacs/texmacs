b4f8a695c43386b9534c880c594129e8d46d150f
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2015-03-20 18:30:25 +0000

    Continued cleaning of code by Gregoire

--- TeXmacs/progs/security/gpg/gpg-wallet.scm
+++ TeXmacs/progs/security/gpg/gpg-wallet.scm
@@ -36,22 +36,21 @@
 (define-preferences
   ("gpg wallet key fingerprint" "" notify-gpg-wallet-key-fingerprint))
 
-(define-public (gpg-wallet-on?)
-  (not (string? gpg-wallet-table)))
+(tm-define (gpg-wallet-on?)
+  (nstring? gpg-wallet-table))
 
-(define-public (gpg-wallet-initialized?)
+(tm-define (gpg-wallet-initialized?)
   (and (supports-gpg?)
-  (if (not (url-exists? gpg-wallet-table-url))
-    #f
-    (with fingerprints (gpg-secret-key-fingerprints gpg-wallet-url)
-      (and fingerprints (not (null? fingerprints))
-	   (string= (car fingerprints) gpg-wallet-key-fingerprint))))))
+       (url-exists? gpg-wallet-table-url)
+       (with fingerprints (gpg-secret-key-fingerprints gpg-wallet-url)
+         (and fingerprints (not (null? fingerprints))
+              (string= (car fingerprints) gpg-wallet-key-fingerprint)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Check passphrase
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-public (gpg-wallet-correct-passphrase? passphrase)
+(tm-define (gpg-wallet-correct-passphrase? passphrase)
   (and (gpg-wallet-initialized?)
        (gpg-correct-passphrase? gpg-wallet-key-fingerprint
 				passphrase gpg-wallet-url)))
@@ -60,52 +59,50 @@
 ;; Turn on/off wallet
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-public (gpg-wallet-turn-on passphrase)
-  (if (gpg-wallet-on?) #t
-    (if (not (gpg-wallet-initialized?)) #f
-      (with b (gpg-load-decrypt-object gpg-wallet-table-url
- 	  			       passphrase gpg-wallet-url)
-      (if b
-        (begin (set! gpg-wallet-table (list->ahash-table b))
-               #t)
-        #f)))))
+(tm-define (gpg-wallet-turn-on passphrase)
+  (or (gpg-wallet-on?)
+      (and (gpg-wallet-initialized?)
+           (and-with b (gpg-load-decrypt-object gpg-wallet-table-url
+                                                passphrase gpg-wallet-url)
+             (set! gpg-wallet-table (list->ahash-table b))
+             #t))))
 
-(define-public (gpg-wallet-save)
+(tm-define (gpg-wallet-save)
   (and (gpg-wallet-on?)
-    (gpg-encrypt-save-object gpg-wallet-table-url
-      (ahash-table->list gpg-wallet-table)
-      gpg-wallet-key-fingerprint gpg-wallet-url)))
+       (gpg-encrypt-save-object gpg-wallet-table-url
+                                (ahash-table->list gpg-wallet-table)
+                                gpg-wallet-key-fingerprint gpg-wallet-url)))
 
-(define-public (gpg-wallet-turn-off)
-  (if (and (gpg-wallet-on?)
-           (gpg-encrypt-save-object gpg-wallet-table-url
-	     (ahash-table->list gpg-wallet-table)
-	     gpg-wallet-key-fingerprint gpg-wallet-url))
-    (begin (set! gpg-wallet-table "") #t)
-    #f))
+(tm-define (gpg-wallet-turn-off)
+  (and (gpg-wallet-on?)
+       (gpg-encrypt-save-object gpg-wallet-table-url
+                                (ahash-table->list gpg-wallet-table)
+                                gpg-wallet-key-fingerprint gpg-wallet-url)
+       (begin
+         (set! gpg-wallet-table "")
+         #t)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initalization
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-public (gpg-wallet-initialize passphrase)
-  ;; very first initialization 
-  (when (not (gpg-wallet-on?))
-  (when (not (gpg-wallet-initialized?))
-  (when (not (url-exists? gpg-wallet-url))
-    (system-mkdir gpg-wallet-url)
-    (if (not (or (os-mingw?) (os-win32?)))
-      (system-1 "chmod og-rwx" gpg-wallet-url)))
-  (when (null? (gpg-secret-keys gpg-wallet-url))
-    (gpg-gen-key "__TeXmacs_wallet__"
-                 "TeXmacs wallet" "" passphrase
-                 gpg-wallet-url))
-  (with fingerprints (gpg-secret-key-fingerprints gpg-wallet-url)
-    (if (not (null? fingerprints))
-      (begin (set-preference "gpg wallet key fingerprint" (car fingerprints))
-	     (set! gpg-wallet-table (make-ahash-table))
-             (gpg-wallet-turn-off))
-      #f)))))
+(tm-define (gpg-wallet-initialize passphrase)
+  ;; very first initialization
+  (when (and (not (gpg-wallet-on?)) (not (gpg-wallet-initialized?)))
+    (when (not (url-exists? gpg-wallet-url))
+      (system-mkdir gpg-wallet-url)
+      (when (not (or (os-mingw?) (os-win32?)))
+        (system-1 "chmod og-rwx" gpg-wallet-url)))
+    (when (null? (gpg-secret-keys gpg-wallet-url))
+      (gpg-gen-key "__TeXmacs_wallet__"
+                   "TeXmacs wallet" "" passphrase
+                   gpg-wallet-url))
+    (with fingerprints (gpg-secret-key-fingerprints gpg-wallet-url)
+      (and (nnull? fingerprints)
+           (begin
+             (set-preference "gpg wallet key fingerprint" (car fingerprints))
+             (set! gpg-wallet-table (make-ahash-table))
+             (gpg-wallet-turn-off))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Reinitialize
@@ -113,74 +110,68 @@
 
 (define (system-remove-directory url)
   (with l (url->list (url-expand (url-complete
-            (url-append url (url-any)) "f")))
-  (for-each system-remove l))
+                                  (url-append url (url-any)) "f")))
+    (for-each system-remove l))
   (rmdir (url->system url)))
 
-(define-public (gpg-wallet-reinitialize old-passphrase new-passphrase)
+(tm-define (gpg-wallet-reinitialize old-passphrase new-passphrase)
   ;; Regenerate secret key and password with no data loss.
-  (if (not (gpg-wallet-initialized?)) #f
-  (with wallet-backup (url-glue gpg-wallet-url
-    (string-append "-" (number->string (current-time))))
-  (with b #t
-  (with on? (gpg-wallet-on?)
-  (when (not on?)
-    (set! b (gpg-wallet-turn-on old-passphrase)))
-  (if b
-    (begin
-      (with table-backup gpg-wallet-table
-        (gpg-wallet-turn-off)
-        (system-move gpg-wallet-url wallet-backup)
-        (if (gpg-wallet-initialize new-passphrase)
-	  (begin
-	    (gpg-wallet-turn-on new-passphrase)
-	    (set! gpg-wallet-table table-backup)
-	    (when (not on?) (gpg-wallet-turn-off))
-	    (system-remove-directory wallet-backup)
-	    #t)
-	  (begin
-	    (system-move wallet-backup gpg-wallet-url)
-	    #f))))
-    #f))))))
+  (and (gpg-wallet-initialized?)
+       (let* ((wallet-backup
+               (url-glue gpg-wallet-url
+                         (string-append "-" (number->string (current-time)))))
+              (b #t)
+              (on? (gpg-wallet-on?)))
+         (when (not on?)
+           (set! b (gpg-wallet-turn-on old-passphrase)))
+         (and b (with table-backup gpg-wallet-table
+                  (gpg-wallet-turn-off)
+                  (system-move gpg-wallet-url wallet-backup)
+                  (if (gpg-wallet-initialize new-passphrase)
+                      (begin
+                        (gpg-wallet-turn-on new-passphrase)
+                        (set! gpg-wallet-table table-backup)
+                        (when (not on?) (gpg-wallet-turn-off))
+                        (system-remove-directory wallet-backup)
+                        #t)
+                      (begin
+                        (system-move wallet-backup gpg-wallet-url)
+                        #f)))))))
 
-(define-public (gpg-wallet-destroy)
+(tm-define (gpg-wallet-destroy)
   ;; Delete the current wallet: for security all data are completely lost!
-  (if (not (gpg-wallet-initialized?)) #f
-    ;(with wallet-backup (url-glue gpg-wallet-url
-    ;  (string-append "-" (number->string (current-time))))
-    ;(system-move gpg-wallet-url wallet-backup))
-    (begin
-      (system-remove-directory gpg-wallet-url)
-      (not (url-exists? gpg-wallet-url)))))
+  (and (gpg-wallet-initialized?)
+       ;;(with wallet-backup (url-glue gpg-wallet-url
+       ;;  (string-append "-" (number->string (current-time))))
+       ;;(system-move gpg-wallet-url wallet-backup))
+       (begin
+         (system-remove-directory gpg-wallet-url)
+         (not (url-exists? gpg-wallet-url)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Set, get, delete entries
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-public (gpg-wallet-set key val)
-  (if (gpg-wallet-on?)
-    (begin
-      (ahash-set! gpg-wallet-table key val)
-      (gpg-wallet-save))
-    #f))
+(tm-define (gpg-wallet-set key val)
+  (and (gpg-wallet-on?)
+       (begin
+         (ahash-set! gpg-wallet-table key val)
+         (gpg-wallet-save))))
 
-(define-public (gpg-wallet-get key)
-  (if (gpg-wallet-on?)
-    (ahash-ref gpg-wallet-table key)
-    #f))
+(tm-define (gpg-wallet-get key)
+  (and (gpg-wallet-on?)
+       (ahash-ref gpg-wallet-table key)))
 
-(define-public (gpg-wallet-delete key)
-  (if (gpg-wallet-on?)
-    (begin
-      (ahash-remove! gpg-wallet-table key)
-      (gpg-wallet-save))
-    #f))
+(tm-define (gpg-wallet-delete key)
+  (and (gpg-wallet-on?)
+       (begin
+         (ahash-remove! gpg-wallet-table key)
+         (gpg-wallet-save))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; List entries
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-public (gpg-wallet-entries)
-  (if (gpg-wallet-on?)
-    (ahash-table->list gpg-wallet-table)
-    #f))
+(tm-define (gpg-wallet-entries)
+  (and (gpg-wallet-on?)
+       (ahash-table->list gpg-wallet-table)))
