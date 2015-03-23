7ed3221830bb3981c5744649d841dbb0ebe8de93
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2015-03-23 20:45:05 +0000

    Activate new encoding/decoding scheme

--- TeXmacs/progs/database/db-format.scm
+++ TeXmacs/progs/database/db-format.scm
@@ -36,17 +36,22 @@
   ;; For each entry+field type, specify the encoding being used for
   ;; the field value.  This allows for instance to use TeXmacs snippets
   ;; instead of plain string values.
+  ((* "type") :identity)
+  ((* "location") :identity)
+  ((* "dir") :identity)
+  ((* "date") :identity)
+  ((* "id") :identity)
   ((* "owner") :users)
   ((* "readable") :users)
   ((* "writable") :users))
 
 (smart-table db-encoder-table
   ;; The routine being used for encoding a field value as a string
-  )
+  (,:identity ,identity))
 
 (smart-table db-decoder-table
   ;; The routine being used for decoding a field value from a string
-  )
+  (,:identity ,identity))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Encoding and decoding of lists of users
@@ -58,6 +63,7 @@
         (and (pair? l) (car l)))))
 
 (define (db-encode-users users)
+  ;;(display* "Encode users " users "\n")
   (list-filter (map db-encode-user users) identity))
 
 (define (db-decode-user id)
@@ -65,6 +71,7 @@
       (db-get-field-first id "id" #f)))
 
 (define (db-decode-users ids)
+  ;;(display* "Decode users " ids "\n")
   (list-filter (map db-decode-user ids) identity))
 
 (smart-table db-encoder-table
@@ -78,6 +85,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (db-encode-texmacs vals)
+  ;;(display* "Encode TeXmacs " vals "\n")
   (map (cut convert <> "texmacs-stree" "texmacs-snippet") vals))
 
 (define (db-decode-texmacs-one val)
@@ -85,6 +93,7 @@
     (if (tm-func? r 'document 1) (tm-ref r 0) r)))
 
 (define (db-decode-texmacs vals)
+  ;;(display* "Decode TeXmacs " vals "\n")
   (map db-decode-texmacs-one vals))
 
 (smart-table db-encoder-table
@@ -157,81 +166,13 @@
                (or (!= (car prop) "owner") (nnull? (cdr prop))))
       (db-set-field id (car prop) (cdr prop)))))
 
-(define (user-decode id)
-  (if (== id "all") id
-      (db-get-field-first id "id" #f)))
-
-(define (user-encode user)
-  (if (== user "all") user
-      (with l (db-search (list (list "type" "user") (list "id" user)))
-        (and (pair? l) (car l)))))
-
-(define (prop-decode x)
-  (with (attr . vals) x
-    (if (nin? attr '("owner" "readable" "writable")) x
-        (cons attr (list-difference (map user-decode vals) (list #f))))))
-
-(define (prop-encode x)
-  (with (attr . vals) x
-    (if (nin? attr '("owner" "readable" "writable")) x
-        (cons attr (list-difference (map user-encode vals) (list #f))))))
-
-(tm-define (db-properties-decode l)
-  ;;(display* "decode " l " -> " (map prop-decode l) "\n")
-  (map prop-decode l))
-
-(tm-define (db-properties-encode l)
-  ;;(display* "encode " l " -> " (map prop-encode l) "\n")
-  (map prop-encode l))
-
 (define (first-leq? p1 p2)
   (string<=? (car p1) (car p2)))
 
 (tm-define (db-get-all-decoded id)
   (with raw-props (sort (db-get-all id) first-leq?)
-    (db-decode-fields (db-properties-decode raw-props))))
+    (db-decode-entry raw-props)))
 
 (tm-define (db-set-all-encoded id props*)
-  (with props (db-encode-fields props*)
-    (with raw-props (db-properties-encode props)
-      (db-set-all id raw-props))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Encoding and decoding of fields as a function of type and property
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(tm-define (db-encode-field type var val)
-  ;;(display* "  Encode " val "\n")
-  (cond ((in? var (db-reserved-attributes)) val)
-        ((smart-ref db-format-table type)
-         (convert val "texmacs-stree" "texmacs-snippet"))
-        ((string? val) val)
-        (else "")))
-
-(tm-define (db-decode-field type var val)
-  ;;(display* "  Decode " val "\n")
-  (cond ((in? var (db-reserved-attributes)) val)
-        ((smart-ref db-format-table type)
-         (with r (convert val "texmacs-snippet" "texmacs-stree")
-           (if (tm-func? r 'document 1) (tm-ref r 0) r)))
-        (else val)))
-
-(tm-define (db-encode-fields l)
-  ;;(display* "  Encoding fields " l "\n")
-  (with type (assoc-ref l "type")
-    (set! type (and (pair? type) (car type)))
-    ;;(display* "    type= " type "\n")
-    (with cv (lambda (f)
-               (cons (car f)
-                     (map (cut db-encode-field type (car f) <>) (cdr f))))
-      (if type (map cv l) l))))
-
-(tm-define (db-decode-fields l)
-  ;;(display* "  Decoding fields " l "\n")
-  (with type (assoc-ref l "type")
-    (set! type (and (pair? type) (car type)))
-    ;;(display* "    type= " type "\n")
-    (with cv (lambda (f)
-               (cons (car f)
-                     (map (cut db-decode-field type (car f) <>) (cdr f))))
-      (if type (map cv l) l))))
+  (with raw-props (db-encode-entry props*)
+    (db-set-all id raw-props)))
