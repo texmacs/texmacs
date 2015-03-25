86072bc61eae8253913a91bf6da24b12ce6c0ed0
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2015-03-25 17:16:26 +0000

    Possibility to grant permissions to all users

--- TeXmacs/progs/client/client-widgets.scm
+++ TeXmacs/progs/client/client-widgets.scm
@@ -18,41 +18,62 @@
 ;; Permissions editor
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (set-permissions permissions enc server id attr vals*)
+(define (set-permission-all perms server id attr on?)
+  (with old-vals (ahash-ref perms attr)
+    (with new-vals (append (if on? (list "all") (list))
+                           (list-difference old-vals (list "all")))
+      (ahash-set! perms attr new-vals)
+      (remote-set-field server id attr new-vals))))
+
+(define (set-permissions perms enc server id attr vals*)
   (with vals (map (cut ahash-ref enc <>) vals*)
-    (ahash-set! permissions attr vals)
+    (ahash-set! perms attr vals)
     (remote-set-field server id attr vals)))
 
-(define (get-permissions permissions dec server id attr)
-  (with vals (list-union (ahash-ref permissions "owner")
-                         (ahash-ref permissions attr))
+(define (get-permissions perms dec server id attr)
+  (with vals (list-union (ahash-ref perms "owner")
+                         (ahash-ref perms attr))
     (with vals* (map (cut ahash-ref dec <>) vals)
       (sort vals* string<=?))))
 
-(tm-widget ((entry-permissions-editor server id attrs
-                                      users enc dec permissions) quit)
+(tm-widget ((entry-permissions-editor server id attrs users enc dec perms) quit)
   (padded
     (tabs
       (loop (attr attrs)
         (tab (text (upcase-first attr))
           (padded
-            (choices (set-permissions permissions enc server id attr answer)
-                     (map (cut ahash-ref dec <>) users)
-                     (get-permissions permissions dec server id attr))))))))
+            (hlist
+              (toggle (begin
+                        (set-permission-all perms server id attr answer)
+                        (refresh-now "permission-checklist"))
+                      (in? "all" (get-permissions perms dec server id attr)))
+              // //
+              (text "All users") >>)
+            ===
+            (refreshable "permission-checklist"
+              (if (nin? "all" (get-permissions perms dec server id attr))
+                  (choices (set-permissions perms enc server id attr answer)
+                           (map (cut ahash-ref dec <>) users)
+                           (get-permissions perms dec server id attr)))
+              (if (in? "all" (get-permissions perms dec server id attr))
+                  (resize "250px" "100px"
+                    (text ""))))))))))
 
 (tm-define (open-entry-permissions-editor server id attrs)
   (:interactive #t)
   (with-remote-search-user users server (list)
-    (let* ((permissions (make-ahash-table))
+    (let* ((perms (make-ahash-table))
            (enc (make-ahash-table))
            (dec (make-ahash-table)))
       (with-remote-get-entry entry server id
         (for (attr attrs)
           (with vals (or (assoc-ref entry attr) (list))
-            (ahash-set! permissions attr vals)
+            (ahash-set! perms attr vals)
             (set! users (list-union users vals))))
         (with-remote-get-user-pseudo pseudos server users
           (with-remote-get-user-name names server users
+            (ahash-set! dec "all" "all")
+            (ahash-set! enc "all" "all")
             (for-each (lambda (user pseudo name)
                         (when (and (string? pseudo) (string? name))
                           (with full (string-append pseudo " (" name ")")
@@ -61,8 +82,7 @@
                       users pseudos names)
             (set! users (list-difference users (list "all")))
             (dialogue-window (entry-permissions-editor server id attrs
-                                                       users enc dec
-                                                       permissions)
+                                                       users enc dec perms)
                              noop "Change permissions")))))))
 
 (tm-define (open-file-permissions-editor server u)
