12e11313479faef4620864918a28d347705390be
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2015-04-04 15:59:53 +0000

    Progress on conservative BibTeX export

--- TeXmacs/styles/test/database-bib.ts
+++ TeXmacs/styles/test/database-bib.ts
@@ -42,6 +42,61 @@
 
   <assign|name-jr|<macro|jr|<with|font-shape|small-caps|color|dark
   grey|<arg|jr>>>>
+
+  <\active*>
+    <\src-comment>
+      Markup for BibTeX compatibility
+    </src-comment>
+  </active*>
+
+  <assign|bib-comm-prefix|<with|font-series|bold|% >>
+
+  <assign|bib-var|<macro|body|<with|color|red|<strong|<arg|body>>>>>
+
+  <assign|bib-latex|<macro|body|<bib-line-env|red||<arg|body>>>>
+
+  <assign|bib-assign|<macro|type|val|<bib-field-env|dark
+  red|red|<macro|b|<with|color|dark red|<strong|<arg|b>>>>||<arg|type>|<arg|val>>>>
+
+  <assign|bib-line-env|<\macro|cbody|pre|body>
+    <with|color|<arg|cbody>|<arg|pre><arg|body>>
+  </macro>>
+
+  <assign|bib-line|<macro|body|<bib-line-env|red||<arg|body>>>>
+
+  <assign|bib-string|<\macro|body>
+    <\padded-normal|0.25em|1.5em>
+      <\wide-std-framed-colored|dark green|pastel green>
+        <with|font-series|bold|color|dark green|Strings: >
+      </wide-std-framed-colored>
+
+      <with|bib-assign|<macro|t|v|<bib-field-env|dark
+      green|black|<macro|b|<with|color|dark
+      green|<strong|<arg|b>>>>||<arg|t>|<arg|v>>>|bib-comment|<macro|b|<with|bib-assign|<macro|t|v|<bib-field-env|dark
+      grey|dark grey|<macro|bb|<with|color|dark
+      grey|<strong|<arg|bb>>>>|<value|bib-comm-prefix>|<arg|t>|<arg|v>>>|<arg|b>>>|<arg|body>>
+    </padded-normal>
+  </macro>>
+
+  <assign|bib-preamble|<\macro|body>
+    <\padded-normal|0.25em|1.5em>
+      <\wide-std-framed-colored|dark orange|pastel orange>
+        <with|font-series|bold|color|dark orange|Preamble: >
+      </wide-std-framed-colored>
+
+      <with|bib-latex|<macro|b|<bib-line-env|black||<arg|b>>>|bib-comment|<macro|b|<with|bib-latex|<macro|bb|<bib-line-env|dark
+      grey|<value|bib-comm-prefix>|<arg|bb>>>|<arg|b>>>|par-left|1.5em|par-first|0|<arg|body>>
+    </padded-normal>
+  </macro>>
+
+  <assign|bib-comment|<\macro|body>
+    <\with|bib-field|<macro|t|v|<bib-field-env|dark grey|dark
+    grey|<macro|b|<with|color|dark grey|<strong|<arg|b>>>>|<value|bib-comm-prefix>|<arg|t>|<arg|v>>>|bib-line|<macro|b|<bib-line-env|dark
+    grey|<value|bib-comm-prefix>|<arg|b>>>>
+      <with|bib-entry|<macro|t|i|b|<bib-entry-env|<value|bib-field>|dark
+      grey|pastel grey|dark grey|<value|bib-comm-prefix>|<arg|t>|<arg|i>|<arg|b>>>|par-first|0|<padded-normal|0.25em|1.5em|<arg|body>>>
+    </with>
+  </macro>>
 </body>
 
 <\initial>
--- src/Data/Convert/BibTeX/conservative_bib.cpp
+++ src/Data/Convert/BibTeX/conservative_bib.cpp
@@ -14,10 +14,87 @@
 #include "analyze.hpp"
 #include "scheme.hpp"
 
+static bool
+bib_skip (string s, int& i) {
+  int n= N(s);
+  while (i < n) {
+    while (i < n && s[i] != '\n') i++;
+    if (i < n) i++;
+    int j= i;
+    while (j < n && (s[j] == ' ' || s[j] == '\t')) j++;
+    if (j >= n || s[j] != '@') continue;
+    j++;
+    int k= j;
+    while (j < n && is_alpha (s[j])) j++;
+    string tp= locase_all (s (k, j));
+    bool ok=
+      (tp == "article") ||
+      (tp == "book") ||
+      (tp == "booklet") ||
+      (tp == "inbook") ||
+      (tp == "incollection") ||
+      (tp == "inproceedings") ||
+      (tp == "conference") ||
+      (tp == "manual") ||
+      (tp == "mastersthesis") ||
+      (tp == "misc") ||
+      (tp == "phdthesis") ||
+      (tp == "proceedings") ||
+      (tp == "techreport") ||
+      (tp == "unpublished");
+    if (ok) return true;
+    bool other=
+      (tp == "comment") ||
+      (tp == "string") ||
+      (tp == "preamble");
+    if (other) return false;
+  }
+  return false;
+}
+
+static void
+bib_rewind (string s, int& i) {
+  while (true) {
+    int save= i;
+    if (i > 0) i--;
+    while (i > 0 && s[i-1] != '\n') i--;
+    for (int j=i; j<save; j++)
+      if (s[j] == '%' || s[j] == '\n') break;
+      else if (s[j] == ' ' || s[j] == '\t');
+      else { i= save; return; }
+  }
+}
+
+static array<string>
+bib_break (string s) {
+  int i=0, n=N(s), start=0;
+  array<string> a;
+  while (i<n) {
+    start= i;
+    while (true) {
+      bool ok= bib_skip (s, i);
+      if (ok || i >= n) break;
+    }
+    if (i >= n) break;
+    a << s (start, i);
+    start= i;
+    (void) bib_skip (s, i);
+    bib_rewind (s, i);
+    ASSERT (i > start, "strange entry in bib_break");
+    a << s (start, i);
+    cout << "---------------------------------------------------------\n";
+    cout << s (start, i);
+  }
+  a << s (start, n);
+  return a;
+}
+
 string
 conservative_bib_export (string src_s, tree src_t, string obj_s, tree obj_t) {
   if (get_preference ("texmacs->bibtex:conservative", "off") != "on")
     return obj_s;
+  //cout << "Conservative export\n";
+  //(void) bib_break (src_s);
   if (src_t == obj_t) return src_s;
   return obj_s;
 }
