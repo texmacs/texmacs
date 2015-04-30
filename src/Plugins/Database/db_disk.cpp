7309e502c6131f0c16ec6992150d272988979e0a
Denis Raux <raux@texmacs.org>
2015-04-30 10:10:51 +0000

    windows random function

--- src/Plugins/Database/db_disk.cpp
+++ src/Plugins/Database/db_disk.cpp
@@ -16,6 +16,10 @@
 #define DB_CREATE_FIELD  2
 #define DB_REMOVE_FIELD  3
 
+#ifdef __MINGW32__
+#define random rand
+#endif
+
 /******************************************************************************
 * Marshalling and unmarshalling
 ******************************************************************************/
