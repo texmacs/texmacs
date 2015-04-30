9d2dcb9daffca2f5dea6a56477be05e089213f97
Denis Raux <raux@texmacs.org>
2015-04-30 09:02:00 +0000

    windows tools

---
+++ packages/windows/FullName.cpp
@@ -0,0 +1,126 @@
+/*
+** Get user's full name
+**
+** Denis RAUX LIX 2013
+*/
+
+#define _WIN32_DCOM
+#include <iostream>
+using namespace std;
+#include <comdef.h>
+#include <Wbemidl.h>
+
+# pragma comment(lib, "wbemuuid.lib")
+
+int main(int argc, char **argv)
+{
+  int ret = 0; //
+  string serr;
+  HRESULT hres;
+
+  if(argc < 2) {serr="Usage : fullname username [-d]"; ret=1; } 
+  else {
+    // Step 1: Initialize COM. ------------------------------------------
+    hres =  CoInitializeEx(0, COINIT_MULTITHREADED); 
+    if (FAILED(hres)) { serr = "Failed to initialize COM library."; ret=12; }
+    else {
+      // Step 2: Set general COM security levels --------------------------
+      hres =  CoInitializeSecurity(
+        NULL, 
+        -1,                          // COM authentication
+        NULL,                        // Authentication services
+        NULL,                        // Reserved
+        RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+        RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+        NULL,                        // Authentication info
+        EOAC_NONE,                   // Additional capabilities 
+        NULL                         // Reserved
+      );
+      if (FAILED(hres)) { serr = "Failed to initialize security."; ret=13; }
+      else {
+        // Step 3: Obtain the initial locator to WMI -------------------------
+        IWbemLocator *pLoc = NULL;
+        hres = CoCreateInstance(CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (LPVOID *) &pLoc);
+        if (FAILED(hres)) { serr =  "Failed to create IWbemLocator object."; ret=14; }
+        else {
+          // Step 4: Connect to WMI through the IWbemLocator::ConnectServer method
+          IWbemServices *pSvc = NULL;
+          // Connect to the root\cimv2 namespace with the current user and obtain pointer pSvc
+          // to make IWbemServices calls.
+          hres = pLoc->ConnectServer(
+            _bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
+            NULL,                    // User name. NULL = current user
+            NULL,                    // User password. NULL = current
+            0,                       // Locale. NULL indicates current
+            NULL,                    // Security flags.
+            0,                       // Authority (for example, Kerberos)
+            0,                       // Context object 
+            &pSvc                    // pointer to IWbemServices proxy
+            );
+
+          if (FAILED(hres)) { serr =  "Could not connect."; ret=15; }
+          else {
+            // Step 5: Set security levels on the proxy -------------------------
+            hres = CoSetProxyBlanket(
+              pSvc,                        // Indicates the proxy to set
+              RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+              RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+              NULL,                        // Server principal name 
+              RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+              RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+              NULL,                        // client identity
+              EOAC_NONE                    // proxy capabilities 
+              );
+            if (FAILED(hres)) { serr =  "Could not set proxy blanket."; ret=16; }
+            else {
+              // Step 6: Use the IWbemServices pointer to make requests of WMI ----
+              IEnumWbemClassObject* pEnumerator = NULL;
+              string qs("SELECT * FROM Win32_UserAccount where Name='");
+              qs += argv[1]; qs += "'";
+              hres = pSvc->ExecQuery(
+                bstr_t("WQL"), 
+                bstr_t(qs.c_str()),
+                WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, 
+                NULL,
+                &pEnumerator);
+                if (FAILED(hres)) { serr =  "Query failed."; ret=17; }
+                else {
+                  // Step 7: Get the data from the query in step 6 -------------------
+                  IWbemClassObject *pclsObj;
+                  ULONG uReturn = 0;
+                  if (!pEnumerator)  { serr = "No iterator"; ret=18; }
+                  else {
+                    hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+                    if (FAILED(hres)) { serr =  "Query failed."; ret=19; }
+                    else {
+                      if(1 == uReturn) {
+                        VARIANT vtProp;
+                        hres = pclsObj->Get(L"FullName", 0, &vtProp, 0, 0);
+                        if (FAILED(hres)) { serr =  "Get methode failed."; ret=20; }
+                        else {
+                          wcout << vtProp.bstrVal;
+                          VariantClear(&vtProp);
+                        }
+                        pclsObj->Release();
+                      } else  { serr = "No item found"; ret=3; }
+                    }
+                  }
+                  pEnumerator->Release();
+                }
+            }
+            pSvc->Release();
+          }
+          pLoc->Release();
+        }
+        CoUninitialize();
+      }
+    }
+  }
+  if(ret && argc > 2 && !strcmp(argv[2], "-d")) {
+    cerr << serr.data();
+    if(ret >= 10) cerr << " Error code = 0x" << hex << hres;
+    cerr << endl;
+  }
+  return ret;
+}
+
---
+++ packages/windows/winwallet.cpp
@@ -0,0 +1,93 @@
+#include <windows.h>
+#include <wincred.h>
+#include <tchar.h>
+#include <io.h>
+#include <stdio.h>
+#include <winbase.h>
+
+#pragma hdrstop
+
+#define ARG_CMD		1
+#define ARG_APP		2
+#define ARG_USER	3
+/******************************************************************************
+* MODULE     : winwallet.cpp
+* DESCRIPTION: to store TeXmacs password into the operating system 
+* COPYRIGHT  : (C) 2015  Denis RAUX
+*******************************************************************************
+* This software falls under the GNU general public license version 3 or later.
+* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+******************************************************************************/
+
+#define CMD_ADD		"ADD"			// Read password on standard input
+#define CMD_GETP	"GETP"
+#define CMD_GETN	"GETN"
+#define CMD_RM		"RM"
+
+#define ERR_OK				0
+#define ERR_GENE			1 
+#define ERR_CMD				2
+#define ERR_ARG				3
+#define ERR_TOOLONG		4
+#define ERR_CRED			5
+#define ERR_WR				6
+#define ERR_RD				7
+
+int _return(int err) {
+	char *msg = NULL;
+	switch(err) {
+	case ERR_OK: break;
+	case ERR_CMD:		msg= "Unknow command\n";break;
+	case ERR_ARG:		msg= "Bad arguments count\nusage: winwallet CMD Application [user] ";break;
+	case ERR_TOOLONG:	msg= "Password too long\n";break;
+	case ERR_CRED:	msg= "Credential interface error\n";break;
+	case ERR_WR:		msg= "Cannot output info\n";break;
+	case ERR_RD:		msg= "CAnnot read password\n";break;
+	case ERR_GENE:
+	default :				msg= "General error\n";break;
+	}
+	if(msg) _write(2, msg, strlen(msg));
+	return(err);
+}
+
+int  main (int argc, char *argv[])
+{
+	int ret= ERR_OK;
+
+	if(!strcmp(argv[ARG_CMD], CMD_ADD)) {
+		if(argc != 4) ret= ERR_ARG;
+		else {
+			char buf[CRED_MAX_CREDENTIAL_BLOB_SIZE+1];
+			int cnt;
+			cnt= _read(0, buf, sizeof(buf));
+			if(cnt == -1) ret= ERR_RD;
+			else if(cnt == sizeof(buf)) ret= ERR_TOOLONG;
+			else {
+				CREDENTIAL cred = {0};
+				cred.Type = CRED_TYPE_GENERIC;
+				cred.TargetName = LPSTR(argv[ARG_APP]);
+				cred.CredentialBlobSize= cnt;
+				cred.CredentialBlob= (LPBYTE)buf;
+				cred.Persist= CRED_PERSIST_LOCAL_MACHINE;
+				cred.UserName= LPSTR(argv[ARG_USER]);
+				ret= ::CredWrite (&cred, 0)? ERR_OK:ERR_CRED;
+			}
+		}
+	} else if(argc != 3) ret= ERR_ARG;
+	else if(!strcmp(argv[ARG_CMD], CMD_GETP) || !strcmp(argv[ARG_CMD], CMD_GETN)) {
+			PCREDENTIAL pcred;
+			if(::CredRead (LPSTR(argv[ARG_APP]), CRED_TYPE_GENERIC, 0, &pcred)) {
+				if(!strcmp(argv[ARG_CMD], CMD_GETP)) {
+					if(_write(1, pcred->CredentialBlob,pcred->CredentialBlobSize) == -1) ret= ERR_WR;
+				} else {
+					if(_write(1, pcred->UserName, strlen(pcred->UserName)) == -1) ret= ERR_WR;
+				}
+				// must free memory allocated by CredRead()!
+				::CredFree (pcred);
+			} else ret= ERR_CRED;
+	} else if(!strcmp(argv[ARG_CMD], CMD_RM)) {
+			if(::CredDelete (LPCSTR(argv[ARG_APP]), CRED_TYPE_GENERIC, 0) == false) ret= ERR_CRED;
+	} else ret= ERR_CMD;
+	return(_return(ret));
+}
