dc52b98d96d15b804d63378a598cfdb6f3d0055c
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2015-11-24 18:11:01 +0000

    Fix for spaces

--- TeXmacs/progs/doc/tmdoc-edit.scm
+++ TeXmacs/progs/doc/tmdoc-edit.scm
@@ -64,11 +64,7 @@
       (with n (tree-arity buf)
         (tree-insert! buf n '((tmdoc-license "")))
         (tree-go-to buf n 0 0)
-        (insert "Permission is granted to copy, distribute and/or modify this
-document under the terms of the GNU Free Documentation License, Version 1.1 or
-any later version published by the Free Software Foundation; with no Invariant
-Sections, with no Front-Cover Texts, and with no Back-Cover Texts. A copy of
-the license is included in the section entitled \"GNU Free Documentation License\".")))))
+        (insert "Permission is granted to copy, distribute and/or modify this document under the terms of the GNU Free Documentation License, Version 1.1 or any later version published by the Free Software Foundation; with no Invariant Sections, with no Front-Cover Texts, and with no Back-Cover Texts. A copy of the license is included in the section entitled \"GNU Free Documentation License\".")))))
 
 (tm-define (tmdoc-insert-copyright-and-license)
   (tmdoc-insert-gnu-fdl)
