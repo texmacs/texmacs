2f813f8eedf3dcb39b04c164b4556480b14e5570
Massimiliano Gubinelli <m.gubinelli@gmail.com>
2016-05-25 09:58:52 +0000

    update to Qt 5.6.0 and disable rpath

--- misc/tm-devel-mac/src/qt5.mk
+++ misc/tm-devel-mac/src/qt5.mk
@@ -5,12 +5,12 @@
 # Qt
 PKG             := qt5
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.5.1
-$(PKG)_CHECKSUM := 3d7e7805d849bcf6cf88788bc83aeb334e1cd875
+$(PKG)_VERSION  := 5.6.0
+$(PKG)_CHECKSUM := 4f111a4d6bb90eaed024b857b1bd3d0731ace8a2
 $(PKG)_SUBDIR   := qt-everywhere-opensource-src-$($(PKG)_VERSION)
 $(PKG)_FILE     := qt-everywhere-opensource-src-$($(PKG)_VERSION).tar.gz
 $(PKG)_WEBSITE  := http://qt.nokia.com/
-$(PKG)_URL      := http://download.qt.io/archive/qt/5.5/5.5.1/single/$($(PKG)_FILE)
+$(PKG)_URL      := http://download.qt.io/archive/qt/5.6/5.6.0/single/$($(PKG)_FILE)
 $(PKG)_DEPS     := 
 #gcc libodbc++ postgresql freetds openssl libgcrypt zlib libpng jpeg libmng tiff sqlite libiconv
 
@@ -28,33 +28,36 @@ define $(PKG)_BUILD
         -opensource \
         -confirm-license \
         -no-c++11 \
-        -arch '$(BUILD_ARCH)' \
-      -shared \
+        -platform macx-clang-32 \
+        -shared \
+        -nomake examples \
+        -nomake tools \
+        -make libs \
+        -no-rpath \
         -v
+
+
+#          -accessibility \
+#          -no-sql-db2 -no-sql-ibase -no-sql-mysql -no-sql-oci \
+#  		-no-sql-odbc -no-sql-psql -no-sql-sqlite -no-sql-sqlite2 \
+#  		-no-sql-tds \
+#  		-no-qml-debug \
+#          -force-pkg-config \
+#          -system-zlib \
+#  		-system-freetype \
+#          -openssl \
+#          -qt-libpng \
+#          -qt-libjpeg \
+#          -no-glib \
+#  		-no-compile-examples \
+#          -no-rpath \
+#          -no-opengl \
+#          -no-gstreamer \
+#          -no-dbus \
+#          -no-reduce-exports \
         
+#        -arch '$(BUILD_ARCH)' \
 #		-sdk macosx10.6  \
-#         -accessibility \
-#         -no-sql-db2 -no-sql-ibase -no-sql-mysql -no-sql-oci \
-# 		-no-sql-odbc -no-sql-psql -no-sql-sqlite -no-sql-sqlite2 \
-# 		-no-sql-tds \
-# 		-no-qml-debug \
-#         -force-pkg-config \
-#         -system-zlib \
-# 		-system-freetype \
-#         -openssl \
-#         -qt-libpng \
-#         -qt-libjpeg \
-#         -no-glib \
-#         -nomake examples \
-#         -nomake tools \
-#         -make libs \
-# 		-no-compile-examples \
-#         -no-rpath \
-#         -no-opengl \
-#         -no-gstreamer \
-#         -no-dbus \
-#         -no-reduce-exports \
-#         -v 
 
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     rm -rf '$(PREFIX)/mkspecs'
