276f0cd0bdc5e72b3f4d1075eadcbf8a99b256d7
Denis Raux <raux@texmacs.org>
2017-07-24 15:38:41 +0000

    bad return value for static Linux binaries

--- src/System/Misc/fakedlopen.c
+++ src/System/Misc/fakedlopen.c
@@ -2,8 +2,10 @@
 ** Denis RAUX - LIX 2013
 ** avoid calling external lib in case of static functions
 */
+#include <stdlib.h>
+
 #if defined(__TMSTATIC__) && !defined(OS_MINGW) && !defined(__WIN32__)
-int dlopen(const char *name, int flag) {
-   return(-1);
+void* dlopen(const char *name, int flag) {
+   return(NULL);
 }
 #endif
