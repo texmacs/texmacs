8400383bfff2f5e6823f5a303eabea85af6f3d4f
Denis Raux <raux@texmacs.org>
2017-09-18 14:37:34 +0000

    dll bundling tool

---
+++ packages/windows/copydll.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Denis RAUX LIX 2016
+# copy the dll files of $1
+#
+# we prefer to use ntldd which gives accurate result on windows10
+# on old environment only ldd is available
+#
+typeset mingwbase=$(cygpath -m /).$'*'
+if test -x "$1"
+then
+	typeset dest=$(dirname $1)
+# with for we have one parameter per loop
+# windows backslash need to be converted to slash in order being able be compared
+	ntldd -R $1 |while read -r lib sign  full trash
+	do [[ ! -x $dest/$lib && ${full//\\/\/} =~ ${mingwbase} ]] && cp $full $dest
+	done
+fi
