50fc333faa8dfa9d9d99d114afe3dc0a5e21677b
Denis Raux <raux@texmacs.org>
2017-12-19 19:29:30 +0000

    for spaces in printing command path

--- src/System/Misc/sys_utils.cpp
+++ src/System/Misc/sys_utils.cpp
@@ -145,7 +145,7 @@ get_printing_default () {
 #if defined (OS_MINGW)
   url embedded ("$TEXMACS_PATH/bin/SumatraPDF.exe");
   if (exists (embedded))
-    return concretize (embedded) * " -print-dialog -exit-when-done";
+    return sys_concretize (embedded) * " -print-dialog -exit-when-done";
   else return "";
 #else
   return "lp";
