1f45a47b1a04e81a47d5f205137dd20a2fba1c45
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2018-03-07 19:23:22 +0000

    Generate a status report

--- TeXmacs/progs/utils/test/test-convert.scm
+++ TeXmacs/progs/utils/test/test-convert.scm
@@ -174,17 +174,179 @@
         (for-each (lambda (x) (test-file x dir type)) u5)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Checking the test suite against a reference build
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define missing-dirs (list))
+(define missing-files (list))
+(define changed-files (list))
+(define changed-sizes (list))
+(define changed-properties (list))
+
+(define (compare-text-file x dir ref)
+  ;;(display* "-- compare-text-file " x ", " dir ", " ref "\n")
+  (let* ((new-file (url-append dir x))
+         (ref-file (url-append ref x)))
+    (when (and (not (url-exists? new-file)) (url-exists? ref-file))
+      (display* "TeXmacs] Missing file " (url->system x) "\n")
+      (set! missing-files (cons new-file missing-files)))
+    (when (and (url-exists? dir) (url-exists? ref))
+      (display* "TeXmacs]   Comparing " (url->system x) "\n")
+      (let* ((new-s (string-load new-file))
+             (ref-s (string-load ref-file))
+             (dist (string-distance new-s ref-s)))
+        (cond ((== dist 0) (noop))
+              ((< dist 5) (noop))
+              (else
+                (display* "TeXmacs]     Changed " dist " bytes\n")
+                (with c (list new-file dist)
+                  (set! changed-files (cons c changed-files)))))))))
+
+(define (pdf-keep? s)
+  (and (not (string-starts? s "CreationDate:"))
+       (not (string-starts? s "File size:"))))
+
+(define (pdf-info u)
+  (if (or (not (url-exists? u))
+          (not (url-exists-in-path? "pdfinfo")))
+      ""
+      (let* ((cmd (string-append "pdfinfo " (url->system u)))
+             (info (eval-system cmd))
+             (l (cpp-string-tokenize info "\n"))
+             (k (list-filter l pdf-keep?)))
+        (cpp-string-recompose k "\n"))))
+
+(define (compare-pdf-file x dir ref)
+  ;;(display* "-- compare-pdf-file " x ", " dir ", " ref "\n")
+  (let* ((new-file (url-append dir x))
+         (ref-file (url-append ref x)))
+    (when (and (not (url-exists? new-file)) (url-exists? ref-file))
+      (display* "TeXmacs] Missing file " (url->system x) "\n")
+      (set! missing-files (cons new-file missing-files)))
+    (when (and (url-exists? dir) (url-exists? ref))
+      (display* "TeXmacs]   Comparing " (url->system x) "\n")
+      (let* ((new-size (string-length (string-load new-file)))
+             (ref-size (string-length (string-load ref-file)))
+             (delta-size (- new-size ref-size))
+             (new-s (pdf-info new-file))
+             (ref-s (pdf-info ref-file)))
+        (when (> (abs delta-size) 150)
+          (display* "TeXmacs]     Size change of " delta-size " bytes\n")
+          (with c (list new-file delta-size)
+            (set! changed-sizes (cons c changed-sizes))))
+        (when (!= new-s ref-s)
+          (display* "TeXmacs]     Changed Pdf properties\n")
+          (with c (list new-file new-s)
+            (set! changed-properties (cons c changed-properties))))))))
+
+(define (compare-texmacs-file x dir ref)
+  ;;(display* "-- compare-texmacs-file " x ", " dir ", " ref "\n")
+  (cond ((string-ends? x ".pdf")
+         (compare-pdf-file x dir ref))
+        ((string-ends? x ".tex")
+         (compare-text-file x dir ref))))
+
+(define (compare-arxiv-file x dir ref)
+  ;;(display* "-- compare-arxiv-file " x ", " dir ", " ref "\n")
+  (cond ((string-ends? x ".tm")
+         (compare-text-file x dir ref))
+        ((string-ends? x ".pdf")
+         (compare-pdf-file x dir ref))))
+
+(define (compare-file x dir ref type)
+  ;;(display* "-- compare-file " x ", " dir ", " ref ", " type "\n")
+  (cond ((== type "texmacs") (compare-texmacs-file x dir ref))
+        ((== type "arxiv") (compare-arxiv-file x dir ref))))
+
+(define (compare-dir dir ref type)
+  ;;(display* "-- compare-dir " dir ", " ref ", " type "\n")
+  (let* ((last (url->string (url-tail dir)))
+         (accept (list "texmacs" "arxiv")))
+    (set! type (or type (and (in? last accept) last)))
+    (when (and (not (url-exists? dir)) (url-exists? ref))
+      (display* "TeXmacs] Missing directory " (url->system dir) "\n")
+      (set! missing-dirs (cons dir missing-dirs)))
+    (when (and (url-exists? dir) (url-exists? ref))
+      (display* "TeXmacs] Entering directory " (url->system dir) "\n")
+      (let* ((u1 (url-append ref (url-wildcard "*")))
+             (u2 (url->list (url-expand (url-complete u1 "dr"))))
+             (u3 (map url->string (map url-tail u2)))
+             (u4 (url->list (url-expand (url-complete u1 "fr"))))
+             (u5 (map url->string (map url-tail u4))))
+        (for-each (lambda (x) (compare-dir (url-append dir x)
+                                           (url-append ref x) type)) u3)
+        (for-each (lambda (x) (compare-file x dir ref type)) u5)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Generate status report
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (status-entry title dir x)
+  (let* ((u (if (list? x) (car x) x))
+         (d (url-delta (url-append dir "dummy") u)))
+    (display* "  " (url->system d) "\n")
+    `(concat (item) (verbatim ,(url->system d)))))
+
+(define (status-section title dir l)
+  (if (null? l) l
+      (begin
+        (display* "\n--------------------------------------------------\n")
+        (display* title)
+        (display* "\n--------------------------------------------------\n")
+        (list `(section* ,title)
+              `(itemize
+                (document
+                  ,@(map (cut status-entry title dir <>) (reverse l))))))))
+
+(define (status-report dir)
+  (let* ((u (url-append dir "status-report.tm"))
+         (l1 (status-section "Missing directories"
+                             dir missing-dirs))
+         (l2 (status-section "Missing files"
+                             dir missing-files))
+         (l3 (status-section "Files with important changes"
+                             dir changed-files))
+         (l4 (status-section "Files with changed sizes"
+                             dir changed-sizes))
+         (l5 (status-section "Pdf files with changed properties"
+                             dir changed-properties))
+         (l (append l1 l2 l3 l4 l5)))
+    (if (null? l)
+        (if (url-exists? u) (system-remove u))
+        (let* ((body `(document ,@l))
+               (doc `(document
+                       (TeXmacs ,(texmacs-version))
+                       (style "generic")
+                       (body ,body)))
+               (t (stree->tree doc))
+               (d (convert t "texmacs-tree" "texmacs-document")))
+          (string-save d u)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Running the test suite
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(tm-define (build-ref-suite orig-dir)
+  (let* ((dir (url-expand (url-complete orig-dir "dr")))
+         (head (url-head dir))
+         (tail (url->string (url-tail dir)))
+         (ref-dir (url-append head (string-append tail "-ref"))))
+    (unpack-dir dir ref-dir #f)
+    (test-dir ref-dir #f)))
 
 (tm-define (run-test-suite orig-dir)
   (let* ((dir (url-expand (url-complete orig-dir "dr")))
          (head (url-head dir))
          (tail (url->string (url-tail dir)))
          (ref-dir (url-append head (string-append tail "-ref")))
-         ;;(check-dir (url-append head (string-append tail "-check")))
-         ;;(dest-dir (if (url-exists? ref-dir) check-dir ref-dir))
-         (dest-dir ref-dir)
-         )
-    (unpack-dir dir dest-dir #f)
-    (test-dir dest-dir #f)))
+         (check-dir (url-append head (string-append tail "-check"))))
+    (when (url-exists? ref-dir)
+      (unpack-dir dir check-dir #f)
+      (test-dir check-dir #f)
+      (set! missing-dirs (list))
+      (set! missing-files (list))
+      (set! changed-files (list))
+      (set! changed-sizes (list))
+      (set! changed-properties (list))
+      (compare-dir check-dir ref-dir #f)
+      (status-report check-dir))))
