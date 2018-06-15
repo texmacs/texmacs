e04ae2fb031fbee8ae4e05cefb74cce3d316aaf2
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2018-06-15 21:36:31 +0000

    Start style file for A0 posters

---
+++ TeXmacs/styles/poster.ts
@@ -0,0 +1,70 @@
+<TeXmacs|1.99.6>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|poster|1.0>
+
+      <\src-purpose>
+        Style for A0 posters.
+      </src-purpose>
+
+      <\src-copyright|2018>
+        Joris van der Hoeven
+      </src-copyright>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
+    </src-title>
+  </active*>
+
+  <use-package|generic|two-columns>
+
+  \;
+
+  <assign|page-type|a0>
+
+  <assign|page-medium|paper>
+
+  <assign|page-screen-margin|false>
+
+  <assign|magnification|3>
+
+  \;
+
+  <assign|page-odd|3cm>
+
+  <assign|page-even|3cm>
+
+  <assign|page-right|3cm>
+
+  <assign|page-top|3cm>
+
+  <assign|page-bot|3cm>
+
+  \;
+
+  <assign|page-odd-footer|>
+
+  <assign|page-even-footer|>
+
+  \;
+
+  <assign|ornament-hpadding|2spc>
+
+  <assign|ornament-vpadding|2spc>
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
