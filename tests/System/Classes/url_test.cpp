6e31f97cff7a8da906ebc65c9ce0e37803618a13
Darcy Shen <sadhen@zoho.com>
2018-10-21 07:49:43 +0000

    Some unit tests on url

---
+++ tests/System/Classes/url_test.cpp
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+
+#include "file.hpp"
+
+TEST (url, exist) {
+  // two cases: root directory
+  // TODO: Windows compatibility
+  url root_tmp = url ("/tmp");
+  url root_no_such_tmp = url ("/no_such_tmp");
+  EXPECT_EQ (exists (root_tmp), true);
+  EXPECT_EQ (exists (root_no_such_tmp), false);
+}
+
+TEST (url, suffix) {
+  // empty suffix should work
+  url no_suffix = url ("/a/b/c/d/no_suffix");
+  EXPECT_EQ (suffix (no_suffix), string (""));
+  url no_suffix2 = url ("/a/b.c/d/no_suffix");
+  EXPECT_EQ (suffix (no_suffix2), string (""));
+
+  // normal suffix should work
+  url png = url ("/a/b/c/d.png");
+  EXPECT_EQ (suffix (png), string ("png"));
+  url png2 = url ("/a/b.c/d.png");
+  EXPECT_EQ (suffix (png2), string ("png"));
+}
