1a823f1b68d71506ace22c59ebbed9c19f809f3a
Darcy Shen <sadhen@zoho.com>
2018-12-08 11:08:45 +0000

    Add some markdown style shortcuts

--- TeXmacs/progs/text/text-kbd.scm
+++ TeXmacs/progs/text/text-kbd.scm
@@ -65,6 +65,13 @@
   ("/ /" "//")
   ("/ / var" (make 'deleted))
 
+  ;; Markdown Style Shortcuts
+  ("# var" (make 'title))
+  ("# # var" (make 'section))
+  ("# # # var" (make 'subsection))
+  ("# # # # var" (make 'subsubsection))
+  ("` ` ` var" (make 'verbatim-code))
+
   ("space var" (make 'nbsp))
   ("space var var" (make-space "1em"))
   ("_" "_")
