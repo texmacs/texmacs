b33065b20ace58403b4ef38296faa28f43526813
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2019-01-31 23:03:45 +0000

    Prepare use of systematic names

--- TeXmacs/packages/beamer/title/title-bar.ts
+++ TeXmacs/packages/beamer/title/title-bar.ts
@@ -30,12 +30,12 @@
 
   <assign|title-vpadding|0spc>
 
-  <assign|title-font-magnify|1>
+  <assign|title-font-size|1>
 
   <assign|tit|<\macro|body>
     <\surround|<assign|page-this-top|0mm>|<assign|gpag-length|<macro|<minus|1pag|2.25fn>>><vspace|5mm>>
       <\with|par-left|<minus|<value|page-odd>>|par-right|<minus|<value|page-right>>>
-        <with|color|<title-color>|math-color|<title-color>|font-size|<value|title-font-magnify>|<resize|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-tsep|<value|title-vpadding>>|<cwith|1|1|1|-1|cell-bsep|<value|title-vpadding>>|<cwith|1|1|1|-1|cell-background|<title-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-hyphen|t>|<cwith|2|2|1|1|cell-vcorrect|n>|<cwith|2|2|1|1|cell-halign|l>|<twith|table-valign|T>|<table|<row|<\cell>
+        <with|color|<title-color>|math-color|<title-color>|font-size|<value|title-font-size>|<resize|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-tsep|<value|title-vpadding>>|<cwith|1|1|1|-1|cell-bsep|<value|title-vpadding>>|<cwith|1|1|1|-1|cell-background|<title-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-hyphen|t>|<cwith|2|2|1|1|cell-vcorrect|n>|<cwith|2|2|1|1|cell-halign|l>|<twith|table-valign|T>|<table|<row|<\cell>
           <title-left|<arg|body>><htab|5mm><arg|body><htab|5mm><title-right|<arg|body>>
         </cell>>|<row|<cell|<with|color|grey|locus-color|grey|opacity|100%|<tiny|<screens-bar|<quote-arg|body>>>>>>>>>||0em||>>
       </with>
