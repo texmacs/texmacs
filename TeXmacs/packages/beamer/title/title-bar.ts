f5cb20c736e6c61c58d3ce10a414001a4b787013
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2019-02-12 07:03:23 +0000

    Spacing adjustments for more customizable title bars in beamer mode

--- TeXmacs/packages/beamer/title/title-bar.ts
+++ TeXmacs/packages/beamer/title/title-bar.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.99.8>
+<TeXmacs|1.99.9>
 
 <style|source>
 
@@ -29,11 +29,11 @@
   <assign|title-theme|title-bar>
 
   <assign|tit|<\macro|body>
-    <\surround|<assign|page-this-top|0mm>|<assign|gpag-length|<macro|<minus|1pag|2.25fn>>><vspace|5mm>>
-      <\with|par-left|<minus|<value|page-odd>>|par-right|<minus|<value|page-right>>>
+    <\surround|<assign|page-this-top|0mm>|<assign|gpag-length|<macro|<minus|1pag|<plus|<times|2|<value|title-vpadding>>|<times|<value|title-font-size>|1.5fn>|0.8fn|<value|par-par-sep>>>>>>
+      <\with|par-left|<minus|<value|page-odd>>|par-right|<minus|<value|page-right>>|par-par-sep|0fn>
         <with|color|<title-color>|math-color|<title-color>|font-size|<if|<value|title-old-style>|1|<value|title-font-size>>|<resize|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-tsep|<value|title-vpadding>>|<cwith|1|1|1|-1|cell-bsep|<value|title-vpadding>>|<cwith|1|1|1|-1|cell-background|<title-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-hyphen|t>|<cwith|2|2|1|1|cell-vcorrect|n>|<cwith|2|2|1|1|cell-halign|l>|<twith|table-valign|T>|<table|<row|<\cell>
           <title-left|<arg|body>><htab|5mm><arg|body><htab|5mm><title-right|<arg|body>>
-        </cell>>|<row|<cell|<with|color|grey|locus-color|grey|opacity|100%|<tiny|<screens-bar|<quote-arg|body>>>>>>>>>||0em||>>
+        </cell>>|<row|<cell|<with|color|grey|locus-color|grey|opacity|100%|<tiny|<screens-bar|<quote-arg|body>>>>>>>>>||<plus|1b|0.2fn>||>>
       </with>
     </surround>
   </macro>>
