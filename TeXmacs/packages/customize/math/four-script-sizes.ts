19b76ab08f2266d43935e01a8714dac0cb11f464
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2019-03-13 18:16:09 +0000

    Extra package for four levels of script sizes

---
+++ TeXmacs/packages/customize/math/four-script-sizes.ts
@@ -0,0 +1,32 @@
+<TeXmacs|1.99.9>
+
+<style|source>
+
+<\body>
+  <active*|<\src-title>
+    <src-package|four-script-sizes|1.0>
+
+    <\src-purpose>
+      Use four levels of sizes for subscripts and superscripts
+    </src-purpose>
+
+    <src-copyright|2019|Joris van der Hoeven>
+
+    <\src-license>
+      This software falls under the <hlink|GNU general public license,
+      version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+      WARRANTY WHATSOEVER. You should have received a copy of the license
+      which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+    </src-license>
+  </src-title>>
+
+  <assign|math-font-sizes|<tuple|<tuple|all|*0.8|*0.6|*0.5>>>
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
