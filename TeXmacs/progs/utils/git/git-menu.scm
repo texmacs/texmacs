ace9fb5a328c62fe1bb9e1209542deb8d9a7463b
Darcy Shen <sadhen@zoho.com>
2019-03-30 07:07:40 +0000

    Add COPYRIGHT for git-*.scm

--- TeXmacs/progs/utils/git/git-menu.scm
+++ TeXmacs/progs/utils/git/git-menu.scm
@@ -1,3 +1,16 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : git-menu.scm
+;; DESCRIPTION : menu for the Git tools
+;; COPYRIGHT   : (C) 2019  Darcy Shen
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (texmacs-module (utils git git-menu)
   (:use (utils git git-utils)
         (utils git git-tmfs)))
