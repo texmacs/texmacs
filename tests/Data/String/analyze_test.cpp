df399c22e2cf0a564380a9cab0baa5b761ef95d8
Darcy Shen <sadhen@zoho.com>
2019-04-23 13:55:26 +0000

    Unit Tests: more for analyze.cpp

--- tests/Data/String/analyze_test.cpp
+++ tests/Data/String/analyze_test.cpp
@@ -13,6 +13,16 @@
 
 #include "analyze.hpp"
 
+TEST (string, is_alpha) {
+  for (unsigned char c=0; c<255; c++) {
+    if ((c>=65 && c<=90) || (c>=97 && c<=122)) {
+      ASSERT_TRUE (is_alpha (c));
+    } else {
+      ASSERT_FALSE (is_alpha (c));
+    }
+  }
+}
+
 TEST (string, locase_all) {
   ASSERT_TRUE (locase_all (string ("true")) == string ("true"));
   ASSERT_TRUE (locase_all (string ("TRue")) == string ("true"));
@@ -25,4 +35,29 @@ TEST (string, upcase_all) {
   ASSERT_TRUE (upcase_all (string ("TRue")) == string ("TRUE"));
   ASSERT_TRUE (upcase_all (string ("TRUE")) == string ("TRUE"));
   ASSERT_TRUE (upcase_all (string ("123true")) == string ("123TRUE"));
+}
+
+TEST (string, string_minus) {
+  ASSERT_TRUE (string_minus ("Hello World", "eo") == string ("Hll Wrld"));
+  ASSERT_TRUE (string_minus ("", "abc") == string (""));
+  ASSERT_TRUE (string_minus ("abc", "") == string ("abc"));
+}
+
+TEST (string, string_union) {
+  ASSERT_TRUE (string_union ("abc", "") == string("abc"));
+  ASSERT_TRUE (string_union ("", "abc") == string("abc"));
+  ASSERT_TRUE (string_union ("Hello World", "eo") == string ("Hll Wrldeo"));
+}
+
+TEST (string, starts) {
+  ASSERT_TRUE (starts ("abc_def", "abc"));
+  ASSERT_FALSE (starts ("abc_def", "def"));
+  ASSERT_TRUE (starts ("abc", ""));
+  ASSERT_TRUE (starts ("", ""));
+}
+
+TEST (string, ends) {
+  ASSERT_TRUE (ends ("abc_def", "def"));
+  ASSERT_TRUE (ends ("abc_def", ""));
+  ASSERT_FALSE (ends ("abc_def", "de"));
 }
