cc35d5e9e0270761129c45dce41cff4682d88289
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2019-05-09 16:29:49 +0000

    Start styles for new website

---
+++ TeXmacs/styles/documentation/texmacs/tmweb1.ts
@@ -0,0 +1,37 @@
+<TeXmacs|1.0.7.14>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|tmweb|1.0>
+
+      <\src-purpose>
+        Style for generating the <TeXmacs> web pages.
+      </src-purpose>
+
+      <\src-copyright|2003--2004>
+        Joris van der Hoeven
+      </src-copyright>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
+    </src-title>
+  </active*>
+
+  <use-package|tmdoc|tmdoc-web>
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|language|english>
+    <associate|preamble|true>
+  </collection>
+</initial>
---
+++ TeXmacs/styles/documentation/texmacs/tmweb2.ts
@@ -0,0 +1,36 @@
+<TeXmacs|1.99.9>
+
+<style|<tuple|source|english>>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|tmweb|1.0>
+
+      <\src-purpose>
+        Style for generating the <TeXmacs> web pages.
+      </src-purpose>
+
+      <\src-copyright|2019>
+        Joris van der Hoeven
+      </src-copyright>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
+    </src-title>
+  </active*>
+
+  <use-package|tmdoc|tmdoc-web2>
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
