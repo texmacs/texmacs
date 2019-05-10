e67122d6dff8bd0314bb2ceda0673f21b0b4955b
Darcy Shen <sadhen@zoho.com>
2019-05-10 13:58:03 +0000

    Git: cleaning

--- TeXmacs/progs/version/version-git.scm
+++ TeXmacs/progs/version/version-git.scm
@@ -297,3 +297,83 @@
     (and (> (length ret2) 0)
          (string-null? (cAr ret2))
          (map convert (cDr ret2)))))
+
+(tm-define (git-show-log)
+  (cursor-history-add (cursor-path))
+  (revert-buffer "tmfs://git/log"))
+
+(tm-define (git-show-status)
+  (cursor-history-add (cursor-path))
+  (revert-buffer "tmfs://git/status"))
+
+(tm-define ($staged-file status file)
+  (cond ((string-starts? status "A")
+         (list 'concat "new file:   " file (list 'new-line)))
+        ((string-starts? status "M")
+         (list 'concat "modified:   " file (list 'new-line)))
+        ((string-starts? status "R")
+         (list 'concat "renamed:    " file (list 'new-line)))
+        (else "")))
+
+(tm-define ($unstaged-file status file)
+  (cond ((string-ends? status "M")
+         (list 'concat "modified:   " file (list 'new-line)))
+        (else "")))
+
+(tm-define ($untracked-file status file)
+  (cond ((== status "??")
+         (list 'concat file (list 'new-line)))
+        (else "")))
+
+(tm-define (git-status-content)
+  (with s (git-status)
+        ($generic
+         ($when (not s)
+                "Not git status available!")
+         ($when s
+                ($tmfs-title "Git Status")
+                ($description-long
+                 ($describe-item "Changes to be commited"
+                                 ($for (x s)
+                                       ($with (status file) x
+                                              ($staged-file status
+                                                            file))))
+                 ($describe-item "Changes not staged for commit"
+                                 ($for (x s)
+                                       ($with (status file) x
+                                              ($unstaged-file status
+                                                              file))))
+                 ($describe-item "Untracked files"
+                                 ($for (x s)
+                                       ($with (status file) x
+                                              ($untracked-file status
+                                                               file)))))))))
+
+
+(tm-define (git-log-content)
+  (with h (git-log)
+        ($generic
+         ($tmfs-title "Git Log")
+         ($when (not h)
+                "This directory is not under version control.")
+         ($when h
+                ($description-long
+                 ($for (x h)
+                       ($with (date by msg commit) x
+                              ($describe-item
+                               ($inline "Commit " commit
+                                        " by " (utf8->cork by)
+                                        " on " date)
+                               (utf8->cork msg)))))))))
+
+(tmfs-title-handler (git name doc)
+  (cond ((== name "status") "Git Status")
+        ((== name "log") "Git Log")
+        (else "unknown")))
+
+(tmfs-load-handler (git name)
+  (cond ((== name "status")
+         (git-status-content))
+        ((== name "log")
+         (git-log-content))
+        (else '())))
