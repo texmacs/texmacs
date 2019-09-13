f7ea890bb3ccc5e372ab95146faeb3946ece0558
Denis Raux <raux@texmacs.org>
2019-09-13 13:09:03 +0000

    mime and kde desktop

--- packages/linux/mime.sh
+++ packages/linux/mime.sh
@@ -9,23 +9,27 @@ mode=$1;shift
 # exit if we are in a packaging process
 test ! -w /usr/share -a "$XDG_UTILS_INSTALL_MODE" == "system" && exit 0
 
-if test -w /usr/share
-then  export svgdst=/usr/share/icons/hicolor/scalable
-			export XDG_UTILS_INSTALL_MODE=system
-else 	export svgdst=$HOME/.local/share/icons/hicolor/scalable
-			export XDG_UTILS_INSTALL_MODE=user
+if test -z "$XDG_UTILS_INSTALL_MODE"
+then  test -w /usr/share && export XDG_UTILS_INSTALL_MODE=system ||
+        export XDG_UTILS_INSTALL_MODE=user
 fi
 export XDG_UTILS_DEBUG_LEVEL=0
 
 case $mode in
 install)
-	test -n "$1" && cd $1 || exit 11
-	xdg-desktop-menu install --novendor texmacs.desktop
-	xdg-mime install --novendor texmacs.xml
-	;;
+  test -n "$1" && cd $1 || exit 11
+  #test if packaging mode
+  if test ! -w /usr/share -a "$XDG_UTILS_INSTALL_MODE" == "system"
+  then cp texmacs.desktop $DESTDIR/usr/share/applications/
+  else 
+    xdg-desktop-menu install --novendor texmacs.desktop
+    xdg-mime install --novendor texmacs.xml
+  fi
+  ;;
 uninstall)
-	xdg-desktop-menu uninstall texmacs.desktop
-	xdg-mime uninstall texmacs.xml
-	;;
+  test -n "$1" && cd $1 || exit 11
+  xdg-desktop-menu uninstall texmacs.desktop
+  xdg-mime uninstall texmacs.xml
+  ;;
 *) exit 1;;
 esac
