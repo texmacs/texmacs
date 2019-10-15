524b1d92448acc7e71147ad878b96b46d60b0690
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2019-10-15 07:45:01 +0000

    Add missing file

---
+++ TeXmacs/packages/customize/spacing/old-dots.ts
@@ -0,0 +1,42 @@
+<TeXmacs|1.99.11>
+
+<style|source>
+
+<\body>
+  <active*|<\src-title>
+    <src-package-dtd|old-dots|1.0|old-dots|1.0>
+
+    <\src-purpose>
+      Old-style mathematical dots of TeXmacs version 1.99.11 and below.
+    </src-purpose>
+
+    <src-copyright|2019|Joris van der Hoeven>
+
+    <\src-license>
+      This software falls under the <hlink|GNU general public license,
+      version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+      WARRANTY WHATSOEVER. You should have received a copy of the license
+      which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+    </src-license>
+  </src-title>>
+
+  <assign|low-dots|<uninit>>
+
+  <assign|low-dots*|<uninit>>
+
+  <assign|center-dots|<uninit>>
+
+  <assign|high-dots|<uninit>>
+
+  <assign|vertical-dots|<uninit>>
+
+  <assign|diagonal-dots|<uninit>>
+
+  <assign|upward-dots|<uninit>>
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
