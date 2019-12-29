3278ac449463da1a8cb68f23d69a6da8afd7846a
Darcy Shen <sadhen@zoho.com>
2019-12-29 03:06:35 +0000

    Octave: Fix bat script on Windows

--- plugins/octave/bin/tm_octave.bat
+++ plugins/octave/bin/tm_octave.bat
@@ -2,7 +2,7 @@
 rem denis RAUX LIX 2013
 rem a simple trick to
 set /p DUMMY="verbatim:" <nul
-octave -v
+rem octave -v
 rem cd $TEXMACS_PATH/plugins/octave/octave; exec octave -qi tm-start.oct
 cd %TEXMACS_PATH%\plugins\octave\octave
-octave -qi tm-start.oct
+octave -qi tm-start.m
