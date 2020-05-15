d76010a763da948e068aa881d085f576d7205734
Darcy Shen <sadhen@zoho.com>
2020-05-15 14:18:45 +0000

    Octave: Improved repl

--- plugins/octave/octave/tm/isnewans.m
+++ plugins/octave/octave/tm/isnewans.m
@@ -1,7 +1,24 @@
-function L=isnewans(A)
-  if !strcmp(typeinfo (A), "string") && !strcmp(typeinfo (A), "sq_string") && !strcmp(typeinfo (A), "dq_string")
+
+###############################################################################
+##
+## MODULE      : isnewans.m
+## DESCRIPTION : Determines if we should display an answer
+## COPYRIGHT   : (C) 2004     Joris van der Hoeven
+##               (C) 2014     François Poulain
+##               (C) 2020     Darcy Shen
+##
+## This software falls under the GNU general public license version 3 or later.
+## It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+## in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+
+function L= isnewans (A)
+  # typeinfo (A) is not string/sq_string/dq_string
+  if !strcmp(typeinfo (A), "string") && ...
+     !strcmp(typeinfo (A), "sq_string") && ...
+     !strcmp(typeinfo (A), "dq_string")
     L= 1;
-  elseif !strcmp(A, "texmacs")
+  # A is not an empty string
+  elseif !strcmp(A, "")
     L= 1;
   else
     L= 0;
--- plugins/octave/octave/tm/tmrepl.m
+++ plugins/octave/octave/tm/tmrepl.m
@@ -1,19 +1,41 @@
+
+###############################################################################
+##
+## MODULE      : tmrepl.m
+## DESCRIPTION : REPL loop
+## COPYRIGHT   : (C) 2004-2010  Joris van der Hoeven
+##               (C) 2014       François Poulain
+##               (C) 2020       Darcy Shen
+##
+## This software falls under the GNU general public license version 3 or later.
+## It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+## in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+
+
 function tmrepl()
-	prompt=sprintf("%cchannel:prompt%coctave> %c",2,5,5);
-	r=input(prompt, "s");
-	ans="texmacs";
-	while ( 1 )
-		if r(length(r))!=";"
-			dispans=1;
-		else
-			dispans=0;
-		endif
-                ans= "texmacs";
-		r=sprintf("%s;",r);
-		eval(r,"tmlasterr");
-		if dispans && isnewans(ans)
-                       tmdisp(ans);
-		endif
-		r=input(prompt,'s');
-	endwhile
+  prompt= sprintf ("%cchannel:prompt%coctave> %c", 2, 5, 5);
+  while (1)
+    r= input (prompt,'s');
+
+    if r(length (r)) != ";"
+      disp_ans= 1;
+    else
+      disp_ans= 0;
+    endif
+
+    trimed_r= strtrim (r);
+    if isvarname (trimed_r) && exist (trimed_r)
+      r= sprintf ("ans= %s;", r);
+    else
+      # if ans is not changed, do not display it
+      # otherwise, display it
+      ans= "";
+      r= sprintf ("%s;", r);
+    endif
+
+    eval (r, "tmlasterr");
+    if disp_ans && isnewans (ans)
+      tmdisp (ans);
+    endif
+  endwhile
 endfunction
