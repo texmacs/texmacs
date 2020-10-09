4d92896c5cd6a164bb49c776713913fab47435b2
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2020-10-09 11:17:27 +0000

    Temporarily allow tweaking of the retina settings under Qt5

--- TeXmacs/progs/texmacs/menus/view-menu.scm
+++ TeXmacs/progs/texmacs/menus/view-menu.scm
@@ -110,7 +110,7 @@
   ("Presentation mode" (toggle-full-screen-mode))
   ("Show panorama" (toggle-panorama-mode))
   ("Remote control" (toggle-remote-control-mode))
-  (assuming (and (os-macos?) (not (qt5-or-later-gui?)))
+  (assuming (os-macos?)
     ("Retina settings" (open-retina-settings)))
   (assuming (not (os-macos?))
     ("High resolution settings" (open-retina-settings)))
