5608cebdcffce8d9601af1c3fb694bde1f8e7dde
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2020-10-31 22:39:32 +0000

    Fix warning

--- src/Plugins/Qt/qt_tm_widget.cpp
+++ src/Plugins/Qt/qt_tm_widget.cpp
@@ -226,14 +226,14 @@ qt_tm_widget_rep::qt_tm_widget_rep(int mask, command _quit)
   //
   // NOTICE: setFixedHeight must be after setIconSize
   // TODO: the size of the toolbar should be calculated dynamically
-  int toolbarHeight= 30;
 #if (QT_VERSION >= 0x050000)
+  int toolbarHeight= 30;
   mainToolBar->setFixedHeight (toolbarHeight + 8);
   modeToolBar->setFixedHeight (toolbarHeight + 4);
   focusToolBar->setFixedHeight (toolbarHeight);
 #else
 #ifndef Q_OS_MAC
-  toolbarHeight *= retina_icons;
+  int toolbarHeight= 30 * retina_icons;
   mainToolBar->setFixedHeight (toolbarHeight + 8);
   modeToolBar->setFixedHeight (toolbarHeight + 4);
   focusToolBar->setFixedHeight (toolbarHeight);  
