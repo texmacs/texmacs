457db9ba98a03c0e27e5375349c13b9e0748dc84
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2020-11-07 16:39:01 +0000

    Don't follow links on Meta+Click

--- src/Edit/Interface/edit_mouse.cpp
+++ src/Edit/Interface/edit_mouse.cpp
@@ -126,7 +126,7 @@ edit_interface_rep::mouse_drag (SI x, SI y) {
 void
 edit_interface_rep::mouse_select (SI x, SI y, int mods, bool drag) {
   if (eb->action ("select" , x, y, 0) != "") return;
-  if (!is_nil (mouse_ids) && (mods & ShiftMask) == 0 && !drag) {
+  if (!is_nil (mouse_ids) && (mods & (ShiftMask+Mod2Mask)) == 0 && !drag) {
     call ("link-follow-ids", object (mouse_ids), object ("click"));
     disable_double_clicks ();
     return;
