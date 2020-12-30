788afe2ba2fccea490e21f5e2cf8dbe6c64fb563
Darcy Shen <sadhen@zoho.com>
2020-12-30 11:34:45 +0000

    macOS: set Songti as default Chinese font

--- src/Graphics/Fonts/font.cpp
+++ src/Graphics/Fonts/font.cpp
@@ -570,11 +570,18 @@ script (int sz, int level) {
 string
 default_chinese_font_name () {
   if (tt_font_exists ("FandolSong-Regular")) return "FandolSong";
+
+  // Set default Chinese font for Windows
+  // see: https://docs.microsoft.com/en-us/typography/fonts/windows_10_font_list
   if (tt_font_exists ("simsun")) return "simsun";
+
+  // Set default Chinese font for macOS
+  // see: https://developer.apple.com/fonts/system-fonts/
+  if (tt_font_exists ("Songti SC Regular")) return "Songti SC";
+
   if (tt_font_exists ("fireflysung")) return "fireflysung";
   if (tt_font_exists ("uming")) return "uming";
   if (tt_font_exists ("儷黑 Pro")) return "lihei";
-  if (tt_font_exists ("华文细黑")) return "heiti";
   if (tt_font_exists ("SimSun")) return "apple-simsun";
   return "roman";
 }
