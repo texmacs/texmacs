3c8060f521c3b164f0f07bc0350c0ca1f8e81d75
sadhen <sadhen@64cb5145-927a-446d-8aed-2fb7b4773692>
2021-08-01 13:02:50 +0000

    Convert link target from cork to utf8
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@13575 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/TeXmacs/progs/link/link-navigate.scm
+++ src/TeXmacs/progs/link/link-navigate.scm
@@ -559,7 +559,7 @@
   (cond ((func? v 'id 1)
          (go-to-id (cadr v) (cursor-path)))
         ((func? v 'url 1)
-         (go-to-url (escape-link-args (cadr v)) (cursor-path)))
+         (go-to-url (escape-link-args (cork->utf8 (cadr v))) (cursor-path)))
         ((func? v 'script)
          (with ok? (== (assoc-ref attrs "secure") "true")
            (apply execute-script (cons* (cadr v) ok? (cddr v)))))
