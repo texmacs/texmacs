20b585be015ad584a7219078d62b1d273e7f89a2
沈达 <sadhen@zoho.com>
2021-08-29 18:08:49 +0800

    CMake: support Guile 3

--- cmake/FindGuile.cmake
+++ cmake/FindGuile.cmake
@@ -1,16 +1,23 @@
 # - Locate the GNU Guile library
 # Once done, this will define
 #
-#  Guile_FOUND - system has Freetype
-#  Guile_INCLUDE_DIRS - the Freetype include directories
-#  Guile_LIBRARIES - link these to use Freetype
-#  Guile_VERSION_STRING - version of Guile
+#  Guile_FOUND - system has Guile
+#  Guile_INCLUDE_DIRS - the Guile include directories
+#  Guile_LIBRARIES - link these to use Guile
+#  Guile_VERSION - version of Guile
 
 include (LibFindMacros)
 
-libfind_pkg_check_modules (Guile guile-1.8)
 
-set (Guile_VERSION_STRING 1.8.8)
+if (Guile_VERSION_STRING STREQUAL "1.8")
+  message (STATUS "Using GNU Guile 1.8 via -DGuile_VERSION_STRING=1.8")
+elseif (Guile_VERSION_STRING STREQUAL "3.0")
+  message (STATUS "Using GNU Guile 3.0 via -DGuile_VERSION_STRING=3.0")
+else (Guile_VERSION_STRING STREQUAL "1.8")
+  set (Guile_VERSION_STRING 3.0)
+endif (Guile_VERSION_STRING STREQUAL "1.8")
+
+libfind_pkg_check_modules (Guile guile-${Guile_VERSION_STRING})
 
 string (REPLACE "-l" " " _guile_lib_list "${Guile_LIBRARIES}" )
 string (REPLACE "-L" " " _guile_libdirs ${Guile_LIBRARY_DIRS} "")
