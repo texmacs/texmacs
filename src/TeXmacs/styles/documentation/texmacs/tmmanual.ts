10011d3a1351105987056bb1080ab2462cde6d85
vdhoeven <vdhoeven@64cb5145-927a-446d-8aed-2fb7b4773692>
2021-11-10 20:16:41 +0000

    Firmer control over typewriter typeface in documentation
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@13630 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/TeXmacs/styles/documentation/texmacs/tmmanual.ts
+++ src/TeXmacs/styles/documentation/texmacs/tmmanual.ts
@@ -1,4 +1,4 @@
-<TeXmacs|1.99.13>
+<TeXmacs|2.1>
 
 <style|<tuple|source|english>>
 
@@ -111,6 +111,50 @@
   <assign|paragraph-title|<macro|name|<style-with|src-compact|none|<sectional-short|<vspace*|0.5fn><paragraph-font|<arg|name>>>>>>
 
   <assign|subparagraph-title|<macro|name|<style-with|src-compact|none|<sectional-short|<vspace*|0.5fn><subparagraph-font|<arg|name>>>>>>
+
+  <active*|<\src-comment>
+    Typewriter font.
+  </src-comment>>
+
+  <assign|tt-font-base|<macro|x|<with|font|roman|font-family|tt|magnification|<times|<value|magnification>|1.06>|<arg|x>>>>
+
+  <assign|tt-font|<macro|x|<tt-font-base|<arg|x>>>>
+
+  <assign|tt-prog-font|<macro|x|<tt-font-base|<arg|x>>>>
+
+  <assign|tt-session-font|<macro|x|<with|font|typewriter=roman,pagella|<arg|x>>>>
+
+  \;
+
+  <assign|verbatim|<macro|body|<with|language|verbatim|<tt-font|<arg|body>>>>>
+
+  <assign|shell|<macro|body|<with|mode|prog|prog-language|shell|<tt-prog-font|<arg|body>>>>>
+
+  <assign|scm|<macro|body|<with|mode|prog|prog-language|scheme|<tt-prog-font|<arg|body>>>>>
+
+  <assign|cpp|<macro|body|<with|mode|prog|prog-language|cpp|<tt-prog-font|<arg|body>>>>>
+
+  <assign|python|<macro|body|<with|mode|prog|prog-language|python|<tt-prog-font|<arg|body>>>>>
+
+  <assign|scilab|<macro|body|<with|mode|prog|prog-language|scilab|<tt-prog-font|<arg|body>>>>>
+
+  <assign|mmx|<macro|body|<with|mode|prog|prog-language|mathemagix|<tt-prog-font|<arg|body>>>>>
+
+  <active*|<\src-comment>
+    Hacks for the typewriter font in plug-in sessions.
+  </src-comment>>
+
+  <assign|scheme-input|<\macro|prompt|body>
+    <\with|generic-prompt-color|<value|scheme-prompt-color>|generic-input-color|<value|scheme-input-color>>
+      <tt-font|<generic-input|<arg|prompt>|<arg|body>>>
+    </with>
+  </macro>>
+
+  <assign|input|<macro|prompt|body|<with|mode|prog|font-family|rm|<tt-session-font|<style-with|src-compact|none|<compound|<style-with|src-compact|none|<if|<provides|<merge|<value|prog-language>|-input>>|<merge|<value|prog-language>|-input>|generic-input>>|<arg|prompt>|<arg|body>>>>>>>
+
+  <assign|output|<macro|body|<with|mode|prog|font-family|rm|<tt-session-font|<style-with|src-compact|none|<compound|<style-with|src-compact|none|<if|<provides|<merge|<value|prog-language>|-output>>|<merge|<value|prog-language>|-output>|generic-output>>|<arg|body>>>>>>>
+
+  <assign|script-input|<macro|language|session|in|out|<tt-session-font|<style-with|src-compact|none|<compound|<style-with|src-compact|none|<if|<provides|<merge|<arg|language>|-script-input>>|<merge|<arg|language>|-script-input>|generic-script-input>>|<arg|language>|<arg|session>|<arg|in>|<arg|out>>>>>>
 </body>
 
 <\initial>
