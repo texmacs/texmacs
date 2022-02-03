eb8b5f886c21e67e60cc53d865f4b9db4d927ef6
沈达 <sadhen@zoho.com>
2022-02-03 20:41:35 +0800

    java

--- plugins/code/progs/init-code.scm
+++ plugins/code/progs/init-code.scm
@@ -59,3 +59,34 @@
 
 (converter julia-snippet texmacs-tree
   (:function julia-snippet->texmacs))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Java source files
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-format java
+  (:name "Java Source Code")
+  (:suffix "java"))
+
+(define (texmacs->java x . opts)
+  (texmacs->verbatim x (acons "texmacs->verbatim:encoding" "SourceCode" '())))
+
+(define (java->texmacs x . opts)
+  (verbatim->texmacs x (acons "verbatim->texmacs:encoding" "SourceCode" '())))
+
+(define (java-snippet->texmacs x . opts)
+  (verbatim-snippet->texmacs x 
+    (acons "verbatim->texmacs:encoding" "SourceCode" '())))
+
+(converter texmacs-tree java-document
+  (:function texmacs->java))
+
+(converter java-document texmacs-tree
+  (:function java->texmacs))
+  
+(converter texmacs-tree java-snippet
+  (:function texmacs->java))
+
+(converter java-snippet texmacs-tree
+  (:function java-snippet->texmacs))
