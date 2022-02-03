b6c8446b4dc4321d07d419db8976e5912c5e17ae
沈达 <sadhen@zoho.com>
2022-02-03 20:43:50 +0800

    scala

--- plugins/code/progs/init-code.scm
+++ plugins/code/progs/init-code.scm
@@ -90,3 +90,35 @@
 
 (converter java-snippet texmacs-tree
   (:function java-snippet->texmacs))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Scala source files
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-format scala
+  (:name "Scala Source Code")
+  (:suffix "scala"))
+
+(define (texmacs->scala x . opts)
+  (texmacs->verbatim x (acons "texmacs->verbatim:encoding" "SourceCode" '())))
+
+(define (scala->texmacs x . opts)
+  (verbatim->texmacs x (acons "verbatim->texmacs:encoding" "SourceCode" '())))
+
+(define (scala-snippet->texmacs x . opts)
+  (verbatim-snippet->texmacs x 
+    (acons "verbatim->texmacs:encoding" "SourceCode" '())))
+
+(converter texmacs-tree scala-document
+  (:function texmacs->scala))
+
+(converter scala-document texmacs-tree
+  (:function scala->texmacs))
+  
+(converter texmacs-tree scala-snippet
+  (:function texmacs->scala))
+
+(converter scala-snippet texmacs-tree
+  (:function scala-snippet->texmacs))
+
