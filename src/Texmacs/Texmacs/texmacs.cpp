2870934a381f41a19347db56f6ae560a215b4433
Philippe Joyez <coding.e.pjoyez@neverbox.com>
2022-02-04 20:46:23 +0000

    fix for #61659

--- src/Texmacs/Texmacs/texmacs.cpp
+++ src/Texmacs/Texmacs/texmacs.cpp
@@ -476,7 +476,7 @@ TeXmacs_main (int argc, char** argv) {
              (s == "-x") || (s == "-execute") ||
              (s == "-log-file") ||
              (s == "-build-manual") ||
-             (s == "-reference-suite") || (s == "-test-suite")) {}
+             (s == "-reference-suite") || (s == "-test-suite")) {i++;}
   }
   if (install_status == 1) {
     if (DEBUG_STD) debug_boot << "Loading welcome message...\n";
