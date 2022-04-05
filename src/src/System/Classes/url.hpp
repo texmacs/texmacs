eb8de9393a1d32d42bc354f4902bdea6dfa8e53e
sadhen <sadhen@64cb5145-927a-446d-8aed-2fb7b4773692>
2022-04-05 12:30:06 +0000

    Some tests for url predicates and operations
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@13859 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/src/System/Classes/url.hpp
+++ src/src/System/Classes/url.hpp
@@ -115,8 +115,6 @@ bool is_rooted_tmfs (url u, string sub_protocol);
 bool is_rooted_blank (url u);
 bool is_name (url u);
 bool is_rooted_name (url u);
-bool is_path (url u);
-bool is_rooted_path (url u);
 bool is_ramdisc (url u);
 
 /******************************************************************************
