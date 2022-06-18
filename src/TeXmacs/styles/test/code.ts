21e4250837822638b6aa889fe3b43a3eb1abfc6a
sadhen <sadhen@64cb5145-927a-446d-8aed-2fb7b4773692>
2022-06-18 04:14:45 +0000

    A new style for displaying code
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@13880 64cb5145-927a-446d-8aed-2fb7b4773692

---
+++ src/TeXmacs/styles/test/code.ts
@@ -0,0 +1,40 @@
+<TeXmacs|2.1.1>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|code|1.0>
+
+      <\src-purpose>
+        The style for code.
+      </src-purpose>
+
+      <\src-copyright|2022>
+        Darcy Shen
+      </src-copyright>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
+    </src-title>
+  </active*>
+
+  <use-package|generic>
+
+  <assign|par-first|0fn>
+
+  <assign|par-par-sep|0fn>
+
+  <assign|page-medium|automatic>
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
