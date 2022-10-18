2396038d9d9c38f99062bdd1dd1adf6c0ac99ef7
vdhoeven <vdhoeven@64cb5145-927a-446d-8aed-2fb7b4773692>
2022-10-18 10:38:09 +0000

    Support for CJK space
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@13955 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/src/System/Language/impl_language.hpp
+++ src/src/System/Language/impl_language.hpp
@@ -26,6 +26,7 @@ extern text_property_rep tp_normal_rep;
 extern text_property_rep tp_hyph_rep;
 extern text_property_rep tp_thin_space_rep;
 extern text_property_rep tp_space_rep;
+extern text_property_rep tp_space_before_rep;
 extern text_property_rep tp_dspace_rep;
 extern text_property_rep tp_nb_thin_space_rep;
 extern text_property_rep tp_nb_space_rep;
