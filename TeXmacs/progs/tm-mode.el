57e3b4f3e52c2bff2b3e383377907640064a3cc2
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2022-10-20 23:12:17 +0000

    Extra keywords

--- TeXmacs/progs/tm-mode.el
+++ TeXmacs/progs/tm-mode.el
@@ -74,7 +74,8 @@
     tm-define tm-define-macro lazy-body-macro tm-property request-handler
     tm-menu define-menu tm-widget define-widget tm-generate tm-tool tm-tool*
     tm-build tm-build-macro tm-build-widget
-    menu-bind define-table extend-table smart-table
+    menu-bind smart-table
+    define-table extend-table define-collection extend-collection
     tm-service tm-call-back
     define-format define-language define-graphics))
 
@@ -89,7 +90,7 @@
 (setq binary-keywords
   '(with with-define with-global and-with with-innermost receive
     with-environment with-environment* converter with-cache
-    speech-map speech-adjust speech-reduce
+    speech-map speech-adjust speech-reduce speech-collection
     user-confirm user-url
     resize cached
     $with))
