e0b81bc5d81a70d38cd005cf28efe655891a10c1
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2022-11-03 20:07:38 +0000

    Support for Scilab

--- plugins/scilab/progs/init-scilab.scm
+++ plugins/scilab/progs/init-scilab.scm
@@ -11,36 +11,6 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Scilab source files
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define-format scilab
-  (:name "Scilab source code")
-  (:suffix "sce" "sci"))
-
-(define (texmacs->scilab x . opts)
-  (texmacs->verbatim x (acons "texmacs->verbatim:encoding" "SourceCode" '())))
-
-(define (scilab->texmacs x . opts)
-  (code->texmacs x))
-
-(define (scilab-snippet->texmacs x . opts)
-  (code-snippet->texmacs x))
-
-(converter texmacs-tree scilab-document
-  (:function texmacs->scilab))
-
-(converter scilab-document texmacs-tree
-  (:function scilab->texmacs))
-  
-(converter texmacs-tree scilab-snippet
-  (:function texmacs->scilab))
-
-(converter scilab-snippet texmacs-tree
-  (:function scilab-snippet->texmacs))
-
-
 (define (scilab-commander s)
   (let* ((t (string->object s))
          (s (cadr t))
