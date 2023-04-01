eab72ca948c2d6fe9f2d4972b8d40f6ba497e7de
Philippe Joyez <coding.e.pjoyez@neverbox.com>
2023-04-01 12:06:00 +0000

    Merge branch 'qt5nopdf'

--- src/Plugins/Qt/qt_utilities.cpp
+++ src/Plugins/Qt/qt_utilities.cpp
@@ -394,6 +394,10 @@ qt_supports (url u) {
 	  debug_convert <<LF;
   }	*/  
   string suf=suffix (u);
+// as of 2023, even if qt claims it can handle pdf, do not use it as it produces blurry pngs
+// see http://forum.texmacs.cn/t/how-are-graphics-supposed-to-look-like/963/12
+  if (suf == "pdf")
+    return false; 
   bool ans= (bool) formats.contains((QByteArray) as_charp(suf));
   //if (DEBUG_CONVERT) {debug_convert <<"QT valid format:"<<((ans)?"yes":"no")<<LF;}
   return ans;
