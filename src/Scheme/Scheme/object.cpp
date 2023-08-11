c5461f66465bb052acedbaead88722d2e6374590
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2023-08-11 20:24:21 +0000

    Fix overloading clash

--- src/Scheme/Scheme/object.cpp
+++ src/Scheme/Scheme/object.cpp
@@ -94,7 +94,7 @@ object list_object (object obj1, object obj2) {
   return cons (obj1, cons (obj2, null_object ())); }
 object list_object (object obj1, object obj2, object obj3) {
   return cons (obj1, cons (obj2, cons (obj3, null_object ()))); }
-object list_object (array<object> objs) {
+object as_list_object (array<object> objs) {
   object r= null_object ();
   for (int i=N(objs)-1; i>=0; i--) r= cons (objs[i], r);
   return r; }
