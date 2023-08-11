e8444fbdf2ebf05cb0c18c19fb84b016b69c39ee
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2023-08-11 20:26:04 +0000

    Style package for customized keyboards

---
+++ TeXmacs/styles/test/new-gui.ts
@@ -0,0 +1,38 @@
+<TeXmacs|2.1.2>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|new-gui|1.0>
+
+      <\src-purpose>
+        Style for <TeXmacs>-driven gui elements.
+      </src-purpose>
+
+      <\src-copyright|2023>
+        Joris van der Hoeven
+      </src-copyright>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
+    </src-title>
+  </active*>
+
+  <use-package|course|gui-keyboard>
+
+  <assign|font|Lucida Grande>
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
