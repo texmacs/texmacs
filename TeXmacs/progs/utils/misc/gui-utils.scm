60871fe8d1bfa91d379c860ab1920e69cecbe365
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2023-10-30 23:32:35 +0000

    Progress on GUI through markup

--- TeXmacs/progs/utils/misc/gui-utils.scm
+++ TeXmacs/progs/utils/misc/gui-utils.scm
@@ -69,18 +69,112 @@
 ;; Table markup
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define (as-symbol x fall-back)
+  (if (tm-atomic? x) (string->symbol (tm->string x)) fall-back))
+(define (as-string x fall-back)
+  (if (tm-atomic? x) (tm->string x) fall-back))
+
 (define (gui-hlist-item t)
   `(cell (document ,t)))
 
-(tm-define (gui-hlist-table tag t)
+(tm-define (gui-hlist-table tag* t)
   (:secure #t)
-  (with label (if (tm-atomic? tag) (string->symbol (tm->string tag)) 'stack)
-    `(,label (table (row ,@(map gui-hlist-item (tree-children t)))))))
+  (with tag (as-symbol tag* 'stack)
+    `(,tag (table (row ,@(map gui-hlist-item (tree-children t)))))))
 
 (define (gui-vlist-item t)
   `(row (cell (document ,t))))
 
-(tm-define (gui-vlist-table tag t)
+(tm-define (gui-vlist-table tag* t)
   (:secure #t)
-  (with label (if (tm-atomic? tag) (string->symbol (tm->string tag)) 'stack)
-    `(,label (table ,@(map gui-vlist-item (tree-children t))))))
+  (with tag (as-symbol tag* 'stack)
+    `(,tag (table ,@(map gui-vlist-item (tree-children t))))))
+
+(define (gui-tile-item t)
+  `(cell (document ,t)))
+
+(define (gui-tiled-row l cols)
+  (let* ((r (map (lambda (x) "") (.. (length l) cols)))
+         (a (append l r)))
+    `(row ,@(map gui-tile-item a))))
+  
+(define (gui-tiled-rows l cols)
+  (if (> (length l) cols)
+      (cons (gui-tiled-row (sublist l 0 cols) cols)
+            (gui-tiled-rows (sublist l cols (length l)) cols))
+      (list (gui-tiled-row l cols))))
+
+(tm-define (gui-tiled tag* t)
+  (:secure #t)
+  (let* ((tag (as-symbol tag* 'stack))
+         (c (tree-children t))
+         (cols (or (and (nnull? c) (string->number (tree->string (car c)))) 8))
+         (args (if (null? c) c (cdr c))))
+    `(,tag (table ,@(gui-tiled-rows args cols)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Choice lists
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (gui-choice-item x f on? off-tag on-tag)
+  (let* ((p (and (tree? x) (tree->path x)))
+         (s (string-append "(" f " '" (object->string p) ")"))
+         (cmd (if p s "(noop)")))
+    `(,(if (on? x) on-tag off-tag) ,x ,cmd)))
+
+(define (check-toggle old val all)
+  (if (func? old 'tuple) (set! old (cdr old)))
+  (if (nlist? old) (set! old (list)))
+  (let* ((rem (list-remove old val))
+         (tog (if (in? val old) rem (cons val old)))
+         (new (list-filter all (cut in? <> tog))))
+    (cons 'tuple (list-remove-duplicates new))))
+
+(tm-define (gui-on-choice tag p)
+  (:secure #t)
+  (and-with t (path->tree p)
+    (and-with u (tm-ref t :up)
+      (when (tree-is? u tag)
+        (let* ((val (tree->stree t))
+               (old (tree->stree (tree-ref u 1)))
+               (all (cddr (tree->stree u)))
+               (new (if (== tag 'choice-list) val
+                        (check-toggle old val all))))
+          (tree-set (tree-ref u 1) new))
+        (let* ((c   (as-string (tree-ref u 0) "(noop)"))
+               (val (tree->stree (tree-ref u 1)))
+               (sel (if (func? val 'tuple) (cdr val) val))
+               (cmd (string-append "(with answer '" (object->string sel)
+                                   " " c ")")))
+          (delayed
+            (:idle 1)
+            (secure-eval (string->object cmd))
+            (keyboard-focus-on "canvas")
+            (update-menus)))))))
+
+(tm-define (gui-choice-list list-tag* off-tag* on-tag* t)
+  (:secure #t)
+  (let* ((list-tag (as-symbol list-tag* 'vlist))
+         (off-tag (as-symbol off-tag* 'menu-button))
+         (on-tag (as-symbol on-tag* 'menu-button-pressed))
+         (c (tree-children t))
+         (n (length c))
+         (cur (if (>= n 2) (tm->stree (cadr c)) '(uninit)))
+         (args (if (>= n 2) (cddr c) (list)))
+         (on? (lambda (x) (== (tm->stree x) cur)))
+         (f "gui-on-choice 'choice-list"))
+    `(,list-tag ,@(map (cut gui-choice-item <> f on? off-tag on-tag) args))))
+
+(tm-define (gui-check-list list-tag* off-tag* on-tag* t)
+  (:secure #t)
+  (let* ((list-tag (as-symbol list-tag* 'vlist))
+         (off-tag (as-symbol off-tag* 'menu-button))
+         (on-tag (as-symbol on-tag* 'menu-button-pressed))
+         (c (tree-children t))
+         (n (length c))
+         (cur (if (>= n 2) (tm->stree (cadr c)) '(uninit)))
+         (sel (if (func? cur 'tuple) (cdr cur) (list)))
+         (args (if (>= n 2) (cddr c) (list)))
+         (on? (lambda (x) (in? (tm->stree x) sel)))
+         (f "gui-on-choice 'check-list"))
+    `(,list-tag ,@(map (cut gui-choice-item <> f on? off-tag on-tag) args))))
