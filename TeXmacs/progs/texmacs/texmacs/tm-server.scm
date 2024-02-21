e91f2ce7f3718454d05e9b043fe3c66c3030c52d
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2024-02-21 13:28:19 +0000

    Fix segfault on quit when there is an unsaved aux buffer

--- TeXmacs/progs/texmacs/texmacs/tm-server.scm
+++ TeXmacs/progs/texmacs/texmacs/tm-server.scm
@@ -212,11 +212,12 @@
         (else (do-kill-window))))
 
 (tm-define (safely-quit-TeXmacs)
-  (with l (filter buffer-modified? (buffer-list))
+  (let* ((m (filter buffer-modified? (buffer-list)))
+	 (l (filter (non buffer-aux?) m)))
     (if (null? l)
         (quit-TeXmacs)
         (begin
-          (when (not (buffer-modified? (current-buffer)))
+          (when (nin? (current-buffer) l)
             ;; FIXME: focus on window with buffer, if any
             (switch-to-buffer (car l)))
           (user-confirm "There are unsaved documents. Really quit?" #f  
