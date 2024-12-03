cdc94f2063bbefe50ab1818a7c4a05b967331d07
lizabelos <lizabelos@64cb5145-927a-446d-8aed-2fb7b4773692>
2024-12-03 11:14:39 +0000

    separating QMacPasteboardMimePDF in an another file
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@14524 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/src/Plugins/Qt/QTMApplication.hpp
+++ src/src/Plugins/Qt/QTMApplication.hpp
@@ -28,60 +28,7 @@ void init_style_sheet (QApplication* app);
 void set_standard_style_sheet (QWidget *w);
 
 #if defined(Q_OS_MAC) && QT_VERSION < 0x060000 
-
-#include <QMacPasteboardMime>
-
-// On MacOS we have to register appropriate mime types for PDF files
-// The QMacPasteboardMimePDF class is instantiated in QTMApplication
-// and provides the necessary support.
-//
-// code from:
-// https://www.lyx.org/trac/browser/lyxsvn/lyx-devel/trunk/src/frontends/qt4/GuiApplication.cpp?rev=24894
-
-// (mg) I'm not sure this is the right place to have this code, but well...
-
-class QMacPasteboardMimePDF : public QMacPasteboardMime
-{
-public:
-  QMacPasteboardMimePDF ()
-    : QMacPasteboardMime (MIME_QT_CONVERTOR | MIME_ALL)
-  {}
-
-  QString convertorName() { return "PDF"; }
-
-  QString flavorFor (QString const & mime)
-  {
-    if (mime == QLatin1String ("application/pdf"))
-      return QLatin1String ("com.adobe.pdf");
-    return QString();
-  }
-
-  QString mimeFor(QString flav)
-  {
-    if (flav == QLatin1String ("com.adobe.pdf"))
-      return QLatin1String ("application/pdf");
-    return QString ();
-  }
-
-  bool canConvert(QString const & mime, QString flav)
-  { return mimeFor (flav) == mime; }
-
-  QVariant convertToMime (QString const & mime, QList<QByteArray> data, QString flav)
-  {
-    (void) flav; (void) mime;
-    if (data.count () > 1)
-      debug_qt << "QMacPasteboardMimePDF: Cannot handle multiple member data " << LF;
-    return data.first ();
-  }
-
-  QList<QByteArray> convertFromMime (QString const & mime, QVariant data, QString flav)
-  {
-    (void) flav; (void) mime;
-    QList<QByteArray> ret;
-    ret.append (data.toByteArray ());
-    return ret;
-  }
-};
+#include "QTMMacPasteboardMimePDF.hpp"
 #endif
 
 
