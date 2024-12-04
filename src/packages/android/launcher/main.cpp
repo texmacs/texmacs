0ef319d8620ed68e21cf0ab90ae37710b4eda731
lizabelos <lizabelos@64cb5145-927a-446d-8aed-2fb7b4773692>
2024-12-04 12:36:02 +0000

    fix commit mistake : add missing main.cpp in the android launcher
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@14531 64cb5145-927a-446d-8aed-2fb7b4773692

---
+++ src/packages/android/launcher/main.cpp
@@ -0,0 +1,80 @@
+#include <pthread.h>
+#include <QDebug>
+
+#define STACK_SIZE 0x1000000
+
+void texmacs_init_guile_hooks();
+int texmacs_entrypoint(int argc, char** argv);
+
+typedef struct android_args {
+  int argc;
+  char **argv;
+} android_args;
+
+void *main_thread (void* args) {
+  android_args *a = (android_args *)args;
+  int argc = a->argc;
+  char **argv = a->argv;
+  qDebug() << "Initializing Guile hooks...";
+  texmacs_init_guile_hooks();
+  qDebug() << "Starting TeXmacs...";
+  texmacs_entrypoint(argc, argv);
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    android_args args;
+    args.argc = argc;
+    args.argv = argv;
+
+    qDebug() << "Initializing pthread attributes...";
+    pthread_attr_t attr;
+    int  rc;
+    void* tmstack;
+    size_t tmstacksize = STACK_SIZE;    
+    if (pthread_attr_init (&attr) == -1) {
+        qWarning() << "error in pthread_attr_init";
+        return 1;
+    }
+
+    /* Get a big enough stack and align it on 4K boundary. */
+    qDebug() << "Allocating stack storage...";
+    tmstack = malloc (tmstacksize + PTHREAD_STACK_MIN);
+    if (tmstack != NULL) {
+        qDebug() << "Using PTHREAD_STACK_MIN to align stack address";
+        tmstack = (void*)((((unsigned long long) tmstack + (PTHREAD_STACK_MIN - 1)) / PTHREAD_STACK_MIN) * PTHREAD_STACK_MIN);
+    } else {
+        qWarning() << "Error: unable to acquire stack storage";
+        return 1;
+    }
+
+    qDebug() << "Setting stack address and size...";
+    rc = pthread_attr_setstack (&attr, tmstack, tmstacksize);
+    if (rc != 0) {
+        qWarning() << "Error: pthread_attr_setstack returned " << QString::number(rc);
+        return 1;
+    }
+
+    qDebug() << "Launching texmacs thread...";
+    pthread_t tmthread;
+    void *ret;
+    if (pthread_create (&tmthread, &attr, main_thread, &args) != 0) {
+        qWarning() << "Error: pthread_create failed";
+        return 1;
+    }
+    if (pthread_join (tmthread, &ret) != 0) {
+        qWarning() << "Error: pthread_join failed";
+        return 1;
+    }
+
+    rc = pthread_attr_destroy (&attr);
+    if (rc != 0) {
+        qWarning() << "Error: pthread_attr_destroy returned " << QString::number(rc);
+        return 1;
+    }
+
+    qDebug() << "TeXmacs thread finished";
+    return 0;
+}
+
