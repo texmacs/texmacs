57ad69dd860d5f3bb1bfba31bb245b38247cd8d3
lizabelos <lizabelos@64cb5145-927a-446d-8aed-2fb7b4773692>
2024-12-16 16:30:26 +0000

    add md5 to assets on android
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@14540 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/packages/android/collect_assets.sh
+++ src/packages/android/collect_assets.sh
@@ -4,7 +4,8 @@ i=$(find "$2" -name "*.raw" -type f | wc -l | tr -d '[:space:]')
 i=$(((i - 1) + 1))
 # rm -f "$2/resource_files.txt"
 find $1 -type f | while read filename; do 
-    echo "r$i $filename" >> "$2/resource_files.txt"
+    md5=$(md5sum "$filename" | cut -d ' ' -f 1)
+    echo "r$i $filename $md5sum" >> "$2/resource_files.txt"
     cp "$filename" "$2/r$i.raw"
     i=$((i + 1))
 done
