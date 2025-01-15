ddcc2ee074fef6a160bddf8138d6ef2f238bcce6
lizabelos <lizabelos@64cb5145-927a-446d-8aed-2fb7b4773692>
2025-01-15 09:45:04 +0000

    fix [bug #66656] Clang 19.1.5 compile error in hashtree.cpp
    git-svn-id: svn://svn.savannah.gnu.org/texmacs/trunk@14566 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/src/Kernel/Containers/hashtree.cpp
+++ src/src/Kernel/Containers/hashtree.cpp
@@ -94,7 +94,7 @@ hashtree<K,V>::operator-> (void) {
 
 template<class K, class V> inline hashtree<K,V> 
 hashtree<K,V>::operator[] (K key) {
-  if (*this->contains (key)) return *this->children (key);
+  if ((*this)->contains (key)) return (*this)->children (key);
   else FAILED ("read-access to non-existent node requested");
 }
   
