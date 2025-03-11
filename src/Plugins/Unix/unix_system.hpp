db00eaa6b74c3bad55b590fc23826e1c50e0b774
Liza Belos <belos.liza@gmail.com>
2025-03-11 12:28:19 +0000

    add texmacs_process_event inside hyphenate to make texmacs responsive while it generate the documentation

--- src/Plugins/Unix/unix_system.hpp
+++ src/Plugins/Unix/unix_system.hpp
@@ -149,5 +149,11 @@ url texmacs_get_application_directory();
   * @see texmacs_system_start_long_task
   */
  void texmacs_system_end_long_task();
+ 
+/*
+ * @brief Process the essential user events. This function should be called
+ * in long tasks to keep TeXmacs responsive.
+ */
+void texmacs_process_event();
 
 #endif // TEXMACS_UNIX_SYSTEM_HPP
