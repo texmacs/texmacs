a31861a751c7d29f17278366cccfda57257e78f4
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2025-08-01 20:22:41 +0000

    Add tool for table properties
    git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@14886 64cb5145-927a-446d-8aed-2fb7b4773692

--- TeXmacs/progs/init-texmacs.scm
+++ TeXmacs/progs/init-texmacs.scm
@@ -312,7 +312,7 @@
 (lazy-menu (table table-menu) insert-table-menu)
 (lazy-define (table table-edit) table-resize-notify)
 (lazy-define (table table-widgets) open-cell-properties open-table-properties)
-(lazy-tool (table table-tools) cell-properties-tool)
+(lazy-tool (table table-tools) cell-properties-tool table-properties-tool)
 (tm-property (open-cell-properties) (:interactive #t))
 (tm-property (open-table-properties) (:interactive #t))
 ;(display* "time: " (- (texmacs-time) boot-start) "\n")
--- TeXmacs/progs/table/table-tools.scm
+++ TeXmacs/progs/table/table-tools.scm
@@ -63,6 +63,27 @@
     (cell-set-format* var val)
     (update-menus)))
 
+(tm-define (window-table-get-format win var)
+  (with-window win
+    (table-get-format var)))
+
+(tm-define (window-table-set-format* win var val)
+  (with-window win
+    (table-set-format* var val)
+    (update-menus)))
+
+(tm-define (window-table-set-extents win r c) 
+  (with-window win
+    (table-set-extents r c)))
+
+(tm-define (window-table-nr-rows win) 
+  (with-window win
+    (table-nr-rows)))
+
+(tm-define (window-table-nr-columns win) 
+  (with-window win
+    (table-nr-columns)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Cell properties
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -149,7 +170,7 @@
                (get** "cell-bsep") "6em"))
       (glue #t #f 0 0))))
 
-(tm-widget (cell-advanced-tool win)
+(tm-widget (cell-special-tool win)
   (aligned
     (item (text "Line wrapping:")
       (enum (window-cell-set-format* win "cell-hyphen" (encode-hyphen answer))
@@ -186,4 +207,166 @@
   === ===
   (centered
     (when (window-inside-table? win)
-      (dynamic (cell-advanced-tool win)))))
+      (dynamic (cell-special-tool win)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Table properties
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(tm-widget (table-extents-tool win)
+  (let* ((set (lambda (v a) (window-table-set-format* win v a)))
+         (get (lambda (v) (list (window-table-get-format win v)
+                                "1" "2" "3" "4" "5" "6" "7" "8" ""))))
+    (horizontal
+      (glue #t #f 0 0)
+      (tile 4
+        (text "")
+        (text "")
+        (horizontal // (text "Minimal") >>>)
+        (horizontal // (text "Maximal") >>>)
+
+        (vertical (glue #f #f 0 6)
+                  (horizontal (glue #t #f 0 0) (text "Rows:") // //)
+                  (glue #f #f 0 6))
+        (horizontal
+	  (enum (when answer
+                  (window-table-set-extents
+                   win (string->number answer) (window-table-nr-columns win)))
+                (list (number->string (window-table-nr-rows win))
+                      "1" "2" "3" "4" "5" "6" "7" "8" "")
+                (number->string (window-table-nr-rows win)) "4em") // // //)
+        (horizontal
+	  (enum (set "table-min-rows" answer)
+                (get "table-min-rows")
+		(table-get-format "table-min-rows") "4em") //)
+        (enum (set "table-max-rows" answer)
+              (get "table-max-rows")
+              (table-get-format "table-max-rows") "4em")
+
+        (vertical (glue #f #f 0 6)
+                  (horizontal (glue #t #f 0 0) (text "Columns:") // //)
+                  (glue #f #f 0 6))
+        (horizontal
+	  (enum (when answer
+                  (window-table-set-extents
+                   win (window-table-nr-rows win) (string->number answer)))
+                (list (number->string (window-table-nr-columns win))
+                      "1" "2" "3" "4" "5" "6" "7" "8" "")
+                (number->string (window-table-nr-columns win)) "4em") // // //)
+        (horizontal
+	  (enum (set "table-min-cols" answer)
+                (get "table-min-cols")
+		(table-get-format "table-min-cols") "4em") //)
+        (enum (set "table-max-cols" answer)
+              (get "table-max-cols")
+              (table-get-format "table-max-cols") "4em"))
+      (glue #t #f 0 0))))
+
+(tm-widget (table-basic-tool win)
+  (aligned
+    (item (text "Width:")
+      (with mode (decode-mode (window-table-get-format win "table-hmode"))
+        (horizontal
+          (enum (window-table-set-format* win "table-hmode"
+                                          (encode-mode answer))
+                '("Auto" "Exact" "Minimal" "Maximal") mode "6em")
+          ///
+          (when (!= mode "Auto")
+            (input (window-table-set-format* win "table-width" answer) "string"
+                   (list (window-table-get-format win "table-width"))
+                   "6em")))))
+    (item (text "Height:")
+      (with mode (decode-mode (window-table-get-format win "table-vmode"))
+        (horizontal
+          (enum (window-table-set-format* win "table-vmode"
+                                          (encode-mode answer))
+                '("Auto" "Exact" "Minimal" "Maximal") mode "6em")
+          ///
+          (when (!= mode "Auto")
+            (input (window-table-set-format* win "table-height" answer) "string"
+                   (list (window-table-get-format win "table-height"))
+                   "6em")))))
+    (item (text "Align:")
+      (let* ((ha (decode-halign (window-table-get-format win "table-halign")))
+             (va (decode-valign* (window-table-get-format win "table-valign"))))
+        (horizontal
+          (enum (table-set-format* "table-halign" (encode-halign answer))
+                '("Left" "Center" "Right") ha "6em")
+          ///
+          (enum (table-set-format* "table-valign" (encode-valign* answer))
+                '("Axis" "Top" "Center" "Bottom"
+                  "Top baseline" "Center baseline" "Bottom baseline")
+                va "6em"))))))
+
+(tm-widget (table-border-tool win)
+  (let* ((set (lambda (v a) (window-table-set-format* win v a)))
+         (get (lambda (v) (list (window-table-get-format win v) "0ln" "1ln")))
+         (get* (lambda (v) (list (window-table-get-format win v) "0fn"))))
+    (horizontal
+      (glue #t #f 0 0)
+      (tile 3
+        (text "")
+        (horizontal // (text "Border") >>>)
+        (horizontal // (text "Padding") >>>)
+
+        (vertical (glue #f #f 0 6)
+                  (horizontal (glue #t #f 0 0) (text "Left:") // //)
+                  (glue #f #f 0 6))
+        (horizontal
+          (input (set "table-lborder" answer) "string"
+                 (get "table-lborder") "6em") // //)
+        (input (set "table-lsep" answer) "string"
+               (get* "table-lsep") "6em")
+
+        (vertical (glue #f #f 0 6)
+                  (horizontal (glue #t #f 0 0) (text "Right:") // //)
+                  (glue #f #f 0 6))
+        (horizontal
+          (input (set "table-rborder" answer) "string"
+                 (get "table-rborder") "6em") // //)
+        (input (set "table-rsep" answer) "string"
+               (get* "table-rsep") "6em")
+
+        (vertical (glue #f #f 0 6)
+                  (horizontal (glue #t #f 0 0) (text "Top:") // //)
+                  (glue #f #f 0 6))
+        (horizontal
+          (input (set "table-tborder" answer) "string"
+                 (get "table-tborder") "6em") // //)
+        (input (set "table-tsep" answer) "string"
+               (get* "table-tsep") "6em")
+
+        (vertical (glue #f #f 0 6)
+                  (horizontal (glue #t #f 0 0) (text "Bottom:") // //)
+                  (glue #f #f 0 6))
+        (horizontal
+          (input (set "table-bborder" answer) "string"
+                 (get "table-bborder") "6em") // //)
+        (input (set "table-bsep" answer) "string"
+               (get* "table-bsep") "6em"))
+      (glue #t #f 0 0))))
+
+(tm-widget (table-special-tool win)
+  (aligned
+    (meti (horizontal // (text "Enable page breaking"))
+      (toggle
+       (window-table-set-format* win "table-hyphen" (if answer "y" "n"))
+       (== (window-table-get-format win "table-hyphen") "y")))))
+
+(tm-tool* (table-properties-tool win)
+  (:name "Table properties")
+  (centered
+    (when (window-inside-table? win)
+      (dynamic (table-extents-tool win))))
+  === ===
+  (centered
+    (when (window-inside-table? win)
+      (dynamic (table-basic-tool win))))
+  === ===
+  (centered
+    (when (window-inside-table? win)
+      (dynamic (table-border-tool win))))
+  === ===
+  (centered
+    (when (window-inside-table? win)
+      (dynamic (table-special-tool win)))))
