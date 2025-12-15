36396062d33983451b27fca9b60fd2f29030ab82
Liza Belos <belos.liza@gmail.com>
2025-12-15 13:35:52 +0000

    aspell as a lib
    git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@15186 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/Plugins/Ispell/ispell.cpp
+++ src/Plugins/Ispell/ispell.cpp
@@ -2,7 +2,7 @@
 /******************************************************************************
 * MODULE     : ispell.cpp
 * DESCRIPTION: interface with the ispell spell checker
-* COPYRIGHT  : (C) 1999  Joris van der Hoeven
+* COPYRIGHT  : (C) 2025  Liza Belos
 *******************************************************************************
 * This software falls under the GNU general public license version 3 or later.
 * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
@@ -12,186 +12,142 @@
 #include "Ispell/ispell.hpp"
 #include "file.hpp"
 #include "resource.hpp"
-#include "tm_link.hpp"
 #include "convert.hpp"
 #include "locale.hpp"
+
+#if USE_ASPELL
+#include <aspell.h>
 
 string ispell_encode (string lan, string s);
 string ispell_decode (string lan, string s);
 
-/******************************************************************************
-* The connection resource
-******************************************************************************/
-
 RESOURCE(ispeller);
+
 struct ispeller_rep: rep<ispeller> {
-  string  lan; // name of the session
-  tm_link ln;  // the pipe
-  bool unavailable; 
+  string         lan;
+  AspellConfig* config;
+  AspellSpeller* speller;
+  bool           unavailable;
+  string         error_msg;
 
 public:
   ispeller_rep (string lan);
+  ~ispeller_rep ();
+
   string start ();
-  string retrieve ();
-  void   send (string cmd);
-private:
-  bool connect_spellchecker (string cmd);
+  
+  tree check (string word);
+  void accept (string word);
+  void insert (string word);
+  void save_all ();
 };
+
 RESOURCE_CODE(ispeller);
 
-/******************************************************************************
-* Routines for ispellers
-******************************************************************************/
+ispeller_rep::ispeller_rep (string lan2): 
+  rep<ispeller> (lan2), 
+  lan (lan2), 
+  config (NULL), 
+  speller (NULL), 
+  unavailable (false) {}
 
-ispeller_rep::ispeller_rep (string lan2): rep<ispeller> (lan2), lan (lan2) {}
+ispeller_rep::~ispeller_rep () {
+  if (speller) delete_aspell_speller (speller);
+  if (config) delete_aspell_config (config);
+}
 
-// connect to spell checker with the desired dictionnary
 string
 ispeller_rep::start () {
-  if (!is_nil (ln)) { 
-    if (ln->alive) return "ok";
-    if (unavailable) return "Error: not available";
-  }
-  string cmd, err;
-  string name = "";
+  if (speller) return "ok";
+  if (unavailable) return error_msg;
+
   string locale = language_to_locale (lan);
-  bool testdic = false;
-  if (exists_in_path ("hunspell")) {
-    cmd= "hunspell";
-    name = cmd;
-    cmd = cmd * " -a -i utf-8";
-    if (locale != "") cmd = cmd * " -d " * locale;
-    testdic = connect_spellchecker(cmd);
+  
+  config = new_aspell_config ();
+  
+  if (locale != "") {
+    char* c_locale = as_charp(locale);
+    aspell_config_replace (config, "lang", c_locale);
+  } else {
+    aspell_config_replace (config, "lang", "en_US"); 
   }
-  if ((name == "") || (!testdic))
-    if (exists_in_path ("aspell")) {
-      cmd= "aspell";
-      name = cmd;
-      cmd = cmd * " -a --encoding=utf-8";
-      if (locale != "") cmd = cmd * " --language-tag=" * locale;
-      testdic = connect_spellchecker(cmd);
-    }
-#ifdef OS_MINGW
-  //look in "program files" for system-wide install (but not found in PATH)
-  url u;
-  bool testcmd;
-  if ((name == "") || (!testdic)){
-    u= url_system ("$PROGRAMFILES\\Hunspell\\bin\\hunspell.exe");
-    testcmd = exists (u);
-    if (!testcmd) {
-      u= url_system ("$PROGRAMFILES(x86)\\Hunspell\\bin\\hunspell.exe");
-      testcmd = exists (u);
-    }
-    if (testcmd) {
-      cmd = as_string(u);
-      name = "Hunspell";
-      cmd = "\"" * cmd * "\" -a -i utf-8";
-      if (locale != "") cmd = cmd * " -d " * locale;
-      testdic = connect_spellchecker(cmd);
-    }
-  }
-  if ((name == "") || (!testdic)) {
-    u = url_system ("$PROGRAMFILES\\Aspell\\bin\\aspell.exe");
-    testcmd = exists (u);
-    if (!testcmd) {
-      u= url_system ("$PROGRAMFILES(x86)\\Aspell\\bin\\aspell.exe");
-      testcmd = exists (u);
-    }
-    if (testcmd)  {
-      cmd = as_string(u);
-      name = "Aspell";
-      cmd = "\"" * cmd * "\" -a --encoding=utf-8";
-      if (locale != "") cmd =  cmd * " --language-tag=" * locale;
-      testdic = connect_spellchecker(cmd);
-    }
-  }
-#ifdef ASPELL
-  if ((name == "") || (!testdic)) {
-    u = url_system ("$TEXMACS_PATH\\" ASPELL "\\bin\\aspell.exe");
-    testcmd = exists (u);
-    if (testcmd)  {
-      cmd = as_string(u);
-      name = "Aspell";
-      cmd = "\"" * cmd * "\" -a --encoding=utf-8";
-      u = url_system ("$TEXMACS_PATH\\" ASPELL "\\lib\\aspell-0.60");
-      if (exists (u)) {
-        cmd = cmd * " --dict-dir=" * as_string(u);
-      }
-      if (locale != "") cmd =  cmd * " --language-tag=" * locale;
-      testdic = connect_spellchecker(cmd);
-    }
-  }
-#endif
-#endif
-  if (name == "") {
-    err = "Error: spellchecker not found in PATH (neither Aspell nor Hunspell) ";
-    std_error << err << "\nCannot spellcheck\n";
+
+  aspell_config_replace (config, "encoding", "utf-8");
+
+  AspellCanHaveError* ret = new_aspell_speller (config);
+
+  if (aspell_error (ret)) {
+    error_msg = "Error: ";
+    error_msg << aspell_error_message (ret);
+    delete_aspell_can_have_error (ret);
+    
+    std_error << error_msg << "\nCannot spellcheck\n";
     unavailable = true;
-    return err;
+    return error_msg;
   }
-  if (!testdic) {
-    err = "Error: no dictionary installed for " * lan * " (" * locale * ")";
-    std_error << err << "\nThe corresponding text is not checked\n";
-    unavailable = true;
-    return err;
-  }
-  debug_spell << "running " << name << " with " << locale << " dictionary for " << lan << "\n";
+
+  speller = to_aspell_speller (ret);
+  
+  if (DEBUG_IO) debug_spell << "Aspell library initialized for " << lan << "\n";
   unavailable = false;
   return "ok";
 }
 
-bool
-ispeller_rep::connect_spellchecker (string cmd) {
-// establishes connection (absence of error means the required dictionnary is available)
-// returns true when the spellchecker is up and running
-  ln= make_pipe_link (cmd);
-  string message= ln->start ();
-  if (DEBUG_IO) debug_spell << "Received " << message << "\n";
-  if (starts (message, "Error: ")) {
-    if (ln->alive) ln->stop ();
-    return false;
-  }
-  message= retrieve ();
-  if (DEBUG_IO) debug_spell << "Received " << message << "\n";
-  if (starts (message, "@(#)")) return true;
-  else {
-    if (ln->alive) ln->stop ();
-    return false;
-    }
-}
+tree
+ispeller_rep::check (string word) {
+  if (unavailable || !speller) return "Error: unavailable";
 
-string
-ispeller_rep::retrieve () {
-  string ret;
-#ifdef OS_MINGW
-  while ((ret != "\r\n") && (!ends (ret, "\r\n\r\n")) &&
-        ((!ends (ret, "\r\n")) || (!starts (ret, "@(#)"))))
-#else
-  while ((ret != "\n") && (!ends (ret, "\n\n")) &&
-        ((!ends (ret, "\n")) || (!starts (ret, "@(#)"))))
-#endif
-    {
-      ln->listen (10000);
-      string mess = ln->read (LINK_ERR);
-      string extra= ln->read (LINK_OUT);
-      if (mess  != "") io_error << "Spellchecker error: " << mess << "\n";
-      if (extra == "") {
-       ln->stop ();
-       return "Error: spellchecker does not respond";
-      }
-      ret << extra;
-    }
-  return ispell_decode (lan, ret);
+  string word_utf8 = ispell_encode (lan, word);
+  int len = N(word_utf8);
+  
+  char* c_word = as_charp(word_utf8); 
+
+  int correct = aspell_speller_check (speller, c_word, len);
+
+  if (correct == -1) {
+    return "Error: aspell check failed";
+  }
+
+  if (correct == 1) {
+    return "ok"; 
+  }
+
+  tree t (TUPLE, word);
+  
+  const AspellWordList* suggestions = aspell_speller_suggest (speller, c_word, len);
+  AspellStringEnumeration* elements = aspell_word_list_elements (suggestions);
+  
+  const char* suggestion;
+  while ((suggestion = aspell_string_enumeration_next (elements)) != NULL) {
+    t << ispell_decode (lan, string(suggestion));
+  }
+  
+  delete_aspell_string_enumeration (elements);
+
+  return t;
 }
 
 void
-ispeller_rep::send (string cmd) {
-  ln->write (ispell_encode (lan, cmd) * "\n", LINK_IN);
+ispeller_rep::accept (string word) {
+  if (unavailable || !speller) return;
+  string word_utf8 = ispell_encode (lan, word);
+  char* c_word = as_charp(word_utf8);
+  aspell_speller_add_to_session (speller, c_word, N(word_utf8));
 }
 
-/******************************************************************************
-* Internationalization
-******************************************************************************/
+void
+ispeller_rep::insert (string word) {
+  if (unavailable || !speller) return;
+  string word_utf8 = ispell_encode (lan, word);
+  char* c_word = as_charp(word_utf8);
+  aspell_speller_add_to_personal (speller, c_word, N(word_utf8));
+}
+
+void
+ispeller_rep::save_all () {
+  if (unavailable || !speller) return;
+  aspell_speller_save_all_word_lists (speller);
+}
 
 string
 ispell_encode (string lan, string s) {
@@ -205,95 +161,51 @@ ispell_decode (string lan, string s) {
   return utf8_to_cork (s);
 }
 
-/******************************************************************************
-* Subroutines
-******************************************************************************/
-
-static tree
-parse_ispell (string s) {
-#ifdef OS_MINGW
-  while (ends (s, "\r\n")) s= s (0, N(s)-2);
-#else
-  while (ends (s, "\n")) s= s (0, N(s)-1);
-#endif
-  bool flag= true;
-  int i, j;
-  tree t (TUPLE);
-  for (i=0, j=0; j<N(s); j++)
-    if (s[j]==':') flag= false;
-    else if (((s[j]==' ') && (flag || (j==i) || (s[j-1]==':'))) || (s[j]==','))
-      {
-       if (j>i) t << s (i, j);
-       i= j+1;
-      }
-  t << s (i, j);
-
-  if (N(t) == 0) return tree (TUPLE, "0");
-  if ((t[0] == "+") || (t[0] == "*") || (t[0] == "-")) return "ok";
-  if ((N(t)>=4) && ((t[0] == "&") || (t[0]=="?"))) {
-    tree u (TUPLE, t[2]);
-    u << A (t (4, N (t)));
-    return u;
-  }
-  return tree (TUPLE, "0");
-}
-
-static void
-ispell_send (string lan, string s) {
-  ispeller sc= ispeller (lan);
-  if ((!is_nil (sc)) && sc->ln->alive) sc->send (s);
-}
-
-static string
-ispell_eval (string lan, string s) {
-  ispeller sc= ispeller (lan);
-  if ((!is_nil (sc)) && sc->ln->alive) {
-    sc->send (s);
-    return sc->retrieve ();
-  }
-  return "";
-}
-
-/******************************************************************************
-* Spell checking interface
-******************************************************************************/
-
 string
 ispell_start (string lan) {
   if (DEBUG_IO) debug_spell << "Start " << lan << "\n";
   ispeller sc= ispeller (lan);
   if (is_nil (sc)) sc= tm_new<ispeller_rep> (lan);
-  return sc->start ();
+  string res = sc->start ();
+  return res;
 }
 
 tree
 ispell_check (string lan, string s) {
   if (DEBUG_IO) debug_spell << "Check " << s << "\n";
   ispeller sc= ispeller (lan);
-  if (is_nil (sc) || (!sc->ln->alive)) {
+  
+  if (is_nil (sc)) {
     string message= ispell_start (lan);
     if (starts (message, "Error: ")) return message;
+    sc = ispeller(lan);
   }
-  if (sc->unavailable) return "Error: unavailable";
-  string ret_s= ispell_eval (lan, "^" * s);
-  if (starts (ret_s, "Error: ")) return ret_s;
-  return parse_ispell (ret_s);
+  
+  if (is_nil(sc) || sc->unavailable) return "Error: unavailable";
+  
+  return sc->check(s);
 }
 
 void
 ispell_accept (string lan, string s) {
   if (DEBUG_IO) debug_spell << "Accept " << s << "\n";
-  ispell_send (lan, "@" * s);
+  ispeller sc= ispeller (lan);
+  if (!is_nil (sc)) sc->accept(s);
 }
 
 void
 ispell_insert (string lan, string s) {
   if (DEBUG_IO) debug_spell << "Insert " << s << "\n";
-  ispell_send (lan, "*" * s);
+  ispeller sc= ispeller (lan);
+  if (!is_nil (sc)) sc->insert(s);
 }
 
 void
 ispell_done (string lan) {
   if (DEBUG_IO) debug_spell << "End " << lan << "\n";
-  ispell_send (lan, "#");
+  ispeller sc= ispeller (lan);
+  if (!is_nil (sc)) {
+      sc->save_all();
+  }
 }
+#endif
