b611f0b0475d45b2959b3c3013a8b79fc27c429e
Robin Wils <robin.wilz@gmail.com>
2025-12-17 10:29:00 +0000

    chat: autoreload after sending
    git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@15191 64cb5145-927a-446d-8aed-2fb7b4773692

--- TeXmacs/progs/client/client-chat.scm
+++ TeXmacs/progs/client/client-chat.scm
@@ -125,7 +125,8 @@
         (begin
           (tree-set! mt `(document ""))
           (client-remote-eval server cmd ignore))
-        (set-message "this chat room is read only" "send message"))))
+        (set-message "this chat room is read only" "send message"))
+    (revert-buffer)))
 
 (tm-define (kbd-control-return)
   (:require (inside? 'chat-input))
