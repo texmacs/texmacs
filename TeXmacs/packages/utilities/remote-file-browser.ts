725b3476ac50d561595b2f63a0f02dd230741aad
Robin Wils <robin.wilz@gmail.com>
2026-01-13 17:30:17 +0000

    client: remote dir entry as table with custom style
    git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@15226 64cb5145-927a-446d-8aed-2fb7b4773692

---
+++ TeXmacs/packages/utilities/remote-file-browser.ts
@@ -0,0 +1,60 @@
+<TeXmacs|2.1.4>
+
+<style|source>
+
+<\body>
+  <active*|<\src-title>
+    <src-package|remote-file-browser|1.0>
+
+    <\src-purpose>
+      Remote File Browser
+    </src-purpose>
+
+    <src-copyright|2026|Robin WILS>
+
+    <\src-license>
+      This software falls under the <hlink|GNU general public license,
+      version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+      WARRANTY WHATSOEVER. You should have received a copy of the license
+      which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+    </src-license>
+  </src-title>>
+
+  <use-package|std-shadow>
+
+  <\active*>
+    <\src-comment>
+      Icons
+    </src-comment>
+  </active*>
+
+  <assign|dir-entry-icon|<macro|name|tip|<with|icon|<image|<find-file|$TEXMACS_PATH/misc/pixmaps/light|$TEXMACS_PATH/misc/pixmaps/modern/24x24/main|$TEXMACS_PATH/misc/pixmaps/modern/16x16/focus|<arg|name>>||12pt||-0.666ex>|<if|<equal|<arg|tip>|>|<value|icon>|<help-balloon|<value|icon>|<arg|tip>|left|Bottom>>>>>
+
+  <\active*>
+    <\src-comment>
+      Entry macros
+    </src-comment>
+  </active*>
+
+  <assign|dir-entry|<\macro|icon-name|name|link|share-action>
+    <concat|<dir-entry-icon|<arg|icon-name>|>|<hspace|1.5spc><hlink|<arg|name>|<arg|link>>|<htab|5mm>|<arg|share-action>>
+  </macro>>
+
+  <\active*>
+    <\src-comment>
+      Container macros
+    </src-comment>
+  </active*>
+
+  <assign|dir-list|<\macro|body>
+    <with|shadow-elevation|0.75|<\drop-shadow>
+      <arg|body>
+    </drop-shadow>>
+  </macro>>
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
