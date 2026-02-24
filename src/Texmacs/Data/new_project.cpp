c005b7ac2946a4a3cfe1cad5735a106244efc9ec
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2026-02-24 16:28:18 +0000

    Fix bug #68009: Segfault with missing master project file
    git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@15296 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/Texmacs/Data/new_project.cpp
+++ src/Texmacs/Data/new_project.cpp
@@ -66,9 +66,10 @@ project_get () {
   tm_buffer buf= concrete_buffer (name);
   if (is_implicit_project (buf)) return buf->buf->name;
   if (buf->data->project == "") return url_none ();
+  url prj_name = head (name) * as_string (buf->data->project);
   // FIXME: check whether we should force large projects to be loaded
   // when opening a chapter.  This might cause unwanted slowness.
-  //url prj_name = head (name) * as_string (buf->data->project);
-  //buf->prj = concrete_buffer_insist (prj_name);
+  // if (is_nil (buf->prj)) buf->prj = concrete_buffer_insist (prj_name);
+  if (is_nil (buf->prj)) return prj_name;
   return buf->prj->buf->name;
 }
