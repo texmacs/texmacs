17fcea4fa8c4f35466a70431acf9757d46eabc8f
Joris van der Hoeven <vdhoeven@lix.polytechnique.fr>
2026-06-27 20:25:27 +0000

    Retrieve JSON value from key
    git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@15550 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/Data/Convert/convert.hpp
+++ src/Data/Convert/convert.hpp
@@ -131,12 +131,13 @@ tree vernac_to_tree (string s);
 tree vernac_document_to_tree (string s);
 
 /*** AI tools ***/
-tree json_to_tree (string s, int mode= 0);
-string tree_to_json (tree t, int mode= 0);
 tree compress_tree (tree t);
 tree decompress_tree (tree t);
 string compress_html (tree t, int mode= 0);
 tree decompress_html (string s, int mode= 0);
+tree json_to_tree (string s, int mode= 0);
+string tree_to_json (tree t, int mode= 0);
+tree json_get (tree t, tree key, int mode= 0);
 string ai_command (string s, string model, string agent, string chat= "");
 string ai_output (string s, string model, string chat= "");
 string ai_latex_command (string s, string model, string chat= "");
