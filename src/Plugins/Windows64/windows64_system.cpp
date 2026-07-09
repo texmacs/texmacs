15f22b9d5d1663fce895801f3eb63fbf0bb1fae0
Robin Wils <robin.wilz@gmail.com>
2026-07-09 14:56:15 +0000

    windows: make rename atomic with MoveFileExW + replace existing flag
    git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@15619 64cb5145-927a-446d-8aed-2fb7b4773692

--- src/Plugins/Windows64/windows64_system.cpp
+++ src/Plugins/Windows64/windows64_system.cpp
@@ -245,10 +245,11 @@ bool texmacs_rmdir(string dirname) {
 }
 
 bool texmacs_rename(string oldname, string newname) {
-  texmacs_remove(newname);
-  return MoveFileW(
-    texmacs_utf8_to_wide(oldname).c_str(), 
-    texmacs_utf8_to_wide(newname).c_str()
+  // Mimic Linux rename: atomic + overwrite existing destination.
+  return MoveFileExW(
+    texmacs_utf8_to_wide(oldname).c_str(),
+    texmacs_utf8_to_wide(newname).c_str(),
+    MOVEFILE_REPLACE_EXISTING|MOVEFILE_WRITE_THROUGH
   ) != 0;
 }
 
